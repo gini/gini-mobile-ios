495c4434403509e63c37b823a887741318f09e76
Nadya Karaban <nadya@gini.net>
2021-10-01 16:12:49 +0200

    feat(GiniHealthAPILibrary): Add HealthAPILibraryExample
    add packages to the Mobile workspace
    update init for HealthAPILibrary pinning due to the SPM
    
    #PIA-1841

--- HealthAPILibrary/HealthAPILibraryPinning/Tests/HealthAPILibraryPinningTests/HealthAPILibraryPinningTests.swift
+++ HealthAPILibrary/HealthAPILibraryPinning/Tests/HealthAPILibraryPinningTests/HealthAPILibraryPinningTests.swift
@@ -1,11 +1,10 @@
 import XCTest
-@testable import HealthAPILibraryPinning
+@testable import GiniHealthAPILibraryPinning
 
 final class HealthAPILibraryPinningTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HealthAPILibraryPinning().text, "Hello, World!")
+        // results.        //XCTAssertEqual(GiniHealthAPILibraryPinning().text, "Hello, World!")
     }
 }
