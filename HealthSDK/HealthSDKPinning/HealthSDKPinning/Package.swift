c6aa78699a0734bf4a3f3453d26e6b77b5c287be
Nadya Karaban <nadya@gini.net>
2021-10-04 19:00:23 +0200

    feat(GiniHealthSDKPinning): Add HealthSDKExample target
    in SPM is currently not possible to have 2 products with shared resources:
    
    https://forums.swift.org/t/two-libraries-with-shared-source-overlapping-error/32448
    https://forums.swift.org/t/spm-multi-package-repositories/43193
    paths cannot be outside from root
    https://forums.swift.org/t/why-does-spm-prevent-target-source-paths-from-outside-the-package-root/35796
    
    update path for GiniHealthSDK package -> no need to specify path if you use Sources/ProductName
    empty folder cannot be set as a path
    remove path from HealthSDK manifest -> no need after renamed folder
    reoder packages in workspace
    
    #PIA-1841

--- HealthSDK/HealthSDKPinning/HealthSDKPinning/Package.swift
+++ HealthSDK/HealthSDKPinning/HealthSDKPinning/Package.swift
@@ -4,29 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "HealthSDK",
-    platforms: [.iOS(.v11)],
+    name: "HealthSDKPinning",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "GiniHealthSDK",
-            targets: ["GiniHealthSDK"]),
+            name: "GiniHealthSDKPinning",
+            targets: ["GiniHealthSDKPinning"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "HealthAPILibrary", path: "../HealthAPILibrary/HealthAPILibrary"),
+        .package(name: "HealthAPILibraryPinning", path: "../HealthAPILibrary/HealthAPILibraryPinning"),
+        .package(name: "HealthSDK", path: "../HealthSDK")
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        
         .target(
-            name: "GiniHealthSDK",
-            dependencies: [.product(name: "GiniHealthAPILibrary", package: "HealthAPILibrary")]
-        ),
+            name: "GiniHealthSDKPinning",
+            dependencies: [.product(name: "GiniHealthAPILibraryPinning", package: "HealthAPILibraryPinning"),
+                           .product(name: "GiniHealthSDK", package: "HealthSDK")]),
         .testTarget(
-            name: "HealthSDKTests",
-            dependencies: ["GiniHealthSDK"]),
+            name: "HealthSDKPinningTests",
+            dependencies: ["GiniHealthSDKPinning"]),
     ]
 )
