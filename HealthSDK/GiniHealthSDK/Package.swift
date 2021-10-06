b7d4b34ce22cc9daefe2ba15c7ebf041d495d0b7
Nadya Karaban <nadya@gini.net>
2021-10-06 18:21:51 +0200

    tfeat(GiniHealthSDK): rename package with prefix Gini
    moved resources and code into the GiniHealthSDK -> path is package manifest is no needed anymore
    rename test target with prefix Gini
    
    #PIA-1841

--- HealthSDK/GiniHealthSDK/Package.swift
+++ HealthSDK/GiniHealthSDK/Package.swift
@@ -4,7 +4,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "HealthSDK",
+    name: "GiniHealthSDK",
+    defaultLocalization: "en",
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -15,7 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "HealthAPILibrary", path: "../HealthAPILibrary/HealthAPILibrary"),
+        .package(name: "GiniHealthAPILibrary", path: "../HealthAPILibrary/GiniHealthAPILibrary"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,10 +25,9 @@ let package = Package(
         .target(
             name: "GiniHealthSDK",
             dependencies: [.product(name: "GiniHealthAPILibrary",
-                                    package: "HealthAPILibrary")],
-            resources: [.copy("Resources")]),
+                                    package: "GiniHealthAPILibrary")]),
         .testTarget(
-            name: "HealthSDKTests",
+            name: "GiniHealthSDKTests",
             dependencies: ["GiniHealthSDK"]),
     ]
 )
