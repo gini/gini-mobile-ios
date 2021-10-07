b9ec166f7673bec7d760164a5e8043ad65a2cd2c
Nadya Karaban <nadya@gini.net>
2021-10-07 14:50:43 +0200

    feat(HealthAPILibrary): add tests
    Add integration tests to the Example project, because it's not possible to pass .entitlements in test target
    https://forums.swift.org/t/newbie-spm-with-or-without-a-project-file/26591/4 (edited)
    Add schemes to the workspace
    
    #PIA-1841

--- HealthAPILibrary/GiniHealthAPILibrary/Package.swift
+++ HealthAPILibrary/GiniHealthAPILibrary/Package.swift
@@ -24,6 +24,9 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "GiniHealthAPILibraryTests",
-            dependencies: ["GiniHealthAPILibrary"]),
+            dependencies: ["GiniHealthAPILibrary"],
+            resources: [
+                    .process("Resources")
+                ]),
     ]
 )
