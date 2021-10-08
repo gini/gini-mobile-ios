6c2bb1abc59eb9ae99eb9b13c04ba65c0e8cc2ac
Nadya Karaban <nadya@gini.net>
2021-10-08 18:42:49 +0200

    refactor(GiniHealthAPILibrary, GiniHealthSDK): review notes
    recreate schemes
    fix tests
    
    #PIA-1841

--- HealthSDK/GiniHealthSDK/Tests/GiniHealthSDKTests/HealthSDKTests.swift
+++ HealthSDK/GiniHealthSDK/Tests/GiniHealthSDKTests/HealthSDKTests.swift
@@ -1,11 +1,7 @@
 import XCTest
-@testable import HealthSDK
+@testable import GiniHealthSDK
 
 final class HealthSDKTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HealthSDK().text, "Hello, World!")
     }
 }
