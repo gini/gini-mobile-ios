242eb440ccb77be6df1a1a89768f7b0abff47551
Nadzeya <nadya@gini.net>
2021-10-08 17:52:47 +0200

    Update HealthSDK/GiniHealthSDKPinning/Package.swift
    Co-authored-by: Alpár Szotyori <alpar@gini.net>

--- HealthSDK/GiniHealthSDKPinning/Package.swift
+++ HealthSDK/GiniHealthSDKPinning/Package.swift
@@ -23,8 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GiniHealthSDKPinning",
-            dependencies: [.product(name: "GiniHealthAPILibraryPinning", package: "GiniHealthAPILibraryPinning"),
-                           .product(name: "GiniHealthSDK", package: "GiniHealthSDK")]),
+            dependencies: ["GiniHealthAPILibraryPinning", "GiniHealthSDK"]),
         .testTarget(
             name: "GiniHealthSDKPinningTests",
             dependencies: ["GiniHealthSDKPinning"]),
