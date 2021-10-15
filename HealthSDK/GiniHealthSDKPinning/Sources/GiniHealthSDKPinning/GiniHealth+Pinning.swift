4a111a78c3bc30a3fbedf96c41f76e02c9aeb30e
Nadya Karaban <nadya@gini.net>
2021-10-15 17:12:06 +0200

    ci: Add swift files with a version
    PIA-1849

--- HealthSDK/GiniHealthSDKPinning/Sources/GiniHealthSDKPinning/GiniHealth+Pinning.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Nadya Karaban on 04.10.21.
-//
-
-import Foundation
---
+++ HealthSDK/GiniHealthSDKPinning/Sources/GiniHealthSDKPinning/GiniHealthSDKPinningVersion.swift
@@ -0,0 +1,8 @@
+//
+//  GiniHealthSDKPinningVersion.swift
+//  
+//
+//  Created by Nadya Karaban on 15.10.21.
+//
+
+public let GiniHealthSDKPinningVersion = "0.0.1"
