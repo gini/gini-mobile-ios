085b397c9c826773eb34fa92401fab7a871207a8
Nadya Karaban <nadya@gini.net>
2021-10-18 16:46:20 +0200

    ci: fastlane setup
    PIA-1849

---
+++ fastlane/util/lanes.rb
@@ -0,0 +1,13 @@
+##
+# Extracts the required options from the options dictionary.
+#
+# Halts and prints an error if the required option is missing.
+#
+def check_and_get_options(options, required_options, ui)
+  required_options.map { |required|
+    if !options[required]
+      ui.user_error! "Missing option: #{required}"
+    end
+    options[required]
+  }
+end
---
+++ fastlane/util/versions.rb
@@ -0,0 +1,70 @@
+##
+# Extract the project version from the tag.
+#
+# The tag must have the following format: `<project-id>;<version>`.
+#
+# Halts and prints an error if the project id is not in the tag.
+#
+def get_project_version_from_tag(project_id, tag, ui)
+  tag = normalize_tag(tag)
+  components = tag.split(';')
+
+  if components[0] != project_id
+    ui.user_error! "The project id '#{project_id}' is not in the tag: #{tag}"
+  end
+
+  if components.size != 2
+    ui.user_error! "Missing project version from the tag: #{tag}"
+  end
+
+  components[1].strip
+end
+
+##
+# Extract the example app version of a project from the tag.
+#
+# The tag must have the following format: `<project-id>;<version>;<example-app-id>;<version>`.
+#
+# Halts and prints an error if the project id or example app id is not in the tag.
+#
+def get_example_app_version_from_tag(project_id, example_app_id, tag, ui)
+  tag = normalize_tag(tag)
+  components = tag.split(';')
+
+  if components[0] != project_id
+    ui.user_error! "The project id '#{project_id}' is not in the tag: #{tag}"
+  end
+
+  if components[2] != example_app_id
+    ui.user_error! "The example app id '#{example_app_id}' is not in the tag: #{tag}"
+  end
+
+  if components.size != 4
+    ui.user_error! "Missing example app version from the tag: #{tag}"
+  end
+
+  components[3].strip
+end
+
+def normalize_tag(tag)
+  tag.delete_prefix("refs/tags/")
+end
+
+##
+# Retrieve the version from the project's "#{project_id}Version.swift" file.
+#
+def get_project_version_from_version_file(version_file_path, ui)
+  version_file = File.open("../#{version_file_path}")
+  version_file.readlines.each do |line|
+    if !line.start_with?('//') && line.include?("Version")
+      components = line.split(" = ")
+
+      if components.size != 2
+        ui.user_error! "Wrong version line formatting: #{line}"
+      end 
+
+      version = line.split(" = ")[1]
+      break version.strip.delete('"')
+    end
+  end
+end
