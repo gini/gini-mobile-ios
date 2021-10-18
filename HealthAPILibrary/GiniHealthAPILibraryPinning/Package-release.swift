4f78f2b059f7661cc60f3c4d4978b63daa605f43
Nadya Karaban <nadya@gini.net>
2021-10-18 16:51:07 +0200

    ci: Fix manifest files for release
    PIA-1849

--- HealthAPILibrary/GiniHealthAPILibraryPinning/Package-release.swift
+++ HealthAPILibrary/GiniHealthAPILibraryPinning/Package-release.swift
@@ -16,8 +16,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         
-        .package(name: "TrustKit", url: "https://github.com/datatheorem/TrustKit.git" , from: "2.0.0"),
-        .package(name: "GiniHealthAPILibrary","https://github.com/gini/health-api-library-ios.git" , from: "0.0.1"),
+        .package(name: "TrustKit", url: "https://github.com/datatheorem/TrustKit.git", from: "2.0.0"),
+        .package(name: "GiniHealthAPILibrary", url: "https://github.com/gini/health-api-library-ios.git", from: "0.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
