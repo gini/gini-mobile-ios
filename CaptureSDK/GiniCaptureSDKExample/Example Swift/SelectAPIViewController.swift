282a89d75168df13d1ca5cb45e4cb283131d42c8
Nadzeya Karaban <nadya@gini.net>
2021-11-02 14:44:01 +0100

    feat(CaptureSDK): add sources for Example project and Pinning SDK
    PIA-1947

---
+++ CaptureSDK/GiniCaptureSDKExample/Example Swift/SelectAPIViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ScreenAPIViewController.swift
+//  GiniCapture
+//
+//  Created by Peter Pult on 05/30/2016.
+//  Copyright © 2016 Gini. All rights reserved.
+//
+
+import UIKit
+import GiniCaptureSDK
+
+protocol SelectAPIViewControllerDelegate: AnyObject {
+    func selectAPI(viewController: SelectAPIViewController, didSelectApi api: GiniCaptureAPIType)
+    func selectAPI(viewController: SelectAPIViewController, didTapSettings: ())
+}
+
+/**
+ Integration options for Gini Capture SDK.
+ */
+enum GiniCaptureAPIType {
+    case screen
+    case component
+}
+
+/**
+ View controller showing how to capture an image of a document using the Screen API of the Gini Capture SDK for iOS
+ and how to process it using the Gini SDK for iOS.
+ */
+final class SelectAPIViewController: UIViewController {
+    
+    @IBOutlet weak var metaInformationButton: UIButton!
+    
+    weak var delegate: SelectAPIViewControllerDelegate?
+        
+    var clientId: String?
+    
+    // MARK: View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        let metaTitle = "Gini Capture SDK: (\(GiniCapture.versionString)) / Client id: \(self.clientId ?? "")"
+        metaInformationButton.setTitle(metaTitle, for: .normal)
+    }
+    
+    // MARK: User interaction
+    @IBAction func launchScreenAPI(_ sender: Any) {
+        delegate?.selectAPI(viewController: self, didSelectApi: .screen)
+    }
+
+    @IBAction func launchComponentAPI(_ sender: Any) {
+        delegate?.selectAPI(viewController: self, didSelectApi: .component)
+    }
+    
+    @IBAction func launchSettings(_ sender: Any) {
+        delegate?.selectAPI(viewController: self, didTapSettings: ())
+    }
+    
+}
---
+++ CaptureSDK/GiniCaptureSDKExample/UITests/GINIOnboardingUITests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+class GINIOnboardingUITests: XCTestCase {
+    let app = XCUIApplication()
+
+    override func setUp() {
+        app.launch()
+    }
+
+    func testScreenAPILaunchAndClose() {
+        let screenAPIButton = app.buttons["Screen API"]
+        screenAPIButton.tap()
+
+        let closeButton = app.navigationBars["Dokument fotografieren"].buttons.firstMatch
+        closeButton.tap()
+
+        XCTAssert(screenAPIButton.exists, "should be back to launch screen including the screen api launch button")
+    }
+}
