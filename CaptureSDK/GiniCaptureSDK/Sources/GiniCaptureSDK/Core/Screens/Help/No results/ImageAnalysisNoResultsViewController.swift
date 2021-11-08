ce2f148cfd1f7311122cd8e1f06db4f07e933026
Nadzeya Karaban <nadya@gini.net>
2021-11-08 15:45:28 +0100

    feat(GiniCaptureSDK): Fix bundle for resources.
    Instead of Bundle(for: GiniCapture.self) now used Bundle.module
    
    PIA-1948

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/No results/ImageAnalysisNoResultsViewController.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/No results/ImageAnalysisNoResultsViewController.swift
@@ -65,22 +65,22 @@ public final class ImageAnalysisNoResultsViewController: UIViewController {
     
     public convenience init(title: String? = nil,
                             subHeaderText: String? = NSLocalizedString("ginicapture.noresults.collection.header",
-                                                                       bundle: Bundle(for: GiniCapture.self),
+                                                                       bundle: giniCaptureBundle(),
                                                                        comment: "no results suggestions collection " +
         "header title"),
                             topViewText: String = NSLocalizedString("ginicapture.noresults.warning",
-                                                                    bundle: Bundle(for: GiniCapture.self),
+                                                                    bundle: giniCaptureBundle(),
                                                                     comment: "Warning text that indicates that there " +
         "was any result for this photo analysis"),
                             topViewIcon: UIImage? = UIImage(named: "warningNoResults",
-                                                            in: Bundle(for: GiniCapture.self),
+                                                            in: giniCaptureBundle(),
                                                             compatibleWith: nil)?.withRenderingMode(.alwaysTemplate),
                             bottomButtonText: String? = NSLocalizedString("ginicapture.noresults.gotocamera",
-                                                                          bundle: Bundle(for: GiniCapture.self),
+                                                                          bundle: giniCaptureBundle(),
                                                                           comment: "bottom button title (go to camera" +
         " button)"),
                             bottomButtonIcon: UIImage? = UIImage(named: "cameraIcon",
-                                                                 in: Bundle(for: GiniCapture.self),
+                                                                 in: giniCaptureBundle(),
                                                                  compatibleWith: nil)) {
         self.init(title: title,
                   subHeaderText: subHeaderText,
