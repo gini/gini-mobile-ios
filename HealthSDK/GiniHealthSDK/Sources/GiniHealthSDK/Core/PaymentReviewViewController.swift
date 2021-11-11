ffb7aa86ecc4f4bfe944a5bbc80ac84880a85528
Nadzeya Karaban <nadya@gini.net>
2021-11-11 14:00:10 +0100

    chor(GiniHealthSDK): Fix bundle in storyboard.
    PIA-1981

--- HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/PaymentReviewViewController.swift
+++ HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/PaymentReviewViewController.swift
@@ -681,23 +681,3 @@ extension PaymentReviewViewController {
         present(alertController, animated: true, completion: nil)
     }
 }
-
-class GiniCustomButton: UIButton {
-    var disabledBackgroundColor: UIColor? = .gray
-    var defaultBackgroundColor: UIColor? {
-        didSet {
-            backgroundColor = defaultBackgroundColor
-        }
-    }
-    override public var isEnabled: Bool {
-        didSet {
-            self.backgroundColor = isEnabled ? defaultBackgroundColor : disabledBackgroundColor
-        }
-    }
-    
-    override var isHighlighted: Bool {
-        didSet {
-            self.alpha = isHighlighted ? 0.5 : 1
-        }
-    }
-}
