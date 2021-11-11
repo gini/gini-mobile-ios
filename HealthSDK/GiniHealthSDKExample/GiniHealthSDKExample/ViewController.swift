32efedd8af0b060f4a77aae0ea7178da98836514
Nadzeya Karaban <nadya@gini.net>
2021-11-11 14:18:23 +0100

    chor(GiniHealthSDK): Rename project folder and targets.
    Tooltips and import works in ComponentAPI with giniConfiguration.onboardingShowAtFirstLaunch = false
    
    PIA-1981

---
+++ HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiniHealthSDKExample
+//
+//  Created by Nadya Karaban on 10.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
