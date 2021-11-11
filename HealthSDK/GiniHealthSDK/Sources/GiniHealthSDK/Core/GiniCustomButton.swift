e2640cf9f22bbac6d1f604384f579e8ca62f456e
Nadzeya Karaban <nadya@gini.net>
2021-11-11 14:00:10 +0100

    chor(GiniHealthSDK): Fix bundle in storyboard.
    PIA-1981

---
+++ HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/GiniCustomButton.swift
@@ -0,0 +1,27 @@
+//
+//  GiniCustomButton.swift
+//  
+//
+//  Created by Nadya Karaban on 10.11.21.
+//
+
+import UIKit
+class GiniCustomButton: UIButton {
+    var disabledBackgroundColor: UIColor? = .gray
+    var defaultBackgroundColor: UIColor? {
+        didSet {
+            backgroundColor = defaultBackgroundColor
+        }
+    }
+    override public var isEnabled: Bool {
+        didSet {
+            self.backgroundColor = isEnabled ? defaultBackgroundColor : disabledBackgroundColor
+        }
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            self.alpha = isHighlighted ? 0.5 : 1
+        }
+    }
+}
