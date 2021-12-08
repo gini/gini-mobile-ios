615ce6121e69dc2defb97233ca48e9e06747b5aa
Nadzeya Karaban <nadya@gini.net>
2021-12-08 17:25:50 +0100

    feat(GiniHealthSDK): Add bank selection screen with mocked data.
    PIA-2040

--- HealthAPILibrary/GiniHealthAPILibrary/Sources/GiniHealthAPILibrary/Documents/Payments/PaymentProvider.swift
+++ HealthAPILibrary/GiniHealthAPILibrary/Sources/GiniHealthAPILibrary/Documents/Payments/PaymentProvider.swift
@@ -10,9 +10,13 @@ import Foundation
 /**
  Struct for MinAppVersions in payment provider response
  */
-struct MinAppVersions: Codable {
+public struct MinAppVersions: Codable {
     var ios: String?
     var android: String?
+    public init(ios: String?, android: String?) {
+        self.ios = ios
+        self.android = android
+    }
 }
 /**
  Struct for payment provider response
@@ -22,6 +26,12 @@ public struct PaymentProvider: Codable {
     public var name: String
     public var appSchemeIOS: String
     var minAppVersion: MinAppVersions?
-}
 
+    public init(id: String, name: String, appSchemeIOS: String, minAppVersion: MinAppVersions?) {
+        self.id = id
+        self.name = name
+        self.appSchemeIOS = appSchemeIOS
+        self.minAppVersion = minAppVersion
+    }
+}
 public typealias PaymentProviders = [PaymentProvider]
