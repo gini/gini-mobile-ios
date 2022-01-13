cb766141baf49995e650c56683c7e4754cb93e55
Nadzeya Karaban <nadya@gini.net>
2022-01-13 11:55:31 +0100

    refactor(GiniCaptureSDK): Add code formatting.
    PIA-2146

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Networking/GiniCaptureNetworkService.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Networking/GiniCaptureNetworkService.swift
@@ -71,27 +71,25 @@ class DefaultCaptureNetworkService: GiniCaptureNetworkService {
     
     func upload(document: GiniCaptureDocument, metadata: Document.Metadata?, completion: @escaping UploadDocumentCompletion) {
         Log(message: "Creating document...", event: "📝")
-        
+
         let fileName = "Partial-\(NSDate().timeIntervalSince1970)"
-        
+
         documentService.createDocument(fileName: fileName,
                                        docType: nil,
                                        type: .partial(document.data),
                                        metadata: metadata) { result in
-                                        switch result {
-                                        case .success(let createdDocument):
-                                            Log(message: "Created document with id: \(createdDocument.id) " +
-                                                "for vision document \(document.id)", event: "📄")
-                                            completion(.success(createdDocument))
-                                        case .failure(let error):
-                                            let message = "Document creation failed"
-                                            Log(message: message, event: .error)
-                                            let errorLog = ErrorLog(description: message, error: error)
-                                            GiniConfiguration.shared.errorLogger.handleErrorLog(error: errorLog)
-                                            completion(.failure(error))
-                                        }
+            switch result {
+            case let .success(createdDocument):
+                Log(message: "Created document with id: \(createdDocument.id) " +
+                    "for vision document \(document.id)", event: "📄")
+                completion(.success(createdDocument))
+            case let .failure(error):
+                let message = "Document creation failed"
+                Log(message: message, event: .error)
+                let errorLog = ErrorLog(description: message, error: error)
+                GiniConfiguration.shared.errorLogger.handleErrorLog(error: errorLog)
+                completion(.failure(error))
+            }
         }
     }
-    
-    
 }
