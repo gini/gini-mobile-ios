7358fbc8d71e95d801ce12f31725553987ecad4c
Alpar Szotyori <alpar@gini.net>
2022-03-01 09:59:28 +0100

    feat(GiniCaptureSDK): Return Gini Bank API document in `AnalysisResult`
    Allows clients to retrieve the created Gini Bank API document when using the Screen API with
    default networking.
    
    PIA-2279

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Networking/AnalysisResult.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Networking/AnalysisResult.swift
@@ -9,24 +9,37 @@ import UIKit
 import GiniBankAPILibrary
 
 @objcMembers public class AnalysisResult: NSObject {
-    /// Images processed in the analysis
+    /**
+     * Images processed in the analysis.
+     */
     public let images: [UIImage]
-    /*
+    
+    /**
      *  Specific extractions obtained in the analysis.
-     *  Besides the list of extractions that can be found here
-     *  (http://developer.gini.net/gini-api/html/document_extractions.html#available-specific-extractions),
+     *
+     *  Besides the list of extractions that can be found
+     *  [here](http://developer.gini.net/gini-api/html/document_extractions.html#available-specific-extractions),
      *  it can also return the epsPaymentQRCodeUrl extraction, obtained from a EPS QR code.
      */
     public let extractions: [String: Extraction]
 
-    /*
+    /**
      *  Line item compound extractions obtained in the analysis.
      */
     public let lineItems: [[Extraction]]?
     
-    public init(extractions: [String: Extraction], lineItems: [[Extraction]]? = nil, images: [UIImage]) {
+    /**
+     *  The analyzed Gini Bank API document.
+     *
+     *  It returns `nil` when extractions were retrieved without using the Gini Bank API.
+     *  For example when the extractions came from an EPS QR code.
+     */
+    public let document: Document?
+    
+    public init(extractions: [String: Extraction], lineItems: [[Extraction]]? = nil, images: [UIImage], document: Document? = nil) {
         self.images = images
         self.extractions = extractions
         self.lineItems = lineItems
+        self.document = document
     }
 }
