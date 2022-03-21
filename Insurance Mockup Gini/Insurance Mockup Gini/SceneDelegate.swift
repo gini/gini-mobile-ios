cf1c47bf2b90a75f729d196e49f27edf43660aff
David Vizaknai <david@halcyonmobile.com>
2022-03-21 15:46:56 +0200

    Add empty Xcode project for the Insurance Mockup app

---
+++ Insurance Mockup Gini/Insurance Mockup Gini/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//   Gini
+//
+//  Created by David Vizaknai on 21.03.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Insurance Mockup Gini/Insurance Mockup Gini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//   Gini
+//
+//  Created by David Vizaknai on 21.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
