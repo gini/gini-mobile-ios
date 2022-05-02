eef9afb2198b04c6b2b056955405ccb340c5f72c
Nadya Karaban <nadya@gini.net>
2022-05-02 19:35:07 +0200

    chore(CaptureSDK): Add example for Pinning.
    PIA-2485

---
+++ CaptureSDK/GiniCaptureSDKPinningExample/Example Swift/NoResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  NoResultViewController.swift
+//  GiniCapture
+//
+//  Created by Peter Pult on 22/08/2016.
+//  Copyright © 2016 Gini. All rights reserved.
+//
+
+import UIKit
+
+protocol NoResultsScreenDelegate: AnyObject {
+    func noResults(viewController: NoResultViewController, didTapRetry: ())
+}
+
+final class NoResultViewController: UIViewController {
+    @IBOutlet var rotateImageView: UIImageView!
+    weak var delegate: NoResultsScreenDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        rotateImageView.image = rotateImageView.image?.withRenderingMode(.alwaysTemplate)
+    }
+
+    @IBAction func retry(_ sender: AnyObject) {
+        delegate?.noResults(viewController: self, didTapRetry: ())
+    }
+}
