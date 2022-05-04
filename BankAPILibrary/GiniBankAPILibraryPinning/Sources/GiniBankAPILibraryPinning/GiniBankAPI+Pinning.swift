4b82e6ffde01682880b8a9117210b2459df5c04e
Nadya Karaban <nadya@gini.net>
2022-05-04 13:29:46 +0200

    refactor(GiniBankAPILibrary): Now is possible to use with a custom access token source and certificate pinning.
    PIA-2485

--- BankAPILibrary/GiniBankAPILibraryPinning/Sources/GiniBankAPILibraryPinning/GiniBankAPI+Pinning.swift
+++ BankAPILibrary/GiniBankAPILibraryPinning/Sources/GiniBankAPILibraryPinning/GiniBankAPI+Pinning.swift
@@ -9,11 +9,54 @@ import TrustKit
 import GiniBankAPILibrary
 
 public extension GiniBankAPI.Builder {
+    
+    /**
+     *  Creates a Gini Bank API Library with certificate pinning configuration.
+     *
+     * - Parameter client:            The Gini Bank API client credentials
+     * - Parameter api:               The Gini Bank API that the library interacts with. `APIDomain.default` by default
+     * - Parameter pinningConfig:     Configuration for certificate pinning.
+     * - Parameter logLevel:          The log level. `LogLevel.none` by default.
+     */
     init(client: Client,
-                api: APIDomain = .default,
-                pinningConfig: [String: Any],
-                logLevel: LogLevel = .none) {
+         api: APIDomain = .default,
+         pinningConfig: [String: Any],
+         logLevel: LogLevel = .none) {
         self.init(client: client, api: api, userApi: .default, logLevel: logLevel, sessionDelegate: SessionDelegate())
+        TrustKit.initSharedInstance(withConfiguration: pinningConfig)
+    }
+    
+    /**
+     *  Creates a Gini Bank API Library with certificate pinning configuration.
+     *
+     * - Parameter client:            The Gini Bank API client credentials
+     * - Parameter api:               The Gini Bank API that the library interacts with. `APIDomain.default` by default
+     * - Parameter userApi:           The Gini User API that the library interacts with. `UserDomain.default` by default
+     * - Parameter pinningConfig:     Configuration for certificate pinning.
+     * - Parameter logLevel:          The log level. `LogLevel.none` by default.
+     */
+    init(client: Client,
+         api: APIDomain = .default,
+         userApi: UserDomain = .default,
+         pinningConfig: [String: Any],
+         logLevel: LogLevel = .none) {
+        self.init(client: client, api: api, userApi: userApi, logLevel: logLevel, sessionDelegate: SessionDelegate())
+        TrustKit.initSharedInstance(withConfiguration: pinningConfig)
+    }
+    
+    /**
+     * Creates a Gini Bank API Library to be used with a transparent proxy and a custom api access token source and cartificate pinning configuration.
+     *
+     * - Parameter customApiDomain:        A custom api domain string.
+     * - Parameter alternativeTokenSource: A protocol for using custom api access token
+     * - Parameter pinningConfig:          Configuration for certificate pinning.
+     * - Parameter logLevel:               The log level. `LogLevel.none` by default.
+     */
+    init(customApiDomain: String,
+         alternativeTokenSource: AlternativeTokenSource,
+         pinningConfig: [String: Any],
+         logLevel: LogLevel = .none) {
+        self.init(customApiDomain: customApiDomain, alternativeTokenSource: alternativeTokenSource, logLevel: logLevel, sessionDelegate: SessionDelegate())
         TrustKit.initSharedInstance(withConfiguration: pinningConfig)
     }
 }
