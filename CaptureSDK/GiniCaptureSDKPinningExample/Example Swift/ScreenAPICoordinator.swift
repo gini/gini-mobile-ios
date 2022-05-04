f9d325e4b7f96a7ff9d3cc1830358d3fd86c2e53
Nadya Karaban <nadya@gini.net>
2022-05-04 15:13:38 +0200

    chore(GiniCaptureSDK): Update tests for the pinning example.
    PIA-2485

--- CaptureSDK/GiniCaptureSDKPinningExample/Example Swift/ScreenAPICoordinator.swift
+++ CaptureSDK/GiniCaptureSDKPinningExample/Example Swift/ScreenAPICoordinator.swift
@@ -32,6 +32,23 @@ final class ScreenAPICoordinator: NSObject, Coordinator {
     var visionDocuments: [GiniCaptureDocument]?
     var visionConfiguration: GiniConfiguration
     var sendFeedbackBlock: (([String: Extraction]) -> Void)?
+    let yourPublicPinningConfig = [
+        kTSKPinnedDomains: [
+        "pay-api.gini.net": [
+            kTSKPublicKeyHashes: [
+            // old *.gini.net public key
+            "cNzbGowA+LNeQ681yMm8ulHxXiGojHE8qAjI+M7bIxU=",
+            // new *.gini.net public key, active from around June 2020
+            "zEVdOCzXU8euGVuMJYPr3DUU/d1CaKevtr0dW0XzZNo="
+        ]],
+        "user.gini.net": [
+            kTSKPublicKeyHashes: [
+            // old *.gini.net public key
+            "cNzbGowA+LNeQ681yMm8ulHxXiGojHE8qAjI+M7bIxU=",
+            // new *.gini.net public key, active from around June 2020
+            "zEVdOCzXU8euGVuMJYPr3DUU/d1CaKevtr0dW0XzZNo="
+        ]],
+    ]] as [String: Any]
 
     init(configuration: GiniConfiguration,
          importedDocuments documents: [GiniCaptureDocument]?,
@@ -45,23 +62,6 @@ final class ScreenAPICoordinator: NSObject, Coordinator {
     }
 
     func start() {
-        let yourPublicPinningConfig = [
-            kTSKPinnedDomains: [
-            "pay-api.gini.net": [
-                kTSKPublicKeyHashes: [
-                // old *.gini.net public key
-                "cNzbGowA+LNeQ681yMm8ulHxXiGojHE8qAjI+M7bIxU=",
-                // new *.gini.net public key, active from around June 2020
-                "zEVdOCzXU8euGVuMJYPr3DUU/d1CaKevtr0dW0XzZNo="
-            ]],
-            "user.gini.net": [
-                kTSKPublicKeyHashes: [
-                // old *.gini.net public key
-                "cNzbGowA+LNeQ681yMm8ulHxXiGojHE8qAjI+M7bIxU=",
-                // new *.gini.net public key, active from around June 2020
-                "zEVdOCzXU8euGVuMJYPr3DUU/d1CaKevtr0dW0XzZNo="
-            ]],
-        ]] as [String: Any]
 
         let viewController = GiniCapture.viewController(withClient: client,
                                                         importedDocuments: visionDocuments,
