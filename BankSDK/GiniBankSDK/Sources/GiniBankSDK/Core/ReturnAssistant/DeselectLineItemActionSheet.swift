bbf0a3e7a8d945e9c18f43b3efb5937406eff3c2
Nadya Karaban <nadya@gini.net>
2022-05-09 17:00:34 +0200

    fix(GiniBankSDK): Allow to customise the deselect action sheet title and cancel action title when you deselect line item.
    PIA-2552

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/ReturnAssistant/DeselectLineItemActionSheet.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/ReturnAssistant/DeselectLineItemActionSheet.swift
@@ -9,38 +9,31 @@ import UIKit
 import GiniBankAPILibrary
 
 class DeselectLineItemActionSheet {
-    
     func present(from viewController: UIViewController,
                  source: UIView?,
                  returnReasons: [ReturnReason],
                  completion: @escaping (DigitalInvoice.LineItem.SelectedState) -> Void) {
-        
         let actionSheet = UIAlertController(title: nil,
-                                            message: NSLocalizedString("ginibank.digitalinvoice.deselectreasonactionsheet.message",
-                                                                       bundle: giniBankBundle(),
-                                                                       comment: ""),
+                                            message: NSLocalizedStringPreferredGiniBankFormat("ginibank.digitalinvoice.deselectreasonactionsheet.message", comment: "Info message when deselect a return reason"),
                                             preferredStyle: .actionSheet)
-        
+
         for reason in returnReasons {
-            
             actionSheet.addAction(UIAlertAction(title: reason.labelInLocalLanguageOrGerman,
                                                 style: .default,
-                                                handler:
-                { _ in
-                    completion(.deselected(reason: reason))
-            }))
+                                                handler: { _ in
+                                                    completion(.deselected(reason: reason))
+                                                }))
         }
-        
-        actionSheet.addAction(UIAlertAction(title: NSLocalizedString("ginibank.digitalinvoice.deselectreasonactionsheet.action.cancel",
-                                                                     bundle: giniBankBundle(),
-                                                                     comment: ""),
+
+        actionSheet.addAction(UIAlertAction(title: NSLocalizedStringPreferredGiniBankFormat("ginibank.digitalinvoice.deselectreasonactionsheet.action.cancel",
+                                                                                            comment: "title for cancel action when deselect a return reason"),
                                             style: .cancel,
                                             handler: { _ in
                                                 completion(.selected)
-        }))
-        
+                                            }))
+
         actionSheet.popoverPresentationController?.sourceView = source
-        
+
         viewController.present(actionSheet, animated: true, completion: nil)
     }
 }
