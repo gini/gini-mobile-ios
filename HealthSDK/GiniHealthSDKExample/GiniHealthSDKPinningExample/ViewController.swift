2c4284a00454efe18b53b5af2c491da694ddaa5d
Nadya Karaban <nadya@gini.net>
2022-05-18 17:54:09 +0200

    chore(GiniHealthSDK): Add tests for the certificates pinning.
    PIA-2537

--- HealthSDK/GiniHealthSDKExample/GiniHealthSDKPinningExample/ViewController.swift
+++ HealthSDK/GiniHealthSDKExample/GiniHealthSDKPinningExample/ViewController.swift
@@ -17,32 +17,32 @@ class ViewController: UIViewController {
     }
 
     func initializeSDK() {
-        let yourPublicPinningConfig = [
-            kTSKPinnedDomains: [
-            "pay-api.gini.net": [
-                kTSKPublicKeyHashes: [
-                // old *.gini.net public key
-                "cNzbGowA+LNeQ681yMm8ulHxXiGojHE8qAjI+M7bIxU=",
-                // new *.gini.net public key, active from around June 2020
-                "zEVdOCzXU8euGVuMJYPr3DUU/d1CaKevtr0dW0XzZNo="
-            ]],
-            "user.gini.net": [
-                kTSKPublicKeyHashes: [
-                // old *.gini.net public key
-                "cNzbGowA+LNeQ681yMm8ulHxXiGojHE8qAjI+M7bIxU=",
-                // new *.gini.net public key, active from around June 2020
-                "zEVdOCzXU8euGVuMJYPr3DUU/d1CaKevtr0dW0XzZNo="
-            ]],
-        ]] as [String: Any]
-        let giniApiLib = GiniHealthAPI
-            .Builder(client: Client(id: "your-id",
-                                    secret: "your-secret",
-                                    domain: "your-domain"),
-                     api: .default,
-                     pinningConfig: yourPublicPinningConfig)
-            .build()
-        let sdk = GiniHealth(with: giniApiLib)
-        let documentService: DefaultDocumentService = sdk.documentService
+//        let yourPublicPinningConfig = [
+//            kTSKPinnedDomains: [
+//            "pay-api.gini.net": [
+//                kTSKPublicKeyHashes: [
+//                // old *.gini.net public key
+//                "cNzbGowA+LNeQ681yMm8ulHxXiGojHE8qAjI+M7bIxU=",
+//                // new *.gini.net public key, active from around June 2020
+//                "zEVdOCzXU8euGVuMJYPr3DUU/d1CaKevtr0dW0XzZNo="
+//            ]],
+//            "user.gini.net": [
+//                kTSKPublicKeyHashes: [
+//                // old *.gini.net public key
+//                "cNzbGowA+LNeQ681yMm8ulHxXiGojHE8qAjI+M7bIxU=",
+//                // new *.gini.net public key, active from around June 2020
+//                "zEVdOCzXU8euGVuMJYPr3DUU/d1CaKevtr0dW0XzZNo="
+//            ]],
+//        ]] as [String: Any]
+//        let giniApiLib = GiniHealthAPI
+//            .Builder(client: Client(id: "your-id",
+//                                    secret: "your-secret",
+//                                    domain: "your-domain"),
+//                     api: .default,
+//                     pinningConfig: yourPublicPinningConfig)
+//            .build()
+//        let sdk = GiniHealth(with: giniApiLib)
+//        let documentService: DefaultDocumentService = sdk.documentService
     }
 }
 
