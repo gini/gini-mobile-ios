de4e273f1846475e02ba8c4cd99130ccf0c54aeb
Nadya Karaban <nadya@gini.net>
2022-06-20 18:30:42 +0200

    chore(GiniCaptureSDK): Fix bundle for package.
    PIA-2603

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Resources/OnboardingViewController.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Resources/OnboardingViewController.swift
@@ -15,7 +15,7 @@ class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICo
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
-        pagesCollection.register(UINib(nibName: "OnboardingPageCell", bundle: nil), forCellWithReuseIdentifier: "onboardingPageCellIdentifier")
+        pagesCollection.register(UINib(nibName: "OnboardingPageCell", bundle: giniCaptureBundle()), forCellWithReuseIdentifier: "onboardingPageCellIdentifier")
         pagesCollection.dataSource = self
         pagesCollection.delegate = self
 
@@ -32,7 +32,7 @@ class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICo
 
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "onboardingPageCellIdentifier", for: indexPath) as! OnboardingPageCell
-        let image = #imageLiteral(resourceName: "Onboarding")
+        let image = UIImageNamedPreferred(named: "onboardingPage1")
         cell.iconView.icon = image
 
         cell.fullText.text = "Ensure that the document is flat, and positioned within the frame"
@@ -54,6 +54,7 @@ class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICo
         pageControl.currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
     }
 }
+
 class CollectionFlowLayout: UICollectionViewFlowLayout{
     override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
         invalidateLayout(with: invalidationContext(forBoundsChange: newBounds))
