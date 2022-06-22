c09f2b5bf19567c00d85c6e3d0bbe9799d11b623
Nadya Karaban <nadya@gini.net>
2022-06-22 18:23:49 +0200

    refactor(GiniCaptureSDK): Add Toolbar for bottom navigation bar which can accept custom view.
    PIA-2603

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingViewController.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingViewController.swift
@@ -11,18 +11,67 @@ import UIKit
 class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     @IBOutlet weak var pagesCollection: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var containerView: UIStackView!
+        @IBOutlet weak var viewContainer: UIStackView!
+    fileprivate var navigationBarBottomProvider: DefaultBottomNavigationBar?
+
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+    fileprivate func configureCollectionView() {
         pagesCollection.register(UINib(nibName: "OnboardingPageCell", bundle: giniCaptureBundle()), forCellWithReuseIdentifier: "onboardingPageCellIdentifier")
         pagesCollection.dataSource = self
         pagesCollection.delegate = self
-
+        
         pagesCollection.setNeedsLayout()
         pagesCollection.layoutIfNeeded()
         pagesCollection.reloadData()
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCollectionView()
+        configureBottomNavigation()
+    }
+    
+    func configureBottomNavigation() {
+        if GiniConfiguration.shared.bottomNavigationBarEnabled {
+            removeButtons()
+            if let someCustomView = DefaultBottomNavigationBar().loadNib() as?
+                DefaultBottomNavigationBar {
+                someCustomView.nextBottom.addTarget(self, action: #selector(close), for: .touchUpInside)
+
+                let customItem = UIBarButtonItem(customView: someCustomView)
+                
+                toolbarItems = [customItem]
+                navigationController?.toolbar.frame = CGRect(x: 0, y: view.frame.size.height - 120, width: view.frame.size.width, height: 120)
+                navigationController?.setToolbarHidden(false, animated: false)
+
+                navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Skip",
+                                                                    style: .plain,
+                                                                    target: self,
+                                                                    action: #selector(close))
+            }
+        }
+    }
+
+
+    fileprivate func removeButtons() {
+        nextButton.removeFromSuperview()
+        containerView.layoutSubviews()
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == pagesCollection {
+            pagesCollection.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+    }
+    
+        @IBAction func close() {
+            dismiss(animated: true)
+        }
+    
+        @IBAction func nextPage() {
+        }
 
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { 1 }
 
