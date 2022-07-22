6ce59963f58a21d063b1d2348ffd8ba102b73a75
Nadya Karaban <nadya@gini.net>
2022-07-22 15:59:26 +0200

    fix(GiniBankSDK): Remove "Save functionality" when back button is clicked.
    PIA-2810

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/ReturnAssistant/LineItemDetailsViewController.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/ReturnAssistant/LineItemDetailsViewController.swift
@@ -79,13 +79,6 @@ class LineItemDetailsViewController: UIViewController {
         super.viewWillDisappear(animated)
         guard let lineItem = lineItem, !lineItem.isUserInitiated else { return }
         
-        if isMovingFromParent, transitionCoordinator?.isInteractive == false {
-            /*
-            Automatically save changes before user returns to main screen, so in case he
-            forgets to save it, changes are not lost
-             */
-            proceedWithSaveAction(shouldPopViewController: false)
-        }
     }
     
     private func setupView() {
