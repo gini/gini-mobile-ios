82f6ba96ff0530c9829caf44ba227f0e436d1291
Krzysztof Kryniecki <chriskryn@gmail.com>
2022-08-04 13:49:07 +0200

    refactor(GiniCaptureSDK): rename file with helpcell protocol
    PIA-2838

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/Protocols/HelpCellProtocol.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/Protocols/HelpCellProtocol.swift
@@ -1,5 +1,5 @@
 //
-//  Protocols.swift
+//  HelpCellProtocol.swift
 //  
 //
 //  Created by Krzysztof Kryniecki on 02/08/2022.
@@ -8,11 +8,6 @@
 
 import Foundation
 import UIKit
-
-protocol HelpDataSourceProtocol: UITableViewDelegate, UITableViewDataSource {
-    associatedtype Cell
-    func configureCell(cell: Cell, indexPath: IndexPath)
-}
 
 public protocol HelpCell: UITableViewCell {
     static var reuseIdentifier: String { get }
