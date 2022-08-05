d9d18e27ede3514f75dedd581739c5c4e8dd6afc
Krzysztof Kryniecki <chriskryn@gmail.com>
2022-08-05 11:43:13 +0200

    refactor(GiniCaptureSDK): revert back to usage of no results screen instead of new HelpScreen
    PIA-2838

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/ComponentAPI/ComponentAPICoordinator.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/ComponentAPI/ComponentAPICoordinator.swift
@@ -472,7 +472,7 @@ extension ComponentAPICoordinator: UINavigationControllerDelegate {
         if toVC is CameraViewController &&
             (fromVC is ReviewViewController ||
                 fromVC is AnalysisViewController ||
-                fromVC is HelpTipsViewController) {
+                fromVC is ImageAnalysisNoResultsViewController) {
             // When going directly from the analysis or from the single page review screen to the camera the pages
             // collection should be cleared, since the document processed in that cases is not going to be reused
             pages.removeAll()
