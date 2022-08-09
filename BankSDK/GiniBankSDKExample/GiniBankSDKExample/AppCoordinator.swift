5c5c3567de374ffad2d934fa25891fe5756e3212
Nadya Karaban <nadya@gini.net>
2022-08-09 16:30:36 +0200

    feat(GiniBankSDKExample): Updates font customization.
    PIA-2865

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/AppCoordinator.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/AppCoordinator.swift
@@ -69,8 +69,15 @@ final class AppCoordinator: Coordinator {
 //        configuration.multipagePageSuccessfullUploadIconBackgroundColor = .systemGreen
 //        configuration.multipagePageFailureUploadIconBackgroundColor = .systemRed
 //        configuration.enableReturnReasons = false
-//        configuration.updateFont(font: UIFont(name: "Avenir", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .regular), for: .caption2)
-
+        
+    // If you need to scale your font please use our method `scaledFont()`. Please, find the example below.
+//    let customFontToBeScaled = UIFont.scaledFont(UIFont(name: "Avenir", size: 20) ?? UIFont.systemFont(ofSize: 7, weight: .regular), textStyle: .caption1)
+//    configuration.updateFont(font: customFontToBeScaled, for: .caption1)
+        
+    // If you would like to pass us already scaled font.
+//    let customScaledFont = UIFontMetrics(forTextStyle: .caption2).scaledFont(for: UIFont.systemFont(ofSize: 28))
+//    configuration.updateFont(font: customScaledFont, for: .caption2)
+        
        return configuration
     }()
     
