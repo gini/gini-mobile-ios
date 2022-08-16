898d600f9459e55ff5b86647d0e351a7a236c389
Nadya Karaban <nadya@gini.net>
2022-08-16 19:42:42 +0200

    feat(GiniCaptureSDK): OnboardingIllustrationAdapter for allowing custom animation
    PIA-2864

---
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingIllustrationAdapter.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nadya Karaban on 08.08.22.
+//
+
+import Foundation
+public protocol OnboardingIllustrationAdapter: InjectedViewAdapter {
+    func pageDidAppear()
+    func pageDidDisappear()
+}
