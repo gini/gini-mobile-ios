ac05c2eb058883197f35bb16ca3be5d5dc74bb58
Nadya Karaban <nadya@gini.net>
2022-08-16 19:27:16 +0200

    refactor(GiniCaptureSDK): Rename InjectedViewProvider -> InjectedViewAdapter
    PIA-2864

---
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/InjectedViewAdapter.swift
@@ -0,0 +1,13 @@
+//
+//  InjectedViewAdapter.swift
+//  
+//
+//  Created by Nadya Karaban on 20.05.22.
+//
+
+import Foundation
+import UIKit
+
+public protocol InjectedViewAdapter {
+    func injectedView() -> UIView
+}
