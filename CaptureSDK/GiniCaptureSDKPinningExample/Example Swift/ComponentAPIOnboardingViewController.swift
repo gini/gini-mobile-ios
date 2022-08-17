45dc0565e37d6ca3bae654759e0e2753b97fea21
Nadya Karaban <nadya@gini.net>
2022-08-17 14:26:06 +0200

    chore(GiniCaptureSDK): Disable Onboarding for component API
    PIA-2864

--- CaptureSDK/GiniCaptureSDKPinningExample/Example Swift/ComponentAPIOnboardingViewController.swift
+++ CaptureSDK/GiniCaptureSDKPinningExample/Example Swift/ComponentAPIOnboardingViewController.swift
@@ -15,32 +15,32 @@ import UIKit
  */
 final class ComponentAPIOnboardingViewController: UIViewController {
     @IBOutlet var containerView: UIView!
-    var contentController = UIViewController()
-
-    // MARK: View life cycle
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // 1. Create the onboarding view controller
-        contentController = OnboardingViewController(scrollViewDelegate: nil)
-
-        // 2. Display the onboarding view controller
-        displayContent(contentController)
-    }
-
-    // Displays the content controller inside the container view
-    func displayContent(_ controller: UIViewController) {
-        addChild(controller)
-        controller.view.frame = containerView.bounds
-        containerView.addSubview(controller.view)
-        controller.didMove(toParent: self)
-    }
+//    var contentController = UIViewController()
+//
+//    // MARK: View life cycle
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // 1. Create the onboarding view controller
+//        contentController = OnboardingViewController(scrollViewDelegate: nil)
+//
+//        // 2. Display the onboarding view controller
+//        displayContent(contentController)
+//    }
+//
+//    // Displays the content controller inside the container view
+//    func displayContent(_ controller: UIViewController) {
+//        addChild(controller)
+//        controller.view.frame = containerView.bounds
+//        containerView.addSubview(controller.view)
+//        controller.didMove(toParent: self)
+//    }
 
     // MARK: User actions
 
     @IBAction func nextPage(_ sender: AnyObject) {
         // Scroll the onboarding to the next page.
-        (contentController as? OnboardingViewController)?.scrollToNextPage(true)
+      //  (contentController as? OnboardingViewController)?.scrollToNextPage(true)
     }
 }
