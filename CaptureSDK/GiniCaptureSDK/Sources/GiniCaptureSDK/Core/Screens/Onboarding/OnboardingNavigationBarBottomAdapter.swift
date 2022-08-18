fbc88a1cde767f75ff89708d41025706302199ca
Nadya Karaban <nadya@gini.net>
2022-08-18 18:20:35 +0200

    chore(GiniCaptureSDK): Add possible implementation options for custom bottom navigation bar
    PIA-2864

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingNavigationBarBottomAdapter.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingNavigationBarBottomAdapter.swift
@@ -7,12 +7,66 @@
 
 import Foundation
 import UIKit
+// Option 3 to limit UI Control variety and limit it to UIControl and connect control and action on Gini side
+public protocol GiniNavigationButtonView: UIControl {
+    func handlePressAction()
+}
 
-public protocol OnboardingNavigationBarBottomAdapter : InjectedViewAdapter {
-    func didClickSkipButton()
+class CustomNavigationButtonView: UIControl, GiniNavigationButtonView {
+    func handlePressAction() {
+    }
+}
+
+//Option 1 Use blocks
+public protocol OnboardingNavigationBarBottomAdapter: InjectedViewAdapter {
+    // Problem 1 - needs to be called in didClickNextButton()
+    var nextButtonCompletionHandler: () -> Void { get set }
+    // Problem 2  - Action needs to be connected to the control
     func didClickNextButton()
-    func didClickGetStartedButton()
-    func showButtons(navigationButtons:[OnboardingNavigationBarBottomButton])
+    
+    func showButtons(navigationButtons: [OnboardingNavigationBarBottomButton])
+}
+
+// Option 2 Use Subclass of  Base class
+// Problem - Action needs to be connected to the control
+
+class BaseNavigationBarBottomAdapter: OnboardingNavigationBarBottomAdapter {
+    var nextButton = CustomNavigationButtonView()
+    
+    func didClickNextButton() {
+        nextButton.handlePressAction()
+    }
+    
+    var nextButtonCompletionHandler: (() -> Void) = {}
+    
+    func showButtons(navigationButtons: [OnboardingNavigationBarBottomButton]) {
+    }
+    
+    func injectedView() -> UIView {
+        if let navigationBarView =
+            OnboardingBottomNavigationBar().loadNib() as?
+                OnboardingBottomNavigationBar {
+
+            return navigationBarView
+        } else {
+            return UIView()
+        }
+    }
+    
+    
+}
+
+class CustomNavigationBarBottomAdapter: BaseNavigationBarBottomAdapter {
+    
+    override func injectedView() -> UIView {
+        return UIView()
+    }
+    
+    
+//    override func didClickNextButton() {
+//        super.didClickNextButton()
+//        //custom
+//    }
 }
 
 public enum OnboardingNavigationBarBottomButton: Int {
@@ -21,27 +75,49 @@ public enum OnboardingNavigationBarBottomButton: Int {
     case GET_STARTED
 }
 
+//public protocol OnboardingNextButton {
+//    func nextButton() -> UIView
+//    func nextButtonAction()
+//}
+
 class DefaultOnboardingNavigationBarBottomAdapter: OnboardingNavigationBarBottomAdapter {
+    var nextButtonCompletionHandler: (() -> Void) = {}
+    private func addAnimation(){}
+    private func bindAction(){}
+    func didClickSkipButton(completion: () -> Void) {
+    }
+    
+    func didClickSkipButton() {
+    }
+    //return UIView
+//    func nextButton() -> UIView {
+//        let btn = UIButton()
+//        //btn.addTarget(<#T##target: Any?##Any?#>, action: (), for: UIControl.Event)
+//        return UIView()
+//    }
+//
+//    func nextButtonAction() {
+//       print("nextButtonAction")
+//    }
+    
+    private var bottomBarView : OnboardingBottomNavigationBar?
     func injectedView() -> UIView {
         if let navigationBarView =
             OnboardingBottomNavigationBar().loadNib() as?
                 OnboardingBottomNavigationBar {
+           // navigationBarView.nextButton.addTarget(self, action: #selector(didClickNextButton), for: .touchUpInside)
+
             return navigationBarView
         } else {
             return UIView()
         }
     }
-    
-    func didClickSkipButton() {
         
-    }
-    
-    func didClickNextButton() {
-        
+    @objc func didClickNextButton() {
+        self.nextButtonCompletionHandler()
     }
     
     func didClickGetStartedButton() {
-        
     }
     
     func showButtons(navigationButtons: [OnboardingNavigationBarBottomButton]) {
