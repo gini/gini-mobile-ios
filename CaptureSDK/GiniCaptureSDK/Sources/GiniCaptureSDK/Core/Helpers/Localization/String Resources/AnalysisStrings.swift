3c2bdf726702209c614c7ba3e728d9638b6823a6
David Vizaknai <david@halcyonmobile.com>
2022-08-26 15:46:20 +0300

    refactor(GiniCaotureSDK) - Remove unused string cases
    PIA-2938

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Helpers/Localization/String Resources/AnalysisStrings.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Helpers/Localization/String Resources/AnalysisStrings.swift
@@ -9,10 +9,7 @@ import Foundation
 
 public enum AnalysisStrings: LocalizableStringResource {
 
-    case analysisErrorMessage, documentCreationErrorMessage, cancelledMessage, loadingText, loadingTextPDF, pdfPages,
-    suggestion1Text, suggestion2Text, suggestion3Text, suggestion4Text, suggestion5Text, suggestionHeader,
-    defaultPdfDokumentTitle, screenTitle,
-    suggestion1Details, suggestion2Details, suggestion3Details, suggestion4Details, suggestion5Details
+    case analysisErrorMessage, documentCreationErrorMessage, cancelledMessage
 
     public var tableName: String {
         return "analysis"
@@ -26,42 +23,6 @@ public enum AnalysisStrings: LocalizableStringResource {
             return ("error.documentCreation", "This message is shown when there is an error creating the document")
         case .cancelledMessage:
             return ("error.cancelled", "This message is shown when the analysis was cancelled")
-        case .loadingText:
-            return ("loadingText", "Text appearing at the center of the analysis screen " +
-            "indicating that the document is being analysed")
-        case .loadingTextPDF:
-            return ("loadingText.pdf", "Text appearing at the center of the analysis screen " +
-            "indicating that a PDF document is being analysed, indicating the title of the document")
-        case .pdfPages:
-            return ("pdfpages",
-                    "Text appearing at the top of the analysis screen indicating pdf number of pages")
-        case .suggestion1Text:
-            return ("suggestion.1", "First suggestion text for analysis screen")
-        case .suggestion2Text:
-            return ("suggestion.2", "Second suggestion text for analysis screen")
-        case .suggestion3Text:
-            return ("suggestion.3", "Third suggestion text for analysis screen")
-        case .suggestion4Text:
-            return ("suggestion.4", "Fourth suggestion text for analysis screen")
-        case .suggestion5Text:
-            return ("suggestion.5", "Fifth suggestion text for analysis screen")
-        case .suggestion1Details:
-            return ("suggestion.1.details", "First suggestion text detail for analysis screen")
-        case .suggestion2Details:
-            return ("suggestion.2.details", "Second suggestion text detail for analysis screen")
-        case .suggestion3Details:
-            return ("suggestion.3.details", "Third suggestion text detail for analysis screen")
-        case .suggestion4Details:
-            return ("suggestion.4.details", "Fourth suggestion text detail for analysis screen")
-        case .suggestion5Details:
-            return ("suggestion.5.details", "Fifth suggestion text detail for analysis screen")
-        case .suggestionHeader:
-            return ("suggestion.header", "Fourth suggestion text for analysis screen")
-        case .screenTitle:
-            return ("screenTitle",
-                    "Text appearing on the top of thje navigation bar as the screen title.")
-        case .defaultPdfDokumentTitle:
-            return ("defaultPdfDokumentTitle", "Default PDF document title")
         }
     }
 
