fc64ec7ba7af743f7c953aeb79b989743a97cb36
Nadya Karaban <nadya@gini.net>
2022-09-02 16:42:02 +0200

    chore(GiniCaptureSDK): review notes
    PIA-2864

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/InjectedViewAdapter.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/InjectedViewAdapter.swift
@@ -8,7 +8,17 @@
 import Foundation
 import UIKit
 
+/**
+ *  Adapter for injectable views. It allows clients to inject their own views into our layouts.
+ */
 public protocol InjectedViewAdapter {
+/**
+ *  Called when the custom view is required. It will be injected into the SDK's layout.
+ */
     func injectedView() -> UIView
+    
+/**
+ *  Called when the view is destroyed/deinitialized.
+ */
     func onDeinit()
 }
