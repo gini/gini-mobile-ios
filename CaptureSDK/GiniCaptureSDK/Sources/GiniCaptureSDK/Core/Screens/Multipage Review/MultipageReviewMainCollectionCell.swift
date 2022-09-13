d14d9faa0088c90864a37bf6b711db476ef846c7
David Vizaknai <david@halcyonmobile.com>
2022-09-13 17:30:47 +0300

    feature(GiniCaptureSDK): Add reuseIdentifier extension
    PIA-2929

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Multipage Review/MultipageReviewMainCollectionCell.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Multipage Review/MultipageReviewMainCollectionCell.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 final class MultipageReviewMainCollectionCell: UICollectionViewCell {
-    
-    static let identifier = "MultipageReviewMainCollectionCellIdentifier"
-    
     lazy var documentImage: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
