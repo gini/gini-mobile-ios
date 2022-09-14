8857eb044f94b7f44b35da342563767ad2f7caf4
Nadya Karaban <nadya@gini.net>
2022-09-14 17:51:02 +0200

    refactor(GiniCaptureSDK): Add struct for onboarding page PIA-2861

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingIllustrationAdapter.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingIllustrationAdapter.swift
@@ -19,3 +19,14 @@ public protocol OnboardingIllustrationAdapter: InjectedViewAdapter {
  */
     func pageDidDisappear()
 }
+
+public struct OnboardingPageNew {
+    let imageName: String
+    let title: String
+    let description: String
+    init(imageName: String, title: String, description: String) {
+        self.imageName = imageName
+        self.title = title
+        self.description = description
+    }
+}
