18079bc9268b8b4ebb3521e6d9f4a37a28051a7a
Nadya Karaban <nadya@gini.net>
2022-09-15 19:00:07 +0200

    refactor(GiniCaptureSDK): OnboardingImageView doesn't confirm to OnboardingIllustrationAdapter protocol but has it instance as a property Move out OnboardingIllustrationAdapter protocol in a separate file PIA-2862, PIA-2261

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingImageView.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingImageView.swift
@@ -6,69 +6,52 @@
 
 import UIKit
 
-class OnboardingImageView: UIView, OnboardingIllustrationAdapter {
-    func onDeinit() {
-    }
+class ImageOnboardingIllustrationAdapter: OnboardingIllustrationAdapter {
     func pageDidAppear() {
     }
     func pageDidDisappear() {
     }
     func injectedView() -> UIView {
-        self
+        let containerView = UIView()
+        return containerView
     }
+    func onDeinit() {
+    }
+}
+
+class OnboardingImageView: UIView {
+var illustrationAdapter: OnboardingIllustrationAdapter?
     var icon: UIImage? {
         didSet {
+            //Remove the previous container with an illustration because we dequeue reusable cells
             self.subviews.forEach({ $0.removeFromSuperview() })
             setupView()
         }
     }
-    // MARK: - Initializers
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-        }
 
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-        }
-    // MARK: - Private Helper Methods
-        private func setupView() {
-            if let image = icon {
-                let imageView = UIImageView()
-                imageView.image = image
-
-                imageView.contentMode = .scaleAspectFit
-
-                imageView.center = CGPoint(x: self.frame.size.width / 2,
-
-                                           y: self.frame.size.height / 2)
-                imageView.fixInView(self)
-
+// MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+// MARK: - Private Helper Methods
+    private func setupView() {
+        if let image = icon {
+            let imageView = UIImageView()
+            imageView.image = image
+            imageView.center = CGPoint(x: self.frame.size.width / 2,
+                                                   y: self.frame.size.height / 2)
+            imageView.contentMode = .scaleAspectFit
+            self.addSubview(imageView)
+            imageView.fixInView(self)
+        } else {
+            if let containerView = illustrationAdapter?.injectedView() {
+                self.addSubview(containerView)
             }
-
         }
-}
-
-extension UIView {
-    func viewFromNibForClass() -> UIView {
-        let bundle = Bundle(for: type(of: self))
-        let nibName = type(of: self).description().components(separatedBy: ".").last!
-        let nib = UINib(nibName: nibName, bundle: bundle)
-        guard let view = nib.instantiate(withOwner: self, options: nil).first, view is UIView else {
-            fatalError("View initializing with nib failed while casting")
-        }
-        return view as? UIView ?? UIView()
     }
 }
 
-extension UIView {
-    func fixInView(_ container: UIView!) {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.frame = container.frame
-        container.addSubview(self)
-        let leadingConstraint = self.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 0)
-        let trailingConstraint = self.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: 0)
-        let topConstraint = self.topAnchor.constraint(equalTo: container.topAnchor, constant: 0)
-        let bottomConstraint = self.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: 0)
-        NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
-    }
-}
+
