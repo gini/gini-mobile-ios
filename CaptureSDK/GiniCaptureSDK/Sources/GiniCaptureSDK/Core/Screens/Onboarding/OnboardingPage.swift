bcc4ad4dd8cf030d0fb2f198b5cbeacf065c81d1
Nadya Karaban <nadya@gini.net>
2022-09-15 18:51:40 +0200

    refactor(GiniCaptureSDK): Move struct for Onboarding page in a separate file Rename old class PIA-2861

---
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingPage.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingPage.swift
+//  
+//
+//  Created by Nadya Karaban on 15.09.22.
+//
+
+import UIKit
+public struct OnboardingPageNew {
+    let imageName: String
+    let title: String
+    let description: String
+    public init(imageName: String, title: String, description: String) {
+        self.imageName = imageName
+        self.title = title
+        self.description = description
+    }
+}
