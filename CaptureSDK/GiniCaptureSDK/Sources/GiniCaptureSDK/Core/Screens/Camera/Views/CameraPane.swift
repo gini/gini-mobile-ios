6efb1c53d9234aa9f0deefe59ef95325500ede7b
Krzysztof Kryniecki <chriskryn@gmail.com>
2022-09-16 16:38:54 +0200

    fix(GiniCaptureSDK): fix margins and display of camera pane buttons
    PIA-2918

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/Views/CameraPane.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/Views/CameraPane.swift
@@ -13,26 +13,60 @@ class CameraPane: UIView {
     @IBOutlet weak var fileUploadButton: BottomLabelButton!
     @IBOutlet weak var flashButton: BottomLabelButton!
     @IBOutlet weak var thumbnailView: ThumbnailView!
-    let giniConfiguration: GiniConfiguration! = nil
+    var giniConfiguration: GiniConfiguration! = nil
 
     func configureView(giniConfiguration: GiniConfiguration) {
+        self.giniConfiguration = giniConfiguration
         thumbnailView.isHidden = true
         fileUploadButton.configureButton(
             image: UIImageNamedPreferred(
                 named: "folder") ?? UIImage(),
             name: NSLocalizedStringPreferredFormat(
             "ginicapture.camera.fileImportButtonLabel",
-            comment: "Import photo"),
-            giniconfiguration: giniConfiguration)
+            comment: "Import photo"))
         flashButton.configureButton(
             image: UIImageNamedPreferred(named: "flashOff") ?? UIImage(),
             name: NSLocalizedStringPreferredFormat(
             "ginicapture.camera.flashButtonLabel",
-            comment: "Flash button"),
-            giniconfiguration: giniConfiguration)
-        flashButton.iconView.image = UIImageNamedPreferred(named: "flashOn")
+            comment: "Flash button"))
+        flashButton.iconView.image = UIImageNamedPreferred(named: "flashOff")
+        flashButton.actionLabel.font = giniConfiguration.textStyleFonts[.caption1]
+        flashButton.actionLabel.textColor = GiniColor(
+            light: UIColor.GiniCapture.light1,
+            dark: UIColor.GiniCapture.light1).uiColor()
+        fileUploadButton.actionLabel.textColor = GiniColor(
+            light: UIColor.GiniCapture.light1,
+            dark: UIColor.GiniCapture.light1).uiColor()
+        fileUploadButton.actionLabel.font = giniConfiguration.textStyleFonts[.caption1]
+        if cameraTitleLabel != nil {
+            cameraTitleLabel.adjustsFontForContentSizeCategory = true
+            cameraTitleLabel.font = giniConfiguration.textStyleFonts[.footnote]
+            cameraTitleLabel.textColor = GiniColor(
+                light: UIColor.GiniCapture.light1,
+                dark: UIColor.GiniCapture.light1).uiColor()
+        }
     }
 
+    func setupFlashButton(state: Bool) {
+        if state {
+            flashButton.configureButton(
+                image: UIImageNamedPreferred(named: "flashOn") ?? UIImage(),
+                name: NSLocalizedStringPreferredFormat(
+                "ginicapture.camera.flashButtonLabel.On",
+                comment: "Flash button"))
+        } else {
+            flashButton.configureButton(
+                image: UIImageNamedPreferred(named: "flashOff") ?? UIImage(),
+                name: NSLocalizedStringPreferredFormat(
+                "ginicapture.camera.flashButtonLabel.Off",
+                comment: "Flash button"))
+        }
+    }
+
+    func toggleFlashButtonActivation(state: Bool) {
+        flashButton.isHidden = !state
+    }
+    
     func toggleCaptureButtonActivation(state: Bool) {
         captureButton.isUserInteractionEnabled = state
         captureButton.isEnabled = state
