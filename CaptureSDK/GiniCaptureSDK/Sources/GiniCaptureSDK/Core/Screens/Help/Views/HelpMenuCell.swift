99c2a339301bdb51876a9c9fc274cb83144f725c
Krzysztof Kryniecki <chriskryn@gmail.com>
2022-09-28 13:00:21 +0200

    feat(GiniCaptureSDK): add custom cell type for help menu screen
    PIA-3134

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/Views/HelpMenuCell.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/Views/HelpMenuCell.swift
@@ -9,4 +9,31 @@ import UIKit
 
 public class HelpMenuCell: UITableViewCell, HelpCell {
     public static var reuseIdentifier: String = "kHelpMenuCell"
+    weak var separatorView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        let separator = UIView()
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(separator)
+        separatorView = separator
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        if let separatorView {
+            NSLayoutConstraint.activate([
+                separatorView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+                separatorView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+                separatorView.heightAnchor.constraint(equalToConstant: 1),
+                separatorView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -1)
+            ])
+        }
+        
+    }
 }
