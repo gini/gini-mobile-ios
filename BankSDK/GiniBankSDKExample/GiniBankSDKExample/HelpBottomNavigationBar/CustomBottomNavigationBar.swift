1867f99ba1a33b92d56188901e6648331710e0aa
Krzysztof Kryniecki <chriskryn@gmail.com>
2022-10-10 12:15:45 +0200

    chore(GiniCaptureSDK): setup custom bottom bar implementation for both help screen and no result
    PIA-2949

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/HelpBottomNavigationBar/CustomBottomNavigationBar.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/HelpBottomNavigationBar/CustomBottomNavigationBar.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CustomHelpBottomNavigationBar: UIView {
+class CustomBottomNavigationBar: UIView {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var backButton: UIButton!
