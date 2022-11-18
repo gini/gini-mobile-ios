6cffd3c4ff44b955f5c207cef69d5bcd73dd14cb
David Vizaknai <david@halcyonmobile.com>
2022-11-18 17:16:11 +0200

    feature(GinICaptureSDK): Change no camera access screen design
    PIA-3150

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/Views/CameraNotAuthorizedView.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/Views/CameraNotAuthorizedView.swift
@@ -7,64 +7,87 @@
 //
 
 import UIKit
+import AVFoundation
 
 final class CameraNotAuthorizedView: UIView {
-
     // User interface
-    fileprivate var label = UILabel()
-    fileprivate var button = UIButton()
-    fileprivate var imageView = UIImageView()
-    fileprivate var contentView = UIView()
-
-    // Images
-    fileprivate var noCameraImage: UIImage? {
-        return UIImageNamedPreferred(named: "cameraNotAuthorizedIcon")
-    }
-
-    init(giniConfiguration: GiniConfiguration = GiniConfiguration.shared) {
-        super.init(frame: CGRect.zero)
-        // Configure image view
-        imageView.image = noCameraImage
-        imageView.contentMode = .scaleAspectFit
-
-        // Configure label
-        label.text = NSLocalizedStringPreferredFormat(
-            "ginicapture.camera.notAuthorized",
-            comment: "Not authorized text")
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = NSLocalizedStringPreferredFormat("ginicapture.camera.notAuthorized.title",
+                                                          comment: "Not authorized title")
         label.numberOfLines = 0
-        label.textColor = GiniColor(light: UIColor.GiniCapture.light1, dark: UIColor.GiniCapture.light1).uiColor()
+        label.textColor = GiniColor(light: UIColor.GiniCapture.dark1, dark: UIColor.GiniCapture.light1).uiColor()
         label.textAlignment = .center
-        label.font = giniConfiguration.textStyleFonts[.title2]
+        label.font = configuration.textStyleFonts[.title2]
+        return label
+    }()
 
-        // Configure button
-        button.setTitle(
-            NSLocalizedStringPreferredFormat(
-                "ginicapture.camera.notAuthorizedButton", comment: "Grant permission"),
-            for: .normal)
-        button.setTitleColor(
-            GiniColor(
-                light: UIColor.GiniCapture.accent1,
-                dark: UIColor.GiniCapture.accent1
-            ).uiColor(),
-            for: .normal)
-        button.setTitleColor(
-            GiniColor(
-                light: UIColor.GiniCapture.accent1,
-                dark: UIColor.GiniCapture.accent1
-            ).uiColor().withAlphaComponent(0.8),
-            for: .highlighted)
-        button.titleLabel?.font = giniConfiguration.textStyleFonts[.subheadline]
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = NSLocalizedStringPreferredFormat("ginicapture.camera.notAuthorized.description",
+                                                          comment: "Not authorized description")
+        label.numberOfLines = 0
+        label.textColor = UIColor.GiniCapture.dark7
+        label.textAlignment = .center
+        label.font = configuration.textStyleFonts[.headline]
+        return label
+    }()
 
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImageNamedPreferred(named: "cameraNotAuthorizedIcon")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle(NSLocalizedStringPreferredFormat("ginicapture.camera.notAuthorizedButton.noStatus",
+                                                         comment: "Grant permission"), for: .normal)
+        button.setTitleColor(GiniColor(light: UIColor.GiniCapture.accent1,
+                                       dark: UIColor.GiniCapture.accent1).uiColor(), for: .normal)
+        button.setTitleColor(GiniColor(light: UIColor.GiniCapture.accent1,
+                                       dark: UIColor.GiniCapture.accent1).uiColor().withAlphaComponent(0.8),
+                             for: .highlighted)
+        button.titleLabel?.font = configuration.textStyleFonts[.subheadline]
         button.addTarget(self, action: #selector(openSettings), for: .touchUpInside)
+        return button
+    }()
+
+    private var contentView = UIView()
+
+    private let configuration = GiniConfiguration.shared
+
+    init() {
+        super.init(frame: CGRect.zero)
+
+        backgroundColor = GiniColor(light: UIColor.GiniCapture.light2, dark: UIColor.GiniCapture.dark2).uiColor()
 
         // Configure view hierachy
         addSubview(contentView)
         contentView.addSubview(imageView)
-        contentView.addSubview(label)
-        contentView.addSubview(button)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+        addSubview(button)
 
         // Add constraints
         addConstraints()
+
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            // This is not happening
+            break
+        case .notDetermined:
+            // Change button title
+            button.setTitle(NSLocalizedStringPreferredFormat("ginicapture.camera.notAuthorizedButton.noStatus",
+                                                             comment: "Give access"), for: .normal)
+        case .denied, .restricted:
+            // Change button title
+            button.setTitle(NSLocalizedStringPreferredFormat("ginicapture.camera.notAuthorizedButton.denied",
+                                                             comment: "Give access"), for: .normal)
+        @unknown default:
+            break
+        }
     }
 
     /**
@@ -76,7 +99,8 @@ final class CameraNotAuthorizedView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    @IBAction func openSettings(_ sender: AnyObject) {
+    @objc
+    private func openSettings() {
         UIApplication.shared.openAppSettings()
     }
 
@@ -84,33 +108,45 @@ final class CameraNotAuthorizedView: UIView {
     fileprivate func addConstraints() {
         contentView.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        label.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
         button.translatesAutoresizingMaskIntoConstraints = false
+
         NSLayoutConstraint.activate([
-            contentView.topAnchor.constraint(greaterThanOrEqualTo: topAnchor, constant: 30),
-            contentView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 5),
-            contentView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
-            contentView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 10),
+            contentView.topAnchor.constraint(greaterThanOrEqualTo: topAnchor, constant: Constants.padding * 2),
+            contentView.bottomAnchor.constraint(equalTo: centerYAnchor),
+            contentView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.padding),
             // Image view
             imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            imageView.widthAnchor.constraint(lessThanOrEqualToConstant: 204),
-            imageView.widthAnchor.constraint(greaterThanOrEqualToConstant: 75),
-            imageView.heightAnchor.constraint(greaterThanOrEqualToConstant: 50),
-            imageView.heightAnchor.constraint(lessThanOrEqualToConstant: 75),
+            imageView.widthAnchor.constraint(equalToConstant: Constants.imageSize.width),
+            imageView.heightAnchor.constraint(equalToConstant: Constants.imageSize.height),
             imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            // label
-            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 35),
-            label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            label.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.7),
+            // titleLabel
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: Constants.padding * 2),
+            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor,
+                                              multiplier: Constants.widthCoefficient),
+            // descriptionLabel
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Constants.padding),
+            descriptionLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            descriptionLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor,
+                                                    multiplier: Constants.widthCoefficient),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             // button
-            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 10),
-            button.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            button.widthAnchor.constraint(greaterThanOrEqualTo: label.widthAnchor),
-            button.heightAnchor.constraint(equalToConstant: 35),
-            button.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor, constant: 4),
-            button.trailingAnchor.constraint(greaterThanOrEqualTo: trailingAnchor, constant: 4),
-            button.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
-        ])
+            button.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor,
+                                           constant: -Constants.padding * 2),
+            button.widthAnchor.constraint(greaterThanOrEqualTo: descriptionLabel.widthAnchor),
+            button.heightAnchor.constraint(equalToConstant: Constants.padding * 4),
+            button.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor, constant: Constants.padding),
+            button.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)])
     }
+}
 
+extension CameraNotAuthorizedView {
+    private enum Constants {
+        static let padding: CGFloat = 16
+        static let widthCoefficient: CGFloat = 0.7
+        static let imageSize: CGSize = CGSize(width: 50, height: 50)
+    }
 }
