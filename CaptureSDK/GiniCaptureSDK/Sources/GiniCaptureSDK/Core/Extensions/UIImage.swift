0a5715d8f9cdd70005f759fec4e7582d23a335ab
David Vizaknai <david@halcyonmobile.com>
2022-11-24 10:33:04 +0200

    feature(GiniCaptureSDK): Increase cropped area by 20%
    PIA-3347

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Extensions/UIImage.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Extensions/UIImage.swift
@@ -156,4 +156,18 @@ extension UIImage {
         // If something failed -- return original
         return self
     }
+
+    // Used to merge the canvas for cropping with the original image
+    func overlayWith(image: UIImage, posX: CGFloat, posY: CGFloat) -> UIImage {
+          let newWidth = size.width < posX + image.size.width ? posX + image.size.width : size.width
+          let newHeight = size.height < posY + image.size.height ? posY + image.size.height : size.height
+          let newSize = CGSize(width: newWidth, height: newHeight)
+
+          UIGraphicsBeginImageContextWithOptions(newSize, false, 1)
+          draw(in: CGRect(origin: CGPoint.zero, size: size))
+          image.draw(in: CGRect(origin: CGPoint(x: posX, y: posY), size: image.size))
+          let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+          UIGraphicsEndImageContext()
+          return newImage
+    }
 }
