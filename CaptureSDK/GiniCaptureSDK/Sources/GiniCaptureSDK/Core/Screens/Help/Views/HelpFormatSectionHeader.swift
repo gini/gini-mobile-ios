290bdcbcb1a74057d0df2acfb553ec9e3085c641
David Vizaknai <david@halcyonmobile.com>
2022-12-01 12:02:26 +0200

    fix(GiniCaptureSDK): Reverse commit
    PIA-3311

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/Views/HelpFormatSectionHeader.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/Views/HelpFormatSectionHeader.swift
@@ -12,12 +12,4 @@ import UIKit
 final class HelpFormatSectionHeader: UITableViewHeaderFooterView {
     @IBOutlet weak var titleLabel: UILabel!
     static var reuseIdentifier: String = "kHelpFormatSectionHeader"
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
-        DispatchQueue.main.async {
-            self.titleLabel.textColor = GiniColor(light: .GiniCapture.dark1, dark: .GiniCapture.light1).uiColor()
-        }
-    }
 }
