e62c2632de360df2660161cacb783b9e3e33444b
David Vizaknai <david@halcyonmobile.com>
2023-01-10 10:26:52 +0200

    feature(GiniCaptureSDK): Onboarding page documentation
    PIA-2869

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingPage.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingPage.swift
@@ -7,10 +7,23 @@
 
 import Foundation
 
+/**
+ `OnboardingPageNew` represents the onboarding page with all it's properties.
+ */
+
 public struct OnboardingPageNew {
     let imageName: String
     let title: String
     let description: String
+
+    /**
+     *  Creates an `OnboardingPageNew` instance.
+     *
+     * - Parameter imageName:   a String representing the name of an image associated with the onboarding page.
+     * - Parameter title:       a String representing the title of the onboarding page
+     * - Parameter description: a String representing a short description of the onboarding page
+     */
+
     public init(imageName: String, title: String, description: String) {
         self.imageName = imageName
         self.title = title
