af85102c5c4469487ee6fba3a0edda0b491b7e9f
Nadya Karaban <nadya@gini.net>
2023-01-16 15:25:07 +0100

    chore(GiniBankAPILibrary):  Bumps version 3.0.0-beta02

--- BankAPILibrary/GiniBankAPILibrary/Tests/GiniBankAPILibraryTests/DocumentServiceTests.swift
+++ BankAPILibrary/GiniBankAPILibrary/Tests/GiniBankAPILibraryTests/DocumentServiceTests.swift
@@ -179,7 +179,7 @@ final class DocumentServicesTests: XCTestCase {
                                     osName: UIDevice.current.systemName,
                                     osVersion: UIDevice.current.systemVersion,
                                     captureSdkVersion: "Not available",
-                                    apiLibVersion: "3.0.0-beta01",
+                                    apiLibVersion: "3.0.0-beta02",
                                     description: "Error logging test",
                                     documentId: "1234",
                                     originalRequestId: "5678")
