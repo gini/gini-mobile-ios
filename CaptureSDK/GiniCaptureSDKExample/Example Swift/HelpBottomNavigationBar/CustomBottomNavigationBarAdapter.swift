9c9f01f273f07eb2dc61fc07fab27a4b86b884c4
David Vizaknai <david@halcyonmobile.com>
2023-02-06 10:52:53 +0200

    feature(GiniCaptureSDK): Add custom bottom navigation bar to capture example
    PIA-3655

---
+++ CaptureSDK/GiniCaptureSDKExample/Example Swift/HelpBottomNavigationBar/CustomBottomNavigationBarAdapter.swift
@@ -0,0 +1,37 @@
+//
+//  CustomHelpBottomNavigationBarAdapter.swift
+//  GiniBankSDKExample
+//
+//  Created by Krzysztof Kryniecki on 07/10/2022.
+//
+
+import UIKit
+import GiniCaptureSDK
+
+final class CustomBottomNavigationBarAdapter: NoResultBottomNavigationBarAdapter, HelpBottomNavigationBarAdapter, ImagePickerBottomNavigationBarAdapter, ErrorBottomNavigationBarAdapter {
+    private var backButtonCallback: (() -> Void)?
+
+    func setBackButtonClickedActionCallback(_ callback: @escaping () -> Void) {
+        backButtonCallback = callback
+    }
+
+    func injectedView() -> UIView {
+        if let navigationBarView = CustomBottomNavigationBar().loadNib() as? CustomBottomNavigationBar {
+            navigationBarView.backButton.addTarget(
+                self,
+                action: #selector(backButtonClicked),
+                for: .touchUpInside)
+            return navigationBarView
+        } else {
+            return UIView()
+        }
+    }
+
+    @objc func backButtonClicked() {
+        backButtonCallback?()
+    }
+
+    func onDeinit() {
+        backButtonCallback = nil
+    }
+}
