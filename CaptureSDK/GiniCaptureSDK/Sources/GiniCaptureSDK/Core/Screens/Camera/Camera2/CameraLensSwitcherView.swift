8afa7ca4f1f384c514b7ec633d45979b80cea137
David Vizaknai <david.vizaknai@lateralgroup.com>
2023-03-24 13:07:54 +0200

    feature(GiniCaptureSDK): Add lens switcher based on available lenses
    PIA-3445

---
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/Camera2/CameraLensSwitcherView.swift
@@ -0,0 +1,117 @@
+//
+//  CameraLensSwitcherView.swift
+//  
+//
+//  Created by David Vizaknai on 24.03.2023.
+//
+
+import UIKit
+
+enum CameraLensesAvailable {
+    case ultraWide
+    case wide
+    case tele
+}
+
+final class CameraLensSwitcherView: UIView {
+    private lazy var buttonContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        return view
+    }()
+
+    private lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fill
+        stackView.axis = .horizontal
+        stackView.spacing = 4
+        stackView.layer.cornerRadius = 12
+        stackView.backgroundColor = .GiniCapture.dark1.withAlphaComponent(0.3)
+        return stackView
+    }()
+
+    private lazy var ultraWideButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.GiniCapture.light1, for: .normal)
+        button.setTitle("0.5", for: .normal)
+        button.backgroundColor = .GiniCapture.dark4.withAlphaComponent(0.3)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 18
+        return button
+    }()
+
+    private lazy var wideButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.GiniCapture.warning1, for: .normal)
+        button.setTitle("1x", for: .normal)
+        button.backgroundColor = .GiniCapture.dark4.withAlphaComponent(0.3)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 18
+        return button
+    }()
+
+    private lazy var teleButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.GiniCapture.light1, for: .normal)
+        button.setTitle("2x", for: .normal)
+        button.backgroundColor = .GiniCapture.dark4.withAlphaComponent(0.3)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 18
+        return button
+    }()
+
+    private var selectedLens: CameraLensesAvailable = .wide
+    private let availableLenses: [CameraLensesAvailable]
+
+    init(availableLenses: [CameraLensesAvailable]) {
+        self.availableLenses = availableLenses
+        super.init(frame: .zero)
+        setupView()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupView() {
+        addSubview(buttonContainerView)
+        buttonContainerView.addSubview(buttonsStackView)
+
+        buttonsStackView.addArrangedSubview(UIView())
+
+        if availableLenses.contains(.ultraWide) {
+            buttonsStackView.addArrangedSubview(ultraWideButton)
+        }
+
+        if availableLenses.contains(.wide) {
+            buttonsStackView.addArrangedSubview(wideButton)
+        }
+
+        if availableLenses.contains(.tele) {
+            buttonsStackView.addArrangedSubview(teleButton)
+        }
+
+        buttonsStackView.addArrangedSubview(UIView())
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            teleButton.widthAnchor.constraint(equalTo: teleButton.heightAnchor),
+            ultraWideButton.widthAnchor.constraint(equalTo: ultraWideButton.heightAnchor),
+            wideButton.widthAnchor.constraint(equalTo: wideButton.heightAnchor),
+
+            buttonContainerView.topAnchor.constraint(equalTo: topAnchor),
+            buttonContainerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            buttonContainerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            buttonContainerView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            buttonsStackView.centerXAnchor.constraint(equalTo: buttonContainerView.centerXAnchor),
+            buttonsStackView.topAnchor.constraint(equalTo: buttonContainerView.topAnchor),
+            buttonsStackView.bottomAnchor.constraint(equalTo: buttonContainerView.bottomAnchor),
+            buttonsStackView.leadingAnchor.constraint(greaterThanOrEqualTo: buttonContainerView.leadingAnchor)
+        ])
+    }
+}
