0001cf64d77e3c9e6bfda4964df8bd425f28c975
David Vizaknai <david.vizaknai@lateralgroup.com>
2023-03-27 09:45:08 +0300

    fix(GiniCaptureSDK): Remove BodyBold textstyle for barbuttons
    PIA-3889

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Custom views/BackButtonBottomNavigationBar.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Custom views/BackButtonBottomNavigationBar.swift
@@ -17,8 +17,7 @@ final class BackButtonBottomNavigationBar: UIView {
 
     /// The button that displays the back arrow icon.
     @IBOutlet weak var backButtonContainer: UIView!
-    let backButton = GiniBarButton(ofType: .back(title: "    "),
-                                   isForBottomNavigation: true)
+    let backButton = GiniBarButton(ofType: .back(title: "    "))
 
     public override func awakeFromNib() {
         super.awakeFromNib()
