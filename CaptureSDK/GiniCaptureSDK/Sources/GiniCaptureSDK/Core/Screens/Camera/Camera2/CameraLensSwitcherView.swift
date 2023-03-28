163c279aac9df29f42bfefdaed957792d9049257
David Vizaknai <david.vizaknai@lateralgroup.com>
2023-03-28 16:00:25 +0300

    feature(GiniCaptureSDK): Add animation and new ui for lens switcher
    PIA-3445

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/Camera2/CameraLensSwitcherView.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/Camera2/CameraLensSwitcherView.swift
@@ -21,25 +21,16 @@ final class CameraLensSwitcherView: UIView {
     private lazy var buttonContainerView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .clear
+        view.backgroundColor = .GiniCapture.dark1.withAlphaComponent(Constants.inactiveStateAlpha)
+        view.layer.cornerRadius = Constants.containerRadius
         return view
-    }()
-
-    private lazy var buttonsStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.distribution = .fill
-        stackView.axis = UIDevice.current.isIpad ? .vertical : .horizontal
-        stackView.spacing = Constants.stackViewSpacing
-        stackView.layer.cornerRadius = Constants.cornerRadius
-        stackView.backgroundColor = .GiniCapture.dark1.withAlphaComponent(Constants.inactiveStateAlpha)
-        return stackView
     }()
 
     private lazy var ultraWideButton: UIButton = {
         let button = UIButton()
         button.setTitleColor(.GiniCapture.light1, for: .normal)
         button.setTitle("0.5", for: .normal)
+        button.titleLabel?.font = GiniConfiguration.shared.textStyleFonts[.caption2]
         button.backgroundColor = .GiniCapture.dark4.withAlphaComponent(Constants.inactiveStateAlpha)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.layer.cornerRadius = Constants.cornerRadius
@@ -50,9 +41,10 @@ final class CameraLensSwitcherView: UIView {
         let button = UIButton()
         button.setTitleColor(.GiniCapture.light1, for: .normal)
         button.setTitle("1x", for: .normal)
+        button.titleLabel?.font = GiniConfiguration.shared.textStyleFonts[.caption2]
         button.backgroundColor = .GiniCapture.dark4.withAlphaComponent(Constants.inactiveStateAlpha)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = Constants.cornerRadius
+        button.layer.cornerRadius = 18
         return button
     }()
 
@@ -60,6 +52,7 @@ final class CameraLensSwitcherView: UIView {
         let button = UIButton()
         button.setTitleColor(.GiniCapture.light1, for: .normal)
         button.setTitle("2x", for: .normal)
+        button.titleLabel?.font = GiniConfiguration.shared.textStyleFonts[.caption2]
         button.backgroundColor = .GiniCapture.dark4.withAlphaComponent(Constants.inactiveStateAlpha)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.layer.cornerRadius = Constants.cornerRadius
@@ -90,56 +83,114 @@ final class CameraLensSwitcherView: UIView {
 
     private func setupView() {
         addSubview(buttonContainerView)
-        buttonContainerView.addSubview(buttonsStackView)
-
-        buttonsStackView.addArrangedSubview(UIView())
 
         if availableLenses.contains(.ultraWide) {
             ultraWideButton.addTarget(self, action: #selector(ultraWideButtonTapped), for: .touchUpInside)
-            buttonsStackView.addArrangedSubview(ultraWideButton)
+            buttonContainerView.addSubview(ultraWideButton)
         }
 
         if availableLenses.contains(.wide) {
             setButtonState(for: wideButton)
             wideButton.addTarget(self, action: #selector(wideButtonTapped), for: .touchUpInside)
-            buttonsStackView.addArrangedSubview(wideButton)
+            buttonContainerView.addSubview(wideButton)
         }
 
         if availableLenses.contains(.tele) {
             teleButton.addTarget(self, action: #selector(teleButtonTapped), for: .touchUpInside)
-            buttonsStackView.addArrangedSubview(teleButton)
+            buttonContainerView.addSubview(teleButton)
         }
-
-        buttonsStackView.addArrangedSubview(UIView())
     }
+
+    private lazy var ultraWideButtonHeightConstraint = ultraWideButton.heightAnchor.constraint(equalToConstant: 24)
+    private lazy var wideButtonHeightConstraint = wideButton.heightAnchor.constraint(equalToConstant: 24)
+    private lazy var teleButtonHeightConstraint = teleButton.heightAnchor.constraint(equalToConstant: 24)
 
     private func setupConstraints() {
         NSLayoutConstraint.activate([
+            ultraWideButtonHeightConstraint,
+            wideButtonHeightConstraint,
+            teleButtonHeightConstraint,
             teleButton.widthAnchor.constraint(equalTo: teleButton.heightAnchor),
             ultraWideButton.widthAnchor.constraint(equalTo: ultraWideButton.heightAnchor),
             wideButton.widthAnchor.constraint(equalTo: wideButton.heightAnchor),
 
-            buttonContainerView.topAnchor.constraint(equalTo: topAnchor),
-            buttonContainerView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            buttonContainerView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            buttonContainerView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            ultraWideButton.centerYAnchor.constraint(equalTo: buttonContainerView.centerYAnchor),
+            ultraWideButton.leadingAnchor.constraint(equalTo: buttonContainerView.leadingAnchor, constant: 8),
+            ultraWideButton.topAnchor.constraint(greaterThanOrEqualTo: buttonContainerView.topAnchor, constant: 3),
+            ultraWideButton.bottomAnchor.constraint(lessThanOrEqualTo: buttonContainerView.bottomAnchor, constant: -3),
 
-            buttonsStackView.centerXAnchor.constraint(equalTo: buttonContainerView.centerXAnchor),
-            buttonsStackView.topAnchor.constraint(equalTo: buttonContainerView.topAnchor),
-            buttonsStackView.bottomAnchor.constraint(equalTo: buttonContainerView.bottomAnchor),
-            buttonsStackView.leadingAnchor.constraint(greaterThanOrEqualTo: buttonContainerView.leadingAnchor)
+            wideButton.centerYAnchor.constraint(equalTo: buttonContainerView.centerYAnchor),
+            wideButton.leadingAnchor.constraint(equalTo: ultraWideButton.trailingAnchor, constant: 8),
+            wideButton.topAnchor.constraint(greaterThanOrEqualTo: buttonContainerView.topAnchor, constant: 3),
+            wideButton.bottomAnchor.constraint(lessThanOrEqualTo: buttonContainerView.bottomAnchor, constant: -3),
+
+            teleButton.centerYAnchor.constraint(equalTo: buttonContainerView.centerYAnchor),
+            teleButton.leadingAnchor.constraint(equalTo: wideButton.trailingAnchor, constant: 8),
+            teleButton.topAnchor.constraint(greaterThanOrEqualTo: buttonContainerView.topAnchor, constant: 3),
+            teleButton.bottomAnchor.constraint(lessThanOrEqualTo: buttonContainerView.bottomAnchor, constant: -3),
+            teleButton.trailingAnchor.constraint(equalTo: buttonContainerView.trailingAnchor, constant: -8),
+
+            buttonContainerView.topAnchor.constraint(equalTo: topAnchor),
+            buttonContainerView.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor),
+            buttonContainerView.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor),
+            buttonContainerView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
 
     private func setButtonState(for button: UIButton) {
+        NSLayoutConstraint.deactivate([
+            ultraWideButtonHeightConstraint,
+            wideButtonHeightConstraint,
+            teleButtonHeightConstraint
+        ])
+
+        ultraWideButtonHeightConstraint.constant = 24
+        wideButtonHeightConstraint.constant = 24
+        teleButtonHeightConstraint.constant = 24
+
+        ultraWideButton.setTitle("0.5", for: .normal)
+        wideButton.setTitle("1", for: .normal)
+        teleButton.setTitle("2", for: .normal)
+
+        UIViewPropertyAnimator(duration: 0.3, curve: .easeIn) { [weak self] in
+            self?.ultraWideButton.layer.cornerRadius = 12
+            self?.wideButton.layer.cornerRadius = 12
+            self?.teleButton.layer.cornerRadius = 12
+        }.startAnimation()
+
+        switch selectedLens {
+        case .ultraWide:
+            ultraWideButton.setTitle("0.5x", for: .normal)
+            ultraWideButtonHeightConstraint.constant = 34
+        case .wide:
+            wideButton.setTitle("1x", for: .normal)
+            wideButtonHeightConstraint.constant = 34
+        case .tele:
+            teleButton.setTitle("2x", for: .normal)
+            teleButtonHeightConstraint.constant = 34
+        }
+
+        NSLayoutConstraint.activate([
+            ultraWideButtonHeightConstraint,
+            wideButtonHeightConstraint,
+            teleButtonHeightConstraint
+        ])
+
         button.setTitleColor(.yellow, for: .normal)
-        button.backgroundColor = .GiniCapture.dark4.withAlphaComponent(Constants.activeStateAlpha)
+        button.backgroundColor = .GiniCapture.dark1.withAlphaComponent(Constants.activeStateAlpha)
+
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            self?.layoutIfNeeded()
+        }
+        UIViewPropertyAnimator(duration: 0.3, curve: .easeIn) {
+            button.layer.cornerRadius = 18
+        }.startAnimation()
     }
 
     private func resetButtonsState() {
         [ultraWideButton, wideButton, teleButton].forEach { button in
             button.setTitleColor(.GiniCapture.light1, for: .normal)
-            button.backgroundColor = .GiniCapture.dark4.withAlphaComponent(Constants.inactiveStateAlpha)
+            button.backgroundColor = .GiniCapture.dark1.withAlphaComponent(Constants.inactiveStateAlpha)
         }
     }
 
@@ -173,9 +224,10 @@ final class CameraLensSwitcherView: UIView {
 
 private extension CameraLensSwitcherView {
     enum Constants {
-        static let cornerRadius: CGFloat = 18
+        static let containerRadius: CGFloat = 20
+        static let cornerRadius: CGFloat = 12
         static let stackViewSpacing: CGFloat = 4
-        static let inactiveStateAlpha: CGFloat = 0.3
+        static let inactiveStateAlpha: CGFloat = 0.24
         static let activeStateAlpha: CGFloat = 0.8
     }
 }
