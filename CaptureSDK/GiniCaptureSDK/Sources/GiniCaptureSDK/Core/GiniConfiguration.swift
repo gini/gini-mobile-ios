4763a6967e00fd758aac248b43b59454692b0efb
David Vizaknai <david.vizaknai@lateralgroup.com>
2023-04-10 12:13:09 +0300

    fix(GiniCaptureSDK): Remove deprecated function
    PIA-3014

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/GiniConfiguration.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/GiniConfiguration.swift
@@ -232,10 +232,6 @@ import GiniBankAPILibrary
     
     /**
      Indicates the status bar style in the Gini Capture SDK.
-     
-     - note: If `UIViewControllerBasedStatusBarAppearance` is set to `false` in the `Info.plist`,
-     it may not work in future versions of iOS since the `UIApplication.setStatusBarStyle` method was
-     deprecated on iOS 9.0.
      */
     @objc public var statusBarStyle = UIStatusBarStyle.lightContent
     
