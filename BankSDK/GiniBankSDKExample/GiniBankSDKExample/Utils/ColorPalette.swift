20430b6190d008aee6f23b386e1cbf793f4dda18
Valentina Iancu <valentina.iancu@gini.net>
2023-06-14 10:12:44 +0200

    feat(GiniBankSDK): Add `ColorPalette` a place where all the colors should be stored
    PIA-4048

---
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/Utils/ColorPalette.swift
@@ -0,0 +1,12 @@
+//
+//  ColorPalette.swift
+//  GiniBankSDKExample
+//
+//  Created by Valentina Iancu on 13.06.23.
+//
+
+import UIKit
+
+enum ColorPalette {
+	static let raspberryPunch = UIColor(named: "raspberry-punch")
+}
