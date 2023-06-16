af0bb5492c54cd4b6909ebb74d9dd2acd5eecbfe
Nadya Karaban <nadya@gini.net>
2023-06-16 11:56:29 +0200

    fix(GiniCaptureSDK): Call the result of `startAnalysis` on main thread.
    PIA-4271

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Networking/GiniNetworkingScreenAPICoordinator.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Networking/GiniNetworkingScreenAPICoordinator.swift
@@ -134,12 +134,12 @@ import GiniBankAPILibrary
 extension GiniNetworkingScreenAPICoordinator {
     fileprivate func startAnalysis(networkDelegate: GiniCaptureNetworkDelegate) {
         self.documentService.startAnalysis { result in
-            switch result {
-            case .success(let extractions):
-                self.deliver(result: extractions, and: self.documentService.document, to: networkDelegate)
-            case .failure(let error):
-                guard error != .requestCancelled else { return }
-                DispatchQueue.main.async {
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let extractions):
+                    self.deliver(result: extractions, and: self.documentService.document, to: networkDelegate)
+                case .failure(let error):
+                    guard error != .requestCancelled else { return }
                     networkDelegate.displayError(errorType: ErrorType(error: error), animated: true)
                 }
             }
