a9c74f88a3f19fc309248c08f1c9bf746439cc8f
Valentina Iancu <valentina.iancu@gini.net>
2023-06-30 16:31:22 +0200

    chore(GiniBankSDK): Rename file
    PIA-4098

---
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/UIView+LoadNib.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+LoadNib.swift
+//  
+//
+//  Created by David Vizaknai on 22.02.2023.
+//
+
+import UIKit
+
+// swiftlint:disable force_cast
+extension UIView {
+    /** Loads instance from nib with the same name. */
+    func loadNib() -> UIView {
+        let bundle = giniBankBundle()
+        let nibName = type(of: self).description().components(separatedBy: ".").last!
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as! UIView
+    }
+}
