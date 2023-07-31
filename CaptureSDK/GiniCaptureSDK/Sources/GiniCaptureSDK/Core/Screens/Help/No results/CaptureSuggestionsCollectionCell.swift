9b4c5f179a6c6b7725e61a4db69d39b1fccb3080
Nadya Karaban <nadya@gini.net>
2023-07-31 13:54:35 +0200

    refactor(GiniCaptureSDK): Used anchor constraints style
    PIA-4391

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/No results/CaptureSuggestionsCollectionCell.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Help/No results/CaptureSuggestionsCollectionCell.swift
@@ -38,20 +38,21 @@ final class CaptureSuggestionsCollectionCell: UICollectionViewCell {
     }
 
     private func addConstraints() {
-        Constraints.active(item: suggestionImage, attr: .top, relatedBy: .equal, to: contentView, attr: .top, priority: 999)
-        Constraints.active(item: suggestionImage, attr: .bottom, relatedBy: .equal, to: contentView, attr: .bottom, priority: 999)
-        Constraints.active(item: suggestionImage, attr: .leading, relatedBy: .equal, to: contentView, attr: .leading,
-                           constant: Constants.horizontalInset)
-        Constraints.active(item: suggestionImage, attr: .trailing, relatedBy: .equal, to: suggestionText,
-                           attr: .leading, constant: -Constants.horizontalInset)
-        Constraints.active(item: suggestionImage, attr: .width, relatedBy: .equal, to: nil, attr: .notAnAttribute,
-                           constant: Constants.suggestionImageWidth)
-        Constraints.active(item: suggestionImage, attr: .height, relatedBy: .lessThanOrEqual, to: nil,
-                           attr: .notAnAttribute, constant: Constants.suggestionImageHeight)
-        Constraints.active(item: suggestionImage, attr: .centerY, relatedBy: .equal, to: contentView, attr: .centerY)
-        Constraints.active(item: suggestionText, attr: .top, relatedBy: .equal, to: contentView, attr: .top)
-        Constraints.active(item: suggestionText, attr: .bottom, relatedBy: .equal, to: contentView, attr: .bottom)
-        Constraints.active(item: suggestionText, attr: .trailing, relatedBy: .equal, to: contentView, attr: .trailing, constant: -Constants.horizontalInset, priority: 999)
+        NSLayoutConstraint.activate([
+            suggestionImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+            suggestionImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            suggestionImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
+                                                     constant: Constants.horizontalInset),
+            suggestionImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
+                                                      constant: -Constants.horizontalInset),
+            suggestionImage.widthAnchor.constraint(equalToConstant: Constants.suggestionImageWidth),
+            suggestionImage.heightAnchor.constraint(lessThanOrEqualToConstant: Constants.suggestionImageHeight),
+            suggestionImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            suggestionText.topAnchor.constraint(equalTo: contentView.topAnchor),
+            suggestionText.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            suggestionText.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
+                                                     constant: -Constants.horizontalInset)
+        ])
     }
 }
 
