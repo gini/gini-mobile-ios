40e66d66e88278d80457a037b4d39bdf5d530dca
Nadya Karaban <nadya@gini.net>
2023-08-08 18:05:21 +0200

    feat(GiniBankSDKExample): Add cameIcon, tintedImage extension, create group for SettingViewController related files
    PIA-4445

---
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/Extensions/UIImage+Utils.swift
@@ -0,0 +1,23 @@
+//
+//  UIImage+Utils.swift
+//  GiniBankSDKExample
+//
+//  Created by Nadya Karaban on 07.08.23.
+//
+
+import UIKit
+
+extension UIImage {
+    func tintedImageWithColor(_ color: UIColor) -> UIImage? {
+        let image = withRenderingMode(.alwaysTemplate)
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        color.set()
+        image.draw(in: CGRect(origin: .zero, size: size))
+
+        guard let imageColored = UIGraphicsGetImageFromCurrentImageContext() else {
+            return nil
+        }
+        UIGraphicsEndImageContext()
+        return imageColored
+    }
+}
