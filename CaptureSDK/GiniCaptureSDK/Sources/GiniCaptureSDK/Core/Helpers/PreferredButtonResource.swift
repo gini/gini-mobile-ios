a460dbd67c8b77dc6731066a5d3e9f3a59528c57
Nadya Karaban <nadya@gini.net>
2023-09-04 16:10:09 +0200

    feat(GiniCaptureSDK): Enable customization for `PrefferedButtonResource` from custom bundle
    PIA-2548

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Helpers/PreferredButtonResource.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Helpers/PreferredButtonResource.swift
@@ -37,9 +37,15 @@ class GiniPreferredButtonResource: PreferredButtonResource {
     private let localizedConfigEntry: String?
     private let appBundle = Bundle.main
     private let libBundle = giniCaptureBundle()
+    private var customBundle : Bundle {
+         guard let customBundle = GiniConfiguration.shared.customResourceBundle else {
+             return Bundle.main
+         }
+         return customBundle
+    }
     private var imageSource: ResourceOrigin {
         if let name = imageName {
-            if UIImage(named: name, in: appBundle, compatibleWith: nil) != nil {
+            if UIImage(named: name, in: customBundle, compatibleWith: nil) != nil {
                 return .custom
             } else if UIImage(named: name, in: libBundle, compatibleWith: nil) != nil {
                 return .library
