86a3ac38e3b7ef4e203cda4fcbf088197720b2e1
Nadya Karaban <nadya@gini.net>
2023-09-08 16:58:37 +0200

    feat(GiniBankSDKExample): Add resources, outlets for labels, keyboard show/hide, keyboard hide on return key.
    PIA-4445

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/SelectAPIViewController.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/SelectAPIViewController.swift
@@ -29,26 +29,33 @@ final class SelectAPIViewController: UIViewController {
     
     @IBOutlet weak var metaInformationButton: UIButton!
     @IBOutlet weak var ibanTextField: UITextField!
-
+    @IBOutlet weak var alternativeTitle: UILabel!
+    @IBOutlet weak var descriptionTitle: UILabel!
+    @IBOutlet weak var welcomeTitlte: UILabel!
     
     weak var delegate: SelectAPIViewControllerDelegate?
         
     var clientId: String?
     
     fileprivate func configureIbanTextField() {
-        if let cameraIcon = UIImage(named: "cameraIcon"), let iconColor = ColorPalette.giniBlue, let tintedImage = cameraIcon.tintedImageWithColor(iconColor) {
-            ibanTextField.layer.cornerRadius = 5
-            ibanTextField.layer.borderWidth = 1
-            ibanTextField.layer.borderColor = iconColor.cgColor
-            let mainView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 45))
+        if let cameraIcon = UIImage(named: "cameraInput"){
+            ibanTextField.delegate = self
+            let iconColor = UIColor.white
+            ibanTextField.layer.cornerRadius = 8
+            ibanTextField.backgroundColor = ColorPalette.ibanBackground
+            ibanTextField.attributedPlaceholder = NSAttributedString(
+                string: "Enter your IBAN code",
+                attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+            )
+            let mainView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 64))
             
-            let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 45))
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 64))
             view.clipsToBounds = true
             view.layer.cornerRadius = 5
             
-            let imageView = UIImageView(image: tintedImage)
+            let imageView = UIImageView(image: cameraIcon)
             imageView.contentMode = .scaleAspectFit
-            imageView.frame = CGRect(x: 12.0, y: 10.0, width: 24.0, height: 24.0)
+            imageView.frame = CGRect(x: 12.0, y: 19.0, width: 24.0, height: 24.0)
             view.addSubview(imageView)
             
             mainView.addSubview(view)
@@ -59,6 +66,16 @@ final class SelectAPIViewController: UIViewController {
             let iconTapGesture = UITapGestureRecognizer(target: self, action: #selector(self.iconTapped))
             ibanTextField.rightView?.addGestureRecognizer(iconTapGesture)
         }
+    }
+    
+    private func configureWelcomeTile(){
+        
+    }
+    private func configureDescriptionTile(){
+        
+    }
+    private func configureAlternativeTile(){
+        
     }
     
     fileprivate func configureMetaTitle() {
@@ -72,6 +89,7 @@ final class SelectAPIViewController: UIViewController {
                 
         configureMetaTitle()
         configureIbanTextField()
+        initializeHideKeyboard()
     }
     
     // MARK: User interaction
@@ -87,4 +105,36 @@ final class SelectAPIViewController: UIViewController {
         delegate?.selectAPI(viewController: self, didSelectEntryPoint: .field)
     }
     
+    // MARK: - Keyboard handling
+    
+    func initializeHideKeyboard(){
+        //Declare a Tap Gesture Recognizer which will trigger our dismissMyKeyboard() function
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissMyKeyboard))
+        
+        //Add this tap gesture recognizer to the parent view
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissMyKeyboard(){
+        //endEditing causes the view (or one of its embedded text fields) to resign the first responder status.
+        //In short- Dismiss the active keyboard.
+        view.endEditing(true)
+    }
+
+    
+}
+
+extension SelectAPIViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Check if there is any other text-field in the view whose tag is +1 greater than the current text-field on which the return key was pressed. If yes → then move the cursor to that next text-field. If No → Dismiss the keyboard
+        if let nextField = self.view.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return false
+    }
 }
--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/Utils/ColorPalette.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/Utils/ColorPalette.swift
@@ -11,4 +11,6 @@ enum ColorPalette {
 	static let raspberryPunch = UIColor(named: "raspberry-punch")
 	static let greySuit = UIColor(named: "grey-suit")
     static let giniBlue = UIColor(named: "gini-blue")
+    static let ibanBackground = UIColor(named: "iban-field-background")
+
 }
