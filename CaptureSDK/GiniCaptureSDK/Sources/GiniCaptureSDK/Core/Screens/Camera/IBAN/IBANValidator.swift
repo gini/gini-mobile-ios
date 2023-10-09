ca92d847d13225a4d72d839102db2ef87684a4e3
Nadya Karaban <nadya@gini.net>
2023-10-09 17:24:37 +0200

    refactor(GiniCaptureSDK): Extracted `IBANKnowledge` in a new file, fix lint issues, rename `extractIBANS` -> IBANRecogniser
    PIA-4356

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/IBAN/IBANValidator.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/IBAN/IBANValidator.swift
@@ -1,32 +1,18 @@
 //
 //  IBANValidator.swift
-//  GiniCapture
+//  GiniCaptureSDK
 //
-//  Created by Enrique del Pozo Gómez on 12/5/17.
+//  Copyright © 2023 Gini GmbH. All rights reserved.
 //
 
 import Foundation
 
 final class IBANValidator {
-    private var countryIbanDictionary: [String: Int] {
-        return [
-            "AL": 28, "AD": 24, "AT": 20, "AZ": 28, "BH": 22, "BE": 16,
-            "BA": 20, "BR": 29, "BG": 22, "CR": 21, "HR": 21, "CY": 28,
-            "CZ": 24, "DK": 18, "DO": 28, "EE": 20, "FO": 18, "FI": 18,
-            "FR": 27, "GE": 22, "DE": 22, "GI": 23, "GB": 22, "GR": 27,
-            "GL": 18, "GT": 28, "HU": 28, "IS": 26, "IE": 22, "IL": 23,
-            "IT": 27, "KZ": 20, "KW": 30, "LV": 21, "LB": 28, "LT": 20,
-            "LU": 20, "MK": 19, "MT": 31, "MR": 27, "MU": 30, "MD": 24,
-            "MC": 27, "ME": 22, "NL": 18, "NO": 15, "PK": 24, "PS": 29,
-            "PL": 28, "PT": 25, "RO": 24, "SM": 27, "SA": 24, "RS": 22,
-            "SK": 24, "SI": 19, "ES": 24, "SE": 24, "TN": 24, "TR": 26,
-            "AE": 23, "VG": 24, "CH": 21
-        ]
-    }
 
     private var validationSet: CharacterSet {
         return CharacterSet(charactersIn: "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ").inverted
     }
+    private let countryIbanDictionary: [String: Int] = IBANKnowledge().countryIbanDictionary
 
     func isValid(iban: String) -> Bool {
         let iban = iban.replacingOccurrences(of: " ", with: "")
