617a963caca1ca12ec49c1949f6e74d077708ed6
Nadya Karaban <nadya@gini.net>
2023-10-31 19:15:50 +0100

    refactor(GiniCaptureSDK): Refactor cleanup -> transaction summary and cleanup methods
    Cleanup should be called after TAN verification, update test
    
    PIA-4596

--- BankSDK/GiniBankSDKPinningExample/Tests/ExtractionFeedbackIntegrationTest.swift
+++ BankSDK/GiniBankSDKPinningExample/Tests/ExtractionFeedbackIntegrationTest.swift
@@ -179,9 +179,9 @@
 //                                                      document: self.giniCaptureSDKDocumentService?.document)
 //
 //                  delegate.giniCaptureAnalysisDidFinishWith(result: analysisResult)
-//                  // 4. Send feedback for the extractions the user saw
+//                  // 4. Send transaction summary for the extractions the user saw
 //                  //    with the final (user confirmed or updated) extraction values
-//                  GiniBankConfiguration.shared.cleanup(paymentRecipient: extractions["paymentRecipient"]?.value ?? "",
+//                  GiniBankConfiguration.shared.transactionSummary(paymentRecipient: extractions["paymentRecipient"]?.value ?? "",
 //                                                       paymentReference: extractions["paymentReference"]?.value ?? "",
 //                                                       paymentPurpose: extractions["paymentPurpose"]?.value ?? "",
 //                                                       iban: extractions["iban"]?.value ?? "",
