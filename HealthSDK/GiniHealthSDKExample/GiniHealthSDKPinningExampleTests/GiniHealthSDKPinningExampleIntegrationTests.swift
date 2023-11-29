5ae0b6afe2ee3d3715e54c382262ea920c10f549
Valentina Iancu <valentina.iancu@gini.net>
2023-11-29 14:20:06 +0100

    fix(GiniHealthSDK): Fix tests in `GiniHealthSDKPinningExampleTests`
    BSDK-61

--- HealthSDK/GiniHealthSDKExample/GiniHealthSDKPinningExampleTests/GiniHealthSDKPinningExampleIntegrationTests.swift
+++ HealthSDK/GiniHealthSDKExample/GiniHealthSDKPinningExampleTests/GiniHealthSDKPinningExampleIntegrationTests.swift
@@ -57,13 +57,15 @@ class GiniHealthSDKPinningExampleIntegrationTests: XCTestCase {
     
     func testCreatePaymentRequest(){
         let expect = expectation(description: "it creates a payment request")
-
+        
         paymentService.createPaymentRequest(sourceDocumentLocation: "", paymentProvider: "dbe3a2ca-c9df-11eb-a1d8-a7efff6e88b7", recipient: "Dr. med. Hackler", iban: "DE02300209000106531065", bic: "CMCIDEDDXXX", amount: "335.50:EUR", purpose: "ReNr AZ356789Z") { result in
             switch result {
-            case .success:
-                expect.fulfill()
-            case let .failure(error):
-                XCTFail(String(describing: error))
+                case .success:
+                    XCTFail("Expected an unauthorized error, but received a success case.")
+                case let .failure(error):
+                    XCTAssertEqual(error, .unauthorized(response: nil, data: nil), 
+                                   "Expected an unauthorized , but found \(error).")
+                    expect.fulfill()
             }
         }
         wait(for: [expect], timeout: 10)
