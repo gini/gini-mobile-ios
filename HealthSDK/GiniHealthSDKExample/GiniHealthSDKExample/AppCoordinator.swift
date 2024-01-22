2e057409a830911f21a9a2e8a1129cf56b2184e0
Valentina Iancu <valentina.iancu@gini.net>
2024-01-22 17:19:47 +0100

    refactor(GiniHealthSDKExample): Use `GiniHealthAPILibrary` instead of `GiniBankAPILibrary` in example app
    - remove unnecessary import of GiniBankAPILibrary
    
    BSDK-26

--- HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/AppCoordinator.swift
+++ HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/AppCoordinator.swift
@@ -11,7 +11,6 @@ import UIKit
 import GiniCaptureSDK
 import GiniHealthAPILibrary
 import GiniHealthSDK
-import GiniBankAPILibrary
 
 final class AppCoordinator: Coordinator {
     
@@ -109,12 +108,12 @@ final class AppCoordinator: Coordinator {
     
     
     fileprivate func showScreenAPI(with pages: [GiniCapturePage]? = nil) {
-        let metadata = GiniBankAPILibrary.Document.Metadata(branchId: documentMetadataBranchId,
-                                                            additionalHeaders: [documentMetadataAppFlowKey: "ScreenAPI"])
-        
+        let metadata = GiniHealthAPILibrary.Document.Metadata(branchId: documentMetadataBranchId,
+                                                              additionalHeaders: [documentMetadataAppFlowKey: "ScreenAPI"])
+
         let screenAPICoordinator = ScreenAPICoordinator(configuration: giniConfiguration,
                                                         importedDocuments: pages?.map { $0.document },
-                                                        client: GiniBankAPILibrary.Client(id: self.client.id,
+                                                        client: GiniHealthAPILibrary.Client(id: self.client.id,
                                                                                           secret: self.client.secret,
                                                                                           domain: self.client.domain),
                                                         documentMetadata: metadata)
--- HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/ScreenAPICoordinator.swift
+++ HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/ScreenAPICoordinator.swift
@@ -26,8 +26,8 @@ final class ScreenAPICoordinator: NSObject, Coordinator, GiniHealthTrackingDeleg
     var giniHealth: GiniHealth?
     var screenAPIViewController: UINavigationController!
     
-    let client: GiniBankAPILibrary.Client
-    let documentMetadata: GiniBankAPILibrary.Document.Metadata?
+    let client: GiniHealthAPILibrary.Client
+    let documentMetadata: GiniHealthAPILibrary.Document.Metadata?
     weak var analysisDelegate: AnalysisDelegate?
     var visionDocuments: [GiniCaptureDocument]?
     var visionConfiguration: GiniConfiguration
@@ -38,8 +38,8 @@ final class ScreenAPICoordinator: NSObject, Coordinator, GiniHealthTrackingDeleg
     
     init(configuration: GiniConfiguration,
          importedDocuments documents: [GiniCaptureDocument]?,
-         client: GiniBankAPILibrary.Client,
-         documentMetadata: GiniBankAPILibrary.Document.Metadata?) {
+         client: GiniHealthAPILibrary.Client,
+         documentMetadata: GiniHealthAPILibrary.Document.Metadata?) {
         visionConfiguration = configuration
         visionDocuments = documents
         self.client = client
