281c9500e7c72b6f8fa13062ba652df3b6ff2d4d
Razvan Capra <razvan.capra@lateralgroup.com>
2024-01-24 12:22:09 +0200

    fix(GiniHealthSDKExample): Fix handling errors with alert view  IPC-95

--- HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/InvoicesList/InvoicesListViewController.swift
+++ HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/InvoicesList/InvoicesListViewController.swift
@@ -11,6 +11,7 @@ protocol InvoicesListViewControllerProtocol: AnyObject {
     func showActivityIndicator()
     func hideActivityIndicator()
     func reloadTableView()
+    func showErrorAlertView(error: String)
 }
 
 final class InvoicesListViewController: UIViewController {
@@ -135,6 +136,14 @@ extension InvoicesListViewController: InvoicesListViewControllerProtocol {
     
     func reloadTableView() {
         self.invoicesTableView.reloadData()
+    }
+    
+    func showErrorAlertView(error: String) {
+        let alertController = UIAlertController(title: viewModel.errorUploadingTitleText, 
+                                                message: error,
+                                                preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default))
+        self.present(alertController, animated: true)
     }
 }
 
