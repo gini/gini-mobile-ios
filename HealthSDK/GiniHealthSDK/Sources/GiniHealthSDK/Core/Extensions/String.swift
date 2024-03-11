71491c098e23408b73821d63a186e41c94eac8e4
Razvan Capra <razvan.capra@lateralgroup.com>
2024-03-11 14:23:09 +0200

    fix(GiniHealthSDK): Move extension files to Extensions folder

--- HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/Extensions/String.swift
+++ HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/Extensions/String.swift
@@ -12,3 +12,15 @@ extension String {
         return UIColor(hex: String.rgbaHexFrom(rgbHex: self))
     }
 }
+
+public extension String {
+    var numberValue: NSNumber? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.number(from: self)
+    }
+    
+    static func rgbaHexFrom(rgbHex: String) -> String {
+       return "#\(rgbHex)FF"
+   }
+}
