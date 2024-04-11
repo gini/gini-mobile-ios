645aa13f8d5ffe9e85d8bcb6600bf19b893a0ca7
Razvan Capra <razvan.capra@lateralgroup.com>
2024-04-11 14:16:07 +0300

    feat(GiniHeatlhSDK): InstallAppBottomSheet view controller, view and viewModel
    IPC IPC-230

---
+++ HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/PaymentComponent/InstallAppBottomSheet.swift
@@ -0,0 +1,52 @@
+//
+//  InstallAppBottomSheet.swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+
+import UIKit
+
+final class InstallAppBottomSheet: UIViewController {
+    var bottomSheet: InstallAppBottomView! {
+        didSet {
+            setupLayout()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewHierarchy()
+        setupViewAttributes()
+    }
+
+    private func setupViewHierarchy() {
+        view.addSubview(bottomSheet)
+    }
+
+    private func setupViewAttributes() {
+        definesPresentationContext = true
+        view.backgroundColor = bottomSheet.viewModel.dimmingBackgroundColor
+        
+        bottomSheet.translatesAutoresizingMaskIntoConstraints = false
+        
+        let gesture = UISwipeGestureRecognizer(target: self, action: #selector(dismissViewController))
+        gesture.direction = .down
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(gesture)
+    }
+    
+    @objc
+    private func dismissViewController() {
+        bottomSheet.viewModel.didTapOnClose()
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            bottomSheet.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            bottomSheet.topAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -(bottomSheet.viewModel.bottomViewHeight)),
+            bottomSheet.widthAnchor.constraint(equalToConstant: self.view.frame.width),
+            bottomSheet.heightAnchor.constraint(equalToConstant: bottomSheet.viewModel.bottomViewHeight)
+        ])
+    }
+}
