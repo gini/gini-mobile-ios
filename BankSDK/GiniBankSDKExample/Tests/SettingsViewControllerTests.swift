691e13e9973eebdb3e9b3805bd0d77f06cbd3e48
Valentina Iancu <valentina.iancu@gini.net>
2024-04-24 20:51:19 +0200

    feat(GiniBankSDK): Add support for custom resource provider to override Gini colors
    PP-427

--- BankSDK/GiniBankSDKExample/Tests/SettingsViewControllerTests.swift
+++ BankSDK/GiniBankSDKExample/Tests/SettingsViewControllerTests.swift
@@ -1985,6 +1985,28 @@ extension SettingsViewControllerTests {
 		}
 	}
 
+    // MARK: - GiniBankCustomResourceProvider
+
+    func testCustomResourceProvider() {
+        guard let index = getSwitchOptionIndex(for: .customResourceProvider) else {
+            XCTFail("`customResourceProvider` option not found in sectionData")
+            return
+        }
+
+        if case .switchOption(var data) = contentData[index] {
+            guard data.type == .customResourceProvider else {
+                XCTFail("Expected type `customResourceProvider`, found a different one: \(data.type)")
+                return
+            }
+            data.isSwitchOn = true
+            let customProvider = GiniBankCustomResourceProvider()
+            configuration.customResourceProvider = customProvider
+
+            XCTAssertTrue(configuration.customResourceProvider != nil,
+                          "customResourceProvider should be enabled in the gini configuration")
+        }
+    }
+
 }
 
 extension SettingsViewControllerTests: GiniCaptureErrorLoggerDelegate {
