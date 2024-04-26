20e1f68413e9a076018f731306e47aaca233afd5
Valentina Iancu <valentina.iancu@gini.net>
2024-04-26 12:54:32 +0200

    fix(GiniBankSDK): Reduce the number of switch cases
    PP-427

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/GiniBankCustomResourceProvider.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/GiniBankCustomResourceProvider.swift
@@ -17,58 +17,6 @@ class GiniBankCustomResourceProvider: CustomResourceProvider {
                     return UIColor(hex: "#C4DDF7")!
                 case .accent02:
                     return UIColor(hex: "#FFFFFF")!
-                case .accent03:
-                    return .cyan
-                case .accent04:
-                    return .cyan
-                case .accent05:
-                    return .cyan
-                case .dark01:
-                    return .cyan
-                case .dark02:
-                    return .cyan
-                case .dark03:
-                    return .cyan
-                case .dark04:
-                    return .cyan
-                case .dark05:
-                    return .cyan
-                case .dark06:
-                    return .cyan
-                case .dark07:
-                    return .cyan
-                case .error01:
-                    return .red
-                case .error02:
-                    return .purple
-                case .error03:
-                    return .systemPink
-                case .error04:
-                    return .systemRed
-                case .error05:
-                    return .brown
-                case .light01:
-                    return .lightGray
-                case .light02:
-                    return .lightGray
-                case .light03:
-                    return .cyan
-                case .light04:
-                    return .lightGray
-                case .light05:
-                    return .cyan
-                case .light06:
-                    return .cyan
-                case .success01:
-                    return .cyan
-                case .success02:
-                    return .cyan
-                case .success03:
-                    return .cyan
-                case .success04:
-                    return .cyan
-                case .success05:
-                    return .cyan
                 case .warning01:
                     return UIColor.GiniBank.accent1
                 case .warning02:
@@ -79,6 +27,8 @@ class GiniBankCustomResourceProvider: CustomResourceProvider {
                     return UIColor.GiniBank.accent4
                 case .warning05:
                     return UIColor.GiniBank.accent5
+                default:
+                    return prefferedColorByProvider(named: name)
             }
         }
         return UIColor.GiniBank.accent1
