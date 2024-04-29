f0cde4bb50b2d69aaf414420aa044661d6f9e92b
Valentina Iancu <valentina.iancu@gini.net>
2024-04-29 17:57:47 +0200

    fix(GiniBankSDK): Revert order of bundles in `prefferedColor` method
    PP-427

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankUtils.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankUtils.swift
@@ -123,17 +123,17 @@ func prefferedImage(named name: String) -> UIImage? {
  - returns: UIColor if found with name.
  */
 func prefferedColor(named name: String) -> UIColor {
+    if let mainBundleColor = UIColor(named: name,
+                                     in: Bundle.main,
+                                     compatibleWith: nil) {
+        return mainBundleColor
+    }
+
     if let customBundle = GiniBankConfiguration.shared.customResourceBundle,
        let customBundleColor = UIColor(named: name,
                                        in: customBundle,
                                        compatibleWith: nil) {
         return customBundleColor
-    }
-
-    if let mainBundleColor = UIColor(named: name,
-                                     in: Bundle.main,
-                                     compatibleWith: nil) {
-        return mainBundleColor
     }
 
     if let color = UIColor(named: name,
