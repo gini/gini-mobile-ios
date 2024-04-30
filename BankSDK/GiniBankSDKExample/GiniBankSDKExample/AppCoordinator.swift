b04daff4a86777059a92d0934f088075cac3b8f7
Valentina Iancu <valentina.iancu@gini.net>
2024-04-30 13:07:03 +0200

    refactor(GiniBankSDKExample): Revert unnecessary configuration setup to show onboarding at launch
    PP-353

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/AppCoordinator.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/AppCoordinator.swift
@@ -35,7 +35,6 @@ final class AppCoordinator: Coordinator {
         configuration.multipageEnabled = true
         configuration.flashToggleEnabled = true
         configuration.localizedStringsTableName = "LocalizableCustomName"
-        configuration.onboardingShowAtLaunch = false
         configuration.customDocumentValidations = { document in
             // As an example of custom document validation, we add a more strict check for file size
             let maxFileSize = 5 * 1024 * 1024
