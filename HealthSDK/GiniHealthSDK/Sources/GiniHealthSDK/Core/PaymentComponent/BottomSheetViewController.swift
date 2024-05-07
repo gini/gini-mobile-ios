e8fad0b36723c35c1ae4ba9a097390a3b6fdcda5
Razvan Capra <razvan.capra@lateralgroup.com>
2024-05-07 13:44:23 +0300

    fix(GiniHealthSDK): Fix bottom sheet on smaller devices

--- HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/PaymentComponent/BottomSheetViewController.swift
+++ HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/PaymentComponent/BottomSheetViewController.swift
@@ -14,6 +14,7 @@ class BottomSheetViewController: UIViewController {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = backgroundColor
+        view.roundCorners(corners: [.topLeft, .topRight], radius: Constants.cornerRadiusView)
         view.layer.cornerRadius = Constants.cornerRadiusView
         view.clipsToBounds = true
         return view
