77b8c08f91911ab48161a52b9a05c8eb6c608272
Nadya Karaban <nadya@gini.net>
2024-05-17 16:09:51 +0200

    refactor(GiniBankSDK): Add copyright and removed the comment PP-468

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/ReturnAssistant/DigitalInvoice/DigitalInvoiceCoordinator.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/ReturnAssistant/DigitalInvoice/DigitalInvoiceCoordinator.swift
@@ -1,8 +1,8 @@
 //
-//  File.swift
-//  
+//  DigitalInvoiceCoordinator.swift
 //
-//  Created by David Vizaknai on 24.02.2023.
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
 //
 
 import GiniCaptureSDK
@@ -22,8 +22,7 @@ final class DigitalInvoiceCoordinator: Coordinator {
     private var digitalInvoiceViewModel: DigitalInvoiceViewModel?
 
     // TODO: This is to cope with the screen coordinator being inadequate at this point to support the return assistant step and needing a refactor.
-    // Remove ASAP
-    weak private var analysisDelegate: AnalysisDelegate?
+    private weak var analysisDelegate: AnalysisDelegate?
 
     weak var delegate: DigitalInvoiceCoordinatorDelegate?
     var rootViewController: UIViewController {
