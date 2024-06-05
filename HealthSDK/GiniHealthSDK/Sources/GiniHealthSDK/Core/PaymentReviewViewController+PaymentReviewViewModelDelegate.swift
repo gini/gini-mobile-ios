1cf29a6bc711993cae336dfd5318533f61047994
Razvan Capra <razvan.capra@lateralgroup.com>
2024-06-05 09:59:30 +0300

    feat(GiniHealthSDK): Download PDF from backend, save it and share it IPC-252
    + Unit tests fix for memory warnings

--- HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/PaymentReviewViewController+PaymentReviewViewModelDelegate.swift
+++ HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/PaymentReviewViewController+PaymentReviewViewModelDelegate.swift
@@ -23,22 +23,34 @@ extension PaymentReviewViewController: PaymentReviewViewModelDelegate {
         model?.incrementOnboardingCountFor(paymentProvider: selectedPaymentProvider)
     }
 
-    func sharePDFActivityUI() {
+    func obtainPDFFromPaymentRequest() {
         // TODO: Load PDF from backend then...
-        model?.loadPDF()
-
-        self.presentedViewController?.dismiss(animated: true, completion: {
-            self.sharePDF()
+        model?.createPaymentRequest(paymentInfo: obtainPaymentInfo(), completion: { [weak self] paymentRequestID in
+            self?.model?.loadPDF(paymentRequestID: paymentRequestID, completion: { [weak self] pdfData in
+                self?.writePDFDataToFile(data: pdfData, fileName: paymentRequestID, completion: { pdfPath, completed in
+                    if let pdfPath, completed {
+                        self?.sharePDF(pdfURL: pdfPath)
+                    }
+                })
+            })
         })
     }
-
-    func sharePDF() {
-        // Load your PDF file
-        guard let pdfURL = Bundle.main.url(forResource: "test_pdf", withExtension: "pdf") else {
-            print("PDF file not found.")
-            return
+    
+    func writePDFDataToFile(data: Data, fileName: String, completion: (_ pdfFileURL: URL? , _ status: Bool) -> ()) {
+        do {
+            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            guard let docDirectoryPath = paths.first else { return }
+            let pdfFileName = fileName + Constants.pdfExtension
+            let pdfPath = docDirectoryPath.appendingPathComponent(pdfFileName)
+            try data.write(to: pdfPath)
+            completion(pdfPath, true)
+        } catch {
+            print("Error while write pdf file to location: \(error.localizedDescription)")
+            completion(nil, false)
         }
+    }
 
+    func sharePDF(pdfURL: URL) {
         // Create UIActivityViewController with the PDF file
         let activityViewController = UIActivityViewController(activityItems: [pdfURL], applicationActivities: nil)
 
@@ -63,12 +75,20 @@ extension PaymentReviewViewController: PaymentReviewViewModelDelegate {
         ]
 
         // Present the UIActivityViewController
-        if let popoverController = activityViewController.popoverPresentationController {
-            popoverController.sourceView = self.view
-            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
-            popoverController.permittedArrowDirections = []
+        DispatchQueue.main.async {
+            if let popoverController = activityViewController.popoverPresentationController {
+                popoverController.sourceView = self.view
+                popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+                popoverController.permittedArrowDirections = []
+            }
+            
+            if (self.presentedViewController != nil) {
+                self.presentedViewController?.dismiss(animated: true, completion: {
+                    self.present(activityViewController, animated: true, completion: nil)
+                })
+            } else {
+                self.present(activityViewController, animated: true, completion: nil)
+            }
         }
-
-        present(activityViewController, animated: true, completion: nil)
     }
 }
