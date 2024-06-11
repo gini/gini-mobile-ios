26c4c8ccfc45e62d4adcd14a73368f6ffc51f7c1
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-06-11 19:29:40 +0200

    feat(GiniBankAPILibrary): remove extension from ConfigurationServiceProtocol and add it to ConfigurationService. remove APIDomain from protocol
    PP-352

--- BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Configuration/ConfigurationServiceProtocol.swift
+++ BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Configuration/ConfigurationServiceProtocol.swift
@@ -8,23 +8,12 @@
 import Foundation
 
 public protocol ConfigurationServiceProtocol: AnyObject {
-    var apiDomain: APIDomain { get }
     func fetchConfigurations(completion: @escaping CompletionResult<Configuration>)
 }
 
 extension ConfigurationServiceProtocol {
     func fetchConfigurations(resourceHandler: ResourceDataHandler<APIResource<Configuration>>,
                             completion: @escaping CompletionResult<Configuration>) {
-        let resource = APIResource<Configuration>(method: .configurations, apiDomain: apiDomain, httpMethod: .get)
-        
-        resourceHandler(resource, { result in
-            switch result {
-            case let .success(configuration):
-                completion(.success(configuration))
-            case let .failure(error):
-                completion(.failure(error))
-            }
-        })
     }
 }
 
