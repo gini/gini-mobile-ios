ad400cbd0e7d0f2905f292bbeebea22787ec536b
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-06-12 14:00:52 +0200

    feat(GiniCaptureSDK): add documentation for AnalyticsConfiguration
    PP-352

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Tracking/Models/AnalyticsConfiguration.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Tracking/Models/AnalyticsConfiguration.swift
@@ -4,19 +4,30 @@
 //  Copyright © 2024 Gini GmbH. All rights reserved.
 //
 
-
-import Foundation
-
+/**
+ Struct for analytics configuration settings
+ */
 public struct AnalyticsConfiguration {
-    public let clientID: String
-    public let userJourneyAnalyticsEnabled: Bool
-    public let mixpanelToken: String?
-    public let amplitudeApiKey: String?
-    
-    public init(clientID: String, userJourneyAnalyticsEnabled: Bool, mixpanelToken: String?, amplitudeApiKey: String?) {
+    /**
+     An initializer for an `AnalyticsConfiguration` structure
+     
+     - parameter clientID: A unique identifier for the client.
+     - parameter userJourneyAnalyticsEnabled: A flag indicating whether user journey analytics is enabled.
+     - parameter mixpanelToken: An optional token for Mixpanel integration.
+     - parameter amplitudeApiKey: An optional API key for Amplitude integration.
+     */
+    public init(clientID: String,
+                userJourneyAnalyticsEnabled: Bool,
+                mixpanelToken: String?,
+                amplitudeApiKey: String?) {
         self.clientID = clientID
         self.userJourneyAnalyticsEnabled = userJourneyAnalyticsEnabled
         self.mixpanelToken = mixpanelToken
         self.amplitudeApiKey = amplitudeApiKey
     }
+
+    public let clientID: String
+    public let userJourneyAnalyticsEnabled: Bool
+    public let mixpanelToken: String?
+    public let amplitudeApiKey: String?
 }
