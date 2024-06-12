c0c1bbd7bcf51e7d1d57f9ae2843738addece119
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-06-12 13:22:22 +0200

    feat(GiniBankAPILibrary): add documentation for Configuration
    PP-352

--- BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Configuration/Configuration.swift
+++ BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Configuration/Configuration.swift
@@ -7,7 +7,34 @@
 
 import Foundation
 
+/**
+ Struct for configuration settings
+ */
 public struct Configuration: Decodable {
+    /**
+     An initializer for a `Configuration` structure
+
+     - parameter clientID: A unique identifier for the client.
+     - parameter userJourneyAnalyticsEnabled: A flag indicating whether user journey analytics is enabled.
+     - parameter mixpanelToken: An optional token for Mixpanel integration.
+     - parameter amplitudeApiKey: An optional API key for Amplitude integration.
+     - parameter skontoEnabled: A flag indicating whether Skonto is enabled.
+     - parameter returnAssistantEnabled: A flag indicating whether the return assistant feature is enabled.
+     */
+    public init(clientID: String,
+                userJourneyAnalyticsEnabled: Bool,
+                mixpanelToken: String? = nil,
+                amplitudeApiKey: String? = nil,
+                skontoEnabled: Bool,
+                returnAssistantEnabled: Bool) {
+        self.clientID = clientID
+        self.userJourneyAnalyticsEnabled = userJourneyAnalyticsEnabled
+        self.mixpanelToken = mixpanelToken
+        self.amplitudeApiKey = amplitudeApiKey
+        self.skontoEnabled = skontoEnabled
+        self.returnAssistantEnabled = returnAssistantEnabled
+    }
+    
     public let clientID: String
     public let userJourneyAnalyticsEnabled: Bool
     public let mixpanelToken: String?
