6c308a0fd686d35803cb73dbeb1dc6c56f2ee477
Max Backevich <Backevich@gmail.com>
2024-06-20 19:11:04 +0200

    feat(GiniMerchantSDK): EC-63 remove iOS 12 support in MerchantSDK
    EC-63 remove iOS 12 support in MerchantSDK

--- MerchantSDK/GiniMerchantSDK/Package.swift
+++ MerchantSDK/GiniMerchantSDK/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GiniMerchantSDK",
     defaultLocalization: "en",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
--- MerchantSDK/GiniMerchantSDK/Sources/GiniMerchantSDK/Core/Extensions/UIColor+Utils.swift
+++ MerchantSDK/GiniMerchantSDK/Sources/GiniMerchantSDK/Core/Extensions/UIColor+Utils.swift
@@ -8,19 +8,14 @@
 import UIKit
 public extension UIColor {
     static func from(giniColor: GiniColor) -> UIColor {
-        if #available(iOS 13, *) {
-            return UIColor { (UITraitCollection: UITraitCollection) -> UIColor in
-                if UITraitCollection.userInterfaceStyle == .dark {
-                    /// Return the color for Dark Mode
-                    return giniColor.darkModeColor
-                } else {
-                    /// Return the color for Light Mode
-                    return giniColor.lightModeColor
-                }
+        return UIColor { (UITraitCollection: UITraitCollection) -> UIColor in
+            if UITraitCollection.userInterfaceStyle == .dark {
+                /// Return the color for Dark Mode
+                return giniColor.darkModeColor
+            } else {
+                /// Return the color for Light Mode
+                return giniColor.lightModeColor
             }
-        } else {
-            /// Return a fallback color for iOS 12 and lower.
-            return giniColor.lightModeColor
         }
     }
     
