84bab3517eb427f88eb050194596bf3a5cc8851e
Max Backevich <Backevich@gmail.com>
2024-06-20 18:23:39 +0200

    feat(GiniMerchantSDK): EC-63 Upate Merchant SDK structure
    EC-63 Upate Merchant SDK structure

--- MerchantSDK/GiniMerchantSDK/Package.swift
+++ MerchantSDK/GiniMerchantSDK/Package.swift
@@ -21,6 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        
         .target(
             name: "GiniMerchantSDK",
             dependencies: ["GiniHealthAPILibrary"]),
