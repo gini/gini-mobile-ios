11c22f3a94050440efc10af757cc4863704d1de2
Max Backevich <Backevich@gmail.com>
2024-06-21 11:09:02 +0200

    feat(GiniMerchantSDK): EC-63 Add tests for MerchantSDK
    EC-63 Add tests for MerchantSDK

--- MerchantSDK/GiniMerchantSDK/Tests/GiniMerchantSDKTests/GiniMerchantSDKTests.swift
+++ MerchantSDK/GiniMerchantSDK/Tests/GiniMerchantSDKTests/GiniMerchantSDKTests.swift
@@ -1,12 +1,359 @@
 import XCTest
 @testable import GiniMerchantSDK
+@testable import GiniHealthAPILibrary
 
-final class GiniMerchantSDKTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+final class GiniHealthTests: XCTestCase {
+    
+    var giniHealthAPI: GiniHealthAPI!
+    var giniMerchant: GiniMerchant!
+    
+    override func setUp() {
+        let sessionManagerMock = MockSessionManager()
+        let documentService = DefaultDocumentService(sessionManager: sessionManagerMock)
+        let paymentService = PaymentService(sessionManager: sessionManagerMock)
+        giniHealthAPI = GiniHealthAPI(documentService: documentService, paymentService: paymentService)
+        giniMerchant = GiniMerchant(with: giniHealthAPI)
+    }
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    override func tearDown() {
+        giniMerchant = nil
+        super.tearDown()
+    }
+    
+    func testSetConfiguration() throws {
+        // Given
+        let configuration = GiniMerchantConfiguration()
+        
+        // When
+        giniMerchant.setConfiguration(configuration)
+        
+        // Then
+        XCTAssertEqual(GiniMerchantConfiguration.shared, configuration)
+    }
+    
+    func testFetchBankingApps_Success() {
+        // Given
+        let expectedProviders: [PaymentProvider]? = loadProviders(fileName: "providers")
+        
+        // When
+        let expectation = self.expectation(description: "Fetching banking apps")
+        var receivedProviders: [PaymentProvider]?
+        giniMerchant.fetchBankingApps { result in
+            switch result {
+            case .success(let providers):
+                receivedProviders = providers
+            case .failure(_):
+                receivedProviders = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+        
+        // Then
+        XCTAssertNotNil(receivedProviders)
+        XCTAssertEqual(receivedProviders?.count, expectedProviders?.count)
+        XCTAssertEqual(receivedProviders, expectedProviders)
+    }
+
+    func testCheckIfDocumentIsPayable_Success() {
+        // Given
+        let fileName = "extractionResultWithIBAN"
+        let expectedExtractions: ExtractionsContainer? = GiniMerchantSDKTests.load(fromFile: fileName)
+        guard let expectedExtractions else {
+            XCTFail("Error loading file: `\(fileName).json`")
+            return
+        }
+        let expectedExtractionsResult = ExtractionResult(extractionsContainer: expectedExtractions)
+        let expectedIsPayable = expectedExtractionsResult.extractions.first(where: { $0.name == "iban" })?.value.isNotEmpty
+        
+        // When
+        let expectation = self.expectation(description: "Checking if document is payable")
+        var isDocumentPayable: Bool?
+        giniMerchant.checkIfDocumentIsPayable(docId: MockSessionManager.payableDocumentID) { result in
+            switch result {
+            case .success(let isPayable):
+                isDocumentPayable = isPayable
+            case .failure(_):
+                isDocumentPayable = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertEqual(expectedIsPayable, isDocumentPayable)
+    }
+    
+    func testCheckIfDocumentIsNotPayable_Success() {
+        // Given
+        let fileName = "extractionResultWithIBAN"
+        let expectedExtractions: ExtractionsContainer? = GiniMerchantSDKTests.load(fromFile: fileName)
+        guard let expectedExtractions else {
+            XCTFail("Error loading file: `\(fileName).json`")
+            return
+        }
+        let expectedExtractionsResult = ExtractionResult(extractionsContainer: expectedExtractions)
+        let expectedIsPayable = expectedExtractionsResult.extractions.first(where: { $0.name == "iban" })?.value.isEmpty
+        
+        // When
+        let expectation = self.expectation(description: "Checking if document is not payable")
+        var isDocumentPayable: Bool?
+        giniMerchant.checkIfDocumentIsPayable(docId: MockSessionManager.notPayableDocumentID) { result in
+            switch result {
+            case .success(let isPayable):
+                isDocumentPayable = isPayable
+            case .failure(_):
+                isDocumentPayable = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertEqual(expectedIsPayable, isDocumentPayable)
+    }
+    
+    func testCheckIfDocumentIsPayable_Failure() {
+        // When
+        let expectation = self.expectation(description: "Checking if request fails")
+        var isDocumentPayable: Bool?
+        giniMerchant.checkIfDocumentIsPayable(docId: MockSessionManager.failurePayableDocumentID) { result in
+            switch result {
+            case .success(let isPayable):
+                isDocumentPayable = isPayable
+            case .failure(_):
+                isDocumentPayable = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertNil(isDocumentPayable)
+    }
+    
+    func testPollDocument_Success() {
+        // Given
+        let expectedDocument: Document? = GiniMerchantSDKTests.load(fromFile: "document1")
+
+        // When
+        let expectation = self.expectation(description: "Polling document")
+        var receivedDocument: Document?
+        giniMerchant.pollDocument(docId: MockSessionManager.payableDocumentID) { result in
+            switch result {
+            case .success(let document):
+                receivedDocument = document
+            case .failure(_):
+                receivedDocument = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertNotNil(receivedDocument)
+        XCTAssertEqual(receivedDocument, expectedDocument)
+    }
+    
+    func testPollDocument_Failure() {
+        // When
+        let expectation = self.expectation(description: "Polling failure document")
+        var receivedDocument: Document?
+        giniMerchant.pollDocument(docId: MockSessionManager.missingDocumentID) { result in
+            switch result {
+            case .success(let document):
+                receivedDocument = document
+            case .failure(_):
+                receivedDocument = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertNil(receivedDocument)
+    }
+    
+    func testGetExtractions_Success() {
+        // Given
+        let fileName = "extractionsWithPayment"
+        let expectedExtractionContainer: ExtractionsContainer? = GiniMerchantSDKTests.load(fromFile: fileName)
+        guard let expectedExtractionContainer else {
+            XCTFail("Error loading file: `\(fileName).json`")
+            return
+        }
+        let expectedExtractions: [Extraction] = ExtractionResult(extractionsContainer: expectedExtractionContainer).payment?.first ?? []
+
+        // When
+        let expectation = self.expectation(description: "Getting extractions")
+        var receivedExtractions: [Extraction]?
+        giniMerchant.getExtractions(docId: MockSessionManager.extractionsWithPaymentDocumentID) { result in
+            switch result {
+            case .success(let extractions):
+                receivedExtractions = extractions
+            case .failure(_):
+                receivedExtractions = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertNotNil(receivedExtractions)
+        XCTAssertEqual(receivedExtractions?.count, expectedExtractions.count)
+    }
+    
+    func testGetExtractions_Failure() {
+        // When
+        let expectation = self.expectation(description: "Extraction failure")
+        var receivedExtractions: [Extraction]?
+        giniMerchant.getExtractions(docId: MockSessionManager.failurePayableDocumentID) { result in
+            switch result {
+            case .success(let extractions):
+                receivedExtractions = extractions
+            case .failure(_):
+                receivedExtractions = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertNil(receivedExtractions)
+    }
+    
+    func testCreatePaymentRequest_Success() {
+        // Given
+        let expectedPaymentRequestID = MockSessionManager.paymentRequestId
+
+        // When
+        let expectation = self.expectation(description: "Creating payment request")
+        var receivedRequestId: String?
+        let paymentInfo = PaymentInfo(recipient: "Uno Flüchtlingshilfe", iban: "DE78370501980020008850", bic: "COLSDE33", amount: "1.00:EUR", purpose: "ReNr 12345", paymentUniversalLink: "ginipay-test://paymentRequester", paymentProviderId: "b09ef70a-490f-11eb-952e-9bc6f4646c57")
+        giniMerchant.createPaymentRequest(paymentInfo: paymentInfo, completion: { result in
+            switch result {
+            case .success(let requestId):
+                receivedRequestId = requestId
+            case .failure(_):
+                receivedRequestId = nil
+            }
+            expectation.fulfill()
+        })
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertNotNil(receivedRequestId)
+        XCTAssertEqual(receivedRequestId, expectedPaymentRequestID)
+    }
+    
+    func testOpenLink_Success() {
+        let mockUIApplication = MockUIApplication(canOpen: true)
+        let urlOpener = URLOpener(mockUIApplication)
+        let waitForWebsiteOpen = expectation(description: "Link was opened")
+
+        giniMerchant.openPaymentProviderApp(requestID: "123", universalLink: "ginipay-bank://", urlOpener: urlOpener, completion: { open in
+            waitForWebsiteOpen.fulfill()
+            XCTAssert(open == true, "testOpenLink - FAILED to open link")
+        })
+
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
+    
+    func testOpenLink_Failure() {
+        let mockUIApplication = MockUIApplication(canOpen: false)
+        let urlOpener = URLOpener(mockUIApplication)
+        let waitForWebsiteOpen = expectation(description: "Link was not opened")
+
+        giniMerchant.openPaymentProviderApp(requestID: "123", universalLink: "ginipay-bank://", urlOpener: urlOpener, completion: { open in
+            waitForWebsiteOpen.fulfill()
+            XCTAssert(open == false, "testOpenLink - MANAGED to open link")
+        })
+
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
+    
+    func testSetDocumentForReview_Success() {
+        // Given
+        let fileName = "extractionsWithPayment"
+        let expectedExtractionContainer: ExtractionsContainer? = GiniMerchantSDKTests.load(fromFile: fileName)
+        guard let expectedExtractionContainer else {
+            XCTFail("Error loading file: `\(fileName).json`")
+            return
+        }
+        let expectedExtractions: [Extraction] = ExtractionResult(extractionsContainer: expectedExtractionContainer).payment?.first ?? []
+
+        // When
+        let expectation = self.expectation(description: "Setting document for review")
+        var receivedExtractions: [Extraction]?
+        giniMerchant.setDocumentForReview(documentId: MockSessionManager.extractionsWithPaymentDocumentID) { result in
+            switch result {
+            case .success(let extractions):
+                receivedExtractions = extractions
+            case .failure(_):
+                receivedExtractions = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertNotNil(receivedExtractions)
+        XCTAssertEqual(receivedExtractions?.count, expectedExtractions.count)
+    }
+    
+    func testFetchDataForReview_Success() {
+        // Given
+        let fileName = "extractionsWithPayment"
+        let expectedExtractionContainer: ExtractionsContainer? = GiniMerchantSDKTests.load(fromFile: fileName)
+        guard let expectedExtractionContainer else {
+            XCTFail("Error loading file: `\(fileName).json`")
+            return
+        }
+        let expectedExtractions: [Extraction] = ExtractionResult(extractionsContainer: expectedExtractionContainer).payment?.first ?? []
+        let documentFileName = "document4"
+        let expectedDocument: Document? = GiniMerchantSDKTests.load(fromFile: documentFileName)
+        guard let expectedDocument else {
+            XCTFail("Error loading file: `\(documentFileName).json`")
+            return
+        }
+        let expectedDatForReview = DataForReview(document: expectedDocument, extractions: expectedExtractions)
+
+        // When
+        let expectation = self.expectation(description: "Fetching data for review")
+        var receivedDataForReview: DataForReview?
+        giniMerchant.fetchDataForReview(documentId: MockSessionManager.extractionsWithPaymentDocumentID) { result in
+            switch result {
+            case .success(let dataForReview):
+                receivedDataForReview = dataForReview
+            case .failure(_):
+                receivedDataForReview = nil
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertNotNil(receivedDataForReview)
+        XCTAssertEqual(receivedDataForReview?.document, expectedDatForReview.document)
+        XCTAssertEqual(receivedDataForReview?.extractions.count, expectedDatForReview.extractions.count)
+    }
+    
+    func testFetchDataForReview_Failure() {
+        // When
+        let expectation = self.expectation(description: "Failure fetching data for review")
+        var receivedError: GiniMerchantError?
+        giniMerchant.fetchDataForReview(documentId: MockSessionManager.missingDocumentID) { result in
+            switch result {
+            case .success(_):
+                receivedError = nil
+            case .failure(let error):
+                receivedError = error
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertNotNil(receivedError)
     }
 }
