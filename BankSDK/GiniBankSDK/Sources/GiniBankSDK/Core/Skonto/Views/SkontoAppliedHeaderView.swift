dd91eff3447a6978ee14bcd178e36b728cdd4899
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-06-21 14:30:32 +0200

    feat(GiniBankSDK): SkontoAppliedHeaderView add Constants & font adjust capability
    PP-566

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoAppliedHeaderView.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoAppliedHeaderView.swift
@@ -14,6 +14,7 @@ public class SkontoAppliedHeaderView: UIView {
                                                               comment: "Mit Skonto")
         label.textColor = GiniColor(light: .GiniBank.dark1, dark: .GiniBank.light1).uiColor()
         label.font = configuration.textStyleFonts[.bodyBold]
+        label.adjustsFontForContentSizeCategory = true
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -28,6 +29,7 @@ public class SkontoAppliedHeaderView: UIView {
                                                                            dark: .GiniBank.success3).uiColor()
                         ])
         label.attributedText = attributedString
+        label.adjustsFontForContentSizeCategory = true
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -40,7 +42,7 @@ public class SkontoAppliedHeaderView: UIView {
         discountSwitch.translatesAutoresizingMaskIntoConstraints = false
         return discountSwitch
     }()
-    
+
     private let configuration = GiniBankConfiguration.shared
 
     override init(frame: CGRect) {
@@ -54,24 +56,32 @@ public class SkontoAppliedHeaderView: UIView {
     }
 
     private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = GiniColor(light: .GiniBank.light1, dark: .GiniBank.dark3).uiColor()
         addSubview(titleLabel)
         addSubview(statusLabel)
         addSubview(discountSwitch)
         setupConstraints()
     }
-    
+
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
 
             statusLabel.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
-            statusLabel.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 4),
+            statusLabel.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: Constants.statusLabelHorizontalPadding),
 
-            discountSwitch.topAnchor.constraint(equalTo: topAnchor, constant: 12),
-            discountSwitch.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
+            discountSwitch.topAnchor.constraint(equalTo: topAnchor, constant: Constants.discountSwitchTopPadding),
+            discountSwitch.bottomAnchor.constraint(equalTo: bottomAnchor),
             discountSwitch.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
-            discountSwitch.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0)
+            discountSwitch.trailingAnchor.constraint(equalTo: trailingAnchor)
         ])
+    }
+}
+
+private extension SkontoAppliedHeaderView {
+    enum Constants {
+        static let statusLabelHorizontalPadding: CGFloat = 4
+        static let discountSwitchTopPadding: CGFloat = 12
     }
 }
