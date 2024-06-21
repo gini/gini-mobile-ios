d5600f7c04368c874c0c014e88bfb8d30b66080a
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-06-21 14:03:27 +0200

    feat(GiniBankSDK): integrate SkontoProceedView to SkontoViewController
    PP-566

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/SkontoViewController.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/SkontoViewController.swift
@@ -12,43 +12,52 @@ public class SkontoViewController: UIViewController {
     let infoView = SkontoAppliedInfoView()
     let amountView = SkontoAppliedAmountView()
     let dateView = SkontoAppliedDateView()
+    let proceedView = SkontoProceedView()
 
     public override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
-    
+
     private func setupView() {
         view.backgroundColor = GiniColor(light: .GiniBank.light1, dark: .GiniBank.dark3).uiColor()
         view.addSubview(headerView)
         view.addSubview(infoView)
         view.addSubview(amountView)
         view.addSubview(dateView)
+        view.addSubview(proceedView)
         setupConstraints()
     }
 
     private func setupConstraints() {
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        infoView.translatesAutoresizingMaskIntoConstraints = false
-        amountView.translatesAutoresizingMaskIntoConstraints = false
-        dateView.translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Constants.verticalPadding),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.horizontalPadding),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.horizontalPadding),
 
-            infoView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 12),
-            infoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            infoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            infoView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: Constants.verticalPadding),
+            infoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.horizontalPadding),
+            infoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.horizontalPadding),
 
-            amountView.topAnchor.constraint(equalTo: infoView.bottomAnchor, constant: 12),
-            amountView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            amountView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            amountView.topAnchor.constraint(equalTo: infoView.bottomAnchor, constant: Constants.verticalPadding),
+            amountView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.horizontalPadding),
+            amountView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.horizontalPadding),
 
-            dateView.topAnchor.constraint(equalTo: amountView.bottomAnchor, constant: 8),
-            dateView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            dateView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            dateView.topAnchor.constraint(equalTo: amountView.bottomAnchor, constant: Constants.dateViewTopPadding),
+            dateView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.horizontalPadding),
+            dateView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.horizontalPadding),
+
+            proceedView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            proceedView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            proceedView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
         ])
+    }
+}
+
+private extension SkontoViewController {
+    enum Constants {
+        static let verticalPadding: CGFloat = 12
+        static let horizontalPadding: CGFloat = 16
+        static let dateViewTopPadding: CGFloat = 8
     }
 }
