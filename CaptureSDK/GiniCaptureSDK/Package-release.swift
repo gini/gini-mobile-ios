5d909c6a224eb0ae6fc29af270adcd9969532024
Valentina Iancu <valentina.iancu@gini.net>
2024-06-21 16:57:47 +0200

    feat(GiniCaptureSDK): Remove integration of Amplitude SDK
    PP-593

--- CaptureSDK/GiniCaptureSDK/Package-release.swift
+++ CaptureSDK/GiniCaptureSDK/Package-release.swift
@@ -17,8 +17,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "GiniBankAPILibrary", url: "https://github.com/gini/bank-api-library-ios.git", .exact("3.1.3")),
-        .package(name: "Mixpanel", url: "https://github.com/mixpanel/mixpanel-swift.git", .exact("4.2.0")),
-        .package(name: "Amplitude", url: "https://github.com/amplitude/Amplitude-iOS", .exact("8.18.1"))
+        .package(name: "Mixpanel", url: "https://github.com/mixpanel/mixpanel-swift.git", .exact("4.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,8 +26,7 @@ let package = Package(
         .target(
             name: "GiniCaptureSDK",
             dependencies: ["GiniBankAPILibrary",
-                           "Mixpanel",
-                           "Amplitude"
+                           "Mixpanel"
                           ]),
         .testTarget(
             name: "GiniCaptureSDKTests",
