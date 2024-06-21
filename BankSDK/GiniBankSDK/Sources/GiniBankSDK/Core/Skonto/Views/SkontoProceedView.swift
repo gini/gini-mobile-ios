925f74e5d95eea5bc5e71a9bfe635795b7f37ccf
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-06-21 13:52:53 +0200

    feat(GiniBankSDK): add SkontoProceedView
    PP-566

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoProceedView.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoProceedView.swift
@@ -4,17 +4,147 @@
 //  Copyright © 2024 Gini GmbH. All rights reserved.
 //
 
-
 import UIKit
+import GiniCaptureSDK
 
 class SkontoProceedView: UIView {
+    private lazy var payButton: MultilineTitleButton = {
+        let button = MultilineTitleButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.configure(with: configuration.primaryButtonConfiguration)
+        button.titleLabel?.font = configuration.textStyleFonts[.bodyBold]
+        // TODO: reused from digital invoice, to doublecheck if its the same for Skonto
+        let buttonTitle = NSLocalizedStringPreferredGiniBankFormat("ginibank.digitalinvoice.paybutton.title",
+                                                                   comment: "Proceed")
+        button.setTitle(buttonTitle, for: .normal)
+        return button
+    }()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private lazy var totalLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.font = configuration.textStyleFonts[.body]
+        label.textColor = GiniColor(light: .GiniBank.dark1, dark: .GiniBank.light1).uiColor()
+        // TODO: reused from digital invoice, to doublecheck if its the same for Skonto
+        let labelTitle = NSLocalizedStringPreferredGiniBankFormat("ginibank.digitalinvoice.lineitem.totalpricetitle",
+                                                                  comment: "Total")
+        label.text = labelTitle
+        label.accessibilityValue = labelTitle
+        return label
+    }()
+
+    private lazy var totalValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = configuration.textStyleFonts[.title1Bold]
+        label.textColor = GiniColor(light: .GiniBank.dark1, dark: .GiniBank.light1).uiColor()
+        label.text = "999,00"
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+
+    private lazy var skontoBadgeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = configuration.textStyleFonts[.caption1]
+        label.textColor = GiniColor(light: .GiniBank.light1, dark: .GiniBank.dark1).uiColor()
+        label.text = "3% Skonto"
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+
+    private lazy var skontoBadgeView: UIView = {
+        let view = UIView()
+        view.backgroundColor = GiniColor(light: .GiniBank.success3,
+                                    dark: .GiniBank.success3).uiColor()
+        view.layer.cornerRadius = 4
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(skontoBadgeLabel)
+        return view
+    }()
+
+    private lazy var dividerView: UIView = {
+        let dividerView = UIView()
+        dividerView.backgroundColor = GiniColor(light: .GiniBank.light3, dark: .GiniBank.dark4).uiColor()
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        return dividerView
+    }()
+
+    private let configuration = GiniBankConfiguration.shared
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
     }
-    */
 
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    private func setupView() {
+        backgroundColor = GiniColor(light: .GiniBank.light1, dark: .GiniBank.dark3).uiColor()
+        translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(dividerView)
+        addSubview(totalLabel)
+        addSubview(totalValueLabel)
+        addSubview(skontoBadgeView)
+        addSubview(payButton)
+
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            dividerView.topAnchor.constraint(equalTo: topAnchor),
+            dividerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            dividerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            dividerView.heightAnchor.constraint(equalToConstant: Constants.dividerViewHeight),
+
+            totalLabel.topAnchor.constraint(equalTo: dividerView.bottomAnchor, constant: Constants.padding),
+            totalLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.padding),
+            totalLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.padding),
+
+            totalValueLabel.topAnchor.constraint(equalTo: totalLabel.bottomAnchor, constant: 0),
+            totalValueLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.padding),
+
+            skontoBadgeView.centerYAnchor.constraint(equalTo: totalValueLabel.centerYAnchor),
+            skontoBadgeView.leadingAnchor.constraint(equalTo: totalValueLabel.trailingAnchor,
+                                                     constant: Constants.badgeSpacing),
+            skontoBadgeView.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor,
+                                                     constant: -Constants.padding),
+
+            skontoBadgeLabel.topAnchor.constraint(equalTo: skontoBadgeView.topAnchor,
+                                                  constant: Constants.badgeVerticalPadding),
+            skontoBadgeLabel.bottomAnchor.constraint(equalTo: skontoBadgeView.bottomAnchor,
+                                                     constant: -Constants.badgeVerticalPadding),
+            skontoBadgeLabel.leadingAnchor.constraint(equalTo: skontoBadgeView.leadingAnchor,
+                                                      constant: Constants.badgeHorizontalPadding),
+            skontoBadgeLabel.trailingAnchor.constraint(equalTo: skontoBadgeView.trailingAnchor,
+                                                       constant: -Constants.badgeHorizontalPadding),
+
+            payButton.topAnchor.constraint(equalTo: totalValueLabel.bottomAnchor, constant: Constants.verticalPadding),
+            // TODO: no safe area bottom padding in design
+            payButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor,
+                                              constant: -Constants.verticalPadding),
+            payButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            payButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.padding),
+            payButton.heightAnchor.constraint(greaterThanOrEqualToConstant: Constants.buttonHeight)
+        ])
+    }
+}
+
+private extension SkontoProceedView {
+    enum Constants {
+        static let padding: CGFloat = 24
+        static let verticalPadding: CGFloat = 16
+        static let buttonHeight: CGFloat = 50
+        static let dividerViewHeight: CGFloat = 1
+        static let badgeHorizontalPadding: CGFloat = 6
+        static let badgeVerticalPadding: CGFloat = 2
+        static let badgeSpacing: CGFloat = 12
+    }
 }
