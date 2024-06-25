d956282c54cb6b72ecd648035e8b53eadb243e31
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-06-25 18:08:57 +0200

    feat(GiniBankSDK): SkontoNotAppliedAmountView bind viewModel
    PP-568

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoNotAppliedAmountView.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoNotAppliedAmountView.swift
@@ -8,12 +8,11 @@ import UIKit
 import GiniCaptureSDK
 
 class SkontoNotAppliedAmountView: UIView {
-    private lazy var amountView: UIView = {
+    private lazy var amountView: SkontoAmountView = {
         let view = SkontoAmountView(title: NSLocalizedStringPreferredGiniBankFormat("ginibank.skonto.notapplied.amount.title",
                                                                                     comment: "Betrag ohne Abzug"),
                                     textFieldText: "1299,00",
-                                    currency: "EUR",
-                                    isEditable: !viewModel.isSkontoApplied)
+                                    currency: "EUR")
         view.delegate = self
         return view
     }()
@@ -26,6 +25,7 @@ class SkontoNotAppliedAmountView: UIView {
         self.viewModel = viewModel
         super.init(frame: .zero)
         setupView()
+        bindViewModel()
     }
 
     required init?(coder: NSCoder) {
@@ -45,6 +45,17 @@ class SkontoNotAppliedAmountView: UIView {
             amountView.leadingAnchor.constraint(equalTo: leadingAnchor),
             amountView.trailingAnchor.constraint(equalTo: trailingAnchor)
         ])
+    }
+
+    private func bindViewModel() {
+        configure(isSkontoApplied: viewModel.isSkontoApplied)
+        viewModel.addObserver { [weak self] isSkontoApplied in
+            self?.configure(isSkontoApplied: isSkontoApplied)
+        }
+    }
+
+    private func configure(isSkontoApplied: Bool) {
+        self.amountView.configure(isEditable: !isSkontoApplied)
     }
 }
 
