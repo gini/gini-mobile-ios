d93f0d1a7a7bdfb95d6a9b47ecbb77c53bd075a9
Valentina Iancu <valentina.iancu@gini.net>
2024-06-26 15:16:10 +0200

    feat(GiniCaptureSDK): Add object models for `BaseEvent`, `EventOptions`, and `EventsBatchPayload`
    PP-593

---
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Tracking/Amplitude/EventsBatchPayload.swift
@@ -0,0 +1,32 @@
+//
+//  EventsBatchPayload.swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+import Foundation
+/**
+ A struct representing the payload for batching events to be sent to the Amplitude server.
+
+ This struct conforms to the `Codable` protocol to facilitate easy encoding and decoding
+ to and from JSON format. It includes the API key and an array of events to be uploaded.
+
+ - Parameters:
+ - apiKey: The API key for the Amplitude analytics platform.
+ - events: An array of `BaseEvent` objects to be included in the batch upload.
+ */
+struct EventsBatchPayload: Codable {
+    let apiKey: String
+    let events: [BaseEvent]
+
+    /**
+     Customizes the coding keys for the `EventsBatchPayload` struct to match the expected JSON format.
+
+     - apiKey: Encoded as "api_key" in the JSON payload.
+     - events: Encoded as "events" in the JSON payload.
+     */
+    enum CodingKeys: String, CodingKey {
+        case apiKey = "api_key"
+        case events
+    }
+}
