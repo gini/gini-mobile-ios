3dfd2a42e16f4ae06f2c35d45470256ffbdb42ba
Valentina Iancu <valentina.iancu@gini.net>
2024-06-30 19:44:55 +0200

    feat(GiniCaptureSDK): Remove `mixpanelToken` from `ClientConfiguration`
    PP-593

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Tracking/Models/AnalyticsConfiguration.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Tracking/Models/AnalyticsConfiguration.swift
@@ -13,21 +13,17 @@ public struct AnalyticsConfiguration {
      
      - parameter clientID: A unique identifier for the client.
      - parameter userJourneyAnalyticsEnabled: A flag indicating whether user journey analytics is enabled.
-     - parameter mixpanelToken: An optional token for Mixpanel integration.
      - parameter amplitudeApiKey: An optional API key for Amplitude integration.
      */
     public init(clientID: String,
                 userJourneyAnalyticsEnabled: Bool,
-                mixpanelToken: String?,
                 amplitudeApiKey: String?) {
         self.clientID = clientID
         self.userJourneyAnalyticsEnabled = userJourneyAnalyticsEnabled
-        self.mixpanelToken = mixpanelToken
         self.amplitudeApiKey = amplitudeApiKey
     }
 
     public let clientID: String
     public let userJourneyAnalyticsEnabled: Bool
-    public let mixpanelToken: String?
     public let amplitudeApiKey: String?
 }
