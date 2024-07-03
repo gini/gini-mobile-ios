61512d41ebee2cc846cab48ed8f8fc7e9482ffcc
Valentina Iancu <valentina.iancu@gini.net>
2024-07-03 17:41:17 +0200

    feat(GiniBankSDK): Add SkontoCoordinator
    PP-631

---
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/SkontoCoordinator.swift
@@ -0,0 +1,62 @@
+//
+//  SkontoCoordinator.swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+import GiniCaptureSDK
+import UIKit
+
+protocol SkontoCoordinatorDelegate: AnyObject {
+    func didCancelAnalysis(_ coordinator: SkontoCoordinator)
+    func didFinishAnalysis(_ coordinator: SkontoCoordinator)
+}
+
+final class SkontoCoordinator: Coordinator {
+
+    var childCoordinators: [Coordinator] = []
+    private var skontoViewController: SkontoViewController?
+    
+    weak var delegate: SkontoCoordinatorDelegate?
+
+    var rootViewController: UIViewController {
+        guard let skontoViewController else {
+            return UIViewController()
+        }
+
+        return skontoViewController
+    }
+
+    private var navigationController: UINavigationController
+
+    func start() {
+        navigationController.pushViewController(rootViewController, animated: true)
+    }
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+
+        let skontoViewModel = SkontoViewModel(isSkontoApplied: true,
+                                              skontoValue: 3.0,
+                                              date: Date(),
+                                              priceWithoutSkonto: .init(value: 99.99, currencyCode: "EUR"))
+
+        skontoViewModel.delegate = self
+        skontoViewController = SkontoViewController(viewModel: skontoViewModel)
+    }
+}
+
+extension SkontoCoordinator: SkontoViewModelDelegate {
+    func didTapHelp() {
+        // Should display Help screen
+    }
+
+    func didTapCancel() {
+        delegate?.didCancelAnalysis(self)
+    }
+
+    func didTapProceed(on viewModel: SkontoViewModel) {
+        // TODO: maybe we need to do something more
+        delegate?.didFinishAnalysis(self)
+    }
+}
