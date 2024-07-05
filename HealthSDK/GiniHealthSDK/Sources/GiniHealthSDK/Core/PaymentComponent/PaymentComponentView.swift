25303445135f39d03787ad40d8d54c4622c4a8dd
Razvan Capra <razvan.capra@lateralgroup.com>
2024-07-05 16:40:50 +0300

    fix(GiniHealthSDK): Fix payment component more information view constraints

--- HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/PaymentComponent/PaymentComponentView.swift
+++ HealthSDK/GiniHealthSDK/Sources/GiniHealthSDK/Core/PaymentComponent/PaymentComponentView.swift
@@ -101,7 +101,9 @@ final class PaymentComponentView: UIView {
         buttonsView.addSubview(buttonsStackView)
         contentStackView.addArrangedSubview(buttonsView)
         
-        bottomStackView.addArrangedSubview(moreInformationView)
+        if !viewModel.isPaymentComponentUsed() {
+            bottomStackView.addArrangedSubview(moreInformationView)
+        }
         bottomStackView.addArrangedSubview(UIView())
         if viewModel.shouldShowBrandedView {
             bottomStackView.addArrangedSubview(poweredByGiniView)
@@ -135,7 +137,6 @@ final class PaymentComponentView: UIView {
     private func updateAvailableViews() {
         let isPaymentComponentUsed = viewModel.isPaymentComponentUsed()
         selectYourBankView.isHidden = isPaymentComponentUsed
-        moreInformationView.isHidden = isPaymentComponentUsed
     }
     
     private func updateButtonsViews() {
