6e56c35aeae6a7c10b920fcb669ea8f9bf015898
Max Backevich <Backevich@gmail.com>
2024-07-08 16:37:09 +0200

    refactor(GiniHealthAPILibrary): Remove TrustKit imports

--- HealthAPILibrary/GiniHealthAPILibraryPinning/Sources/GiniHealthAPILibraryPinning/GiniHealthAPI+Pinning.swift
+++ HealthAPILibrary/GiniHealthAPILibraryPinning/Sources/GiniHealthAPILibraryPinning/GiniHealthAPI+Pinning.swift
@@ -5,8 +5,8 @@
 //  Created by Enrique del Pozo Gómez on 1/21/18.
 //
 
-import TrustKit
 import GiniHealthAPILibrary
+import Foundation
 
 public extension GiniHealthAPI.Builder {
     
@@ -16,27 +16,20 @@ public extension GiniHealthAPI.Builder {
      * - Parameter client:            The Gini Health API client credentials
      * - Parameter api:               The Gini Health API that the library interacts with. `APIDomain.default` by default
      * - Parameter userApi:           The Gini User API that the library interacts with. `UserDomain.default` by default
-     * - Parameter pinningConfig:     Configuration for certificate pinning.
+     * - Parameter pinningConfig:     Configuration for certificate pinning. Format ["PinnedDomains" : ["PublicKeyHashes"]]
      * - Parameter logLevel:          The log level. `LogLevel.none` by default.
      */
 
     init(client: Client,
          api: APIDomain = .default,
          userApi: UserDomain = .default,
-         pinningConfig: [String: Any],
+         pinningConfig: [String: [String]],
          logLevel: LogLevel = .none) {
         self.init(client: client, api: api, userApi: userApi, logLevel: logLevel, sessionDelegate: SessionDelegate())
         
-        if let pinnedDomains = pinningConfig[kTSKPinnedDomains] as? [String: Any] {
-            var keys: [String] = []
-            for domainConfig in pinnedDomains.values {
-                if let domainConfig = domainConfig as? [String: Any],
-                   let keyHashes = domainConfig[kTSKPublicKeyHashes] as? [String] {
-                    keys.append(contentsOf: keyHashes)
-                }
-            }
-            SSLPinningManager.shared.pinnedKeyHashes = keys
-        }
+        // Extract and set pinnedKeyHashes from the configuration
+         let allKeyHashes = pinningConfig.values.flatMap { $0 }
+         SSLPinningManager.shared.pinnedKeyHashes = allKeyHashes
     }
     
     /**
@@ -44,27 +37,18 @@ public extension GiniHealthAPI.Builder {
      *
      * - Parameter customApiDomain:        A custom api domain string.
      * - Parameter alternativeTokenSource: A protocol for using custom api access token
-     * - Parameter pinningConfig:          Configuration for certificate pinning.
+     * - Parameter pinningConfig:     Configuration for certificate pinning. Format ["PinnedDomains" : ["PublicKeyHashes"]]
      * - Parameter logLevel:               The log level. `LogLevel.none` by default.
      */
     init(customApiDomain: String,
          alternativeTokenSource: AlternativeTokenSource,
-         pinningConfig: [String: Any],
+         pinningConfig: [String: [String]],
          logLevel: LogLevel = .none) {
         self.init(customApiDomain: customApiDomain, alternativeTokenSource: alternativeTokenSource, logLevel: logLevel, sessionDelegate: SessionDelegate())
-//        TrustKit.initSharedInstance(withConfiguration: pinningConfig)
         
         // Extract and set pinnedKeyHashes from the configuration
-        if let pinnedDomains = pinningConfig[kTSKPinnedDomains] as? [String: Any] {
-            var keys: [String] = []
-            for domainConfig in pinnedDomains.values {
-                if let domainConfig = domainConfig as? [String: Any],
-                   let keyHashes = domainConfig[kTSKPublicKeyHashes] as? [String] {
-                    keys.append(contentsOf: keyHashes)
-                }
-            }
-            SSLPinningManager.shared.pinnedKeyHashes = keys
-        }
+         let allKeyHashes = pinningConfig.values.flatMap { $0 }
+         SSLPinningManager.shared.pinnedKeyHashes = allKeyHashes
     }
 }
 
@@ -73,8 +57,5 @@ class SessionDelegate: NSObject, URLSessionDelegate {
                     didReceive challenge: URLAuthenticationChallenge,
                     completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
         SSLPinningManager.shared.validate(challenge: challenge, completionHandler: completionHandler)
-//        if TrustKit.sharedInstance().pinningValidator.handle(challenge, completionHandler: completionHandler) == false {
-//            completionHandler(.performDefaultHandling, nil)
-//        }
     }
 }
