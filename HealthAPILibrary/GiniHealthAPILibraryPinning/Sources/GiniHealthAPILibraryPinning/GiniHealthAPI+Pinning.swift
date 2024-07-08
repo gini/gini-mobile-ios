83b7a126245462d249f8b4fe6adc05556d81de8d
Max Backevich <Backevich@gmail.com>
2024-07-08 15:42:15 +0200

    ref(HealthAPILibrary): Remove TrustKit

--- HealthAPILibrary/GiniHealthAPILibraryPinning/Sources/GiniHealthAPILibraryPinning/GiniHealthAPI+Pinning.swift
+++ HealthAPILibrary/GiniHealthAPILibraryPinning/Sources/GiniHealthAPILibraryPinning/GiniHealthAPI+Pinning.swift
@@ -26,7 +26,17 @@ public extension GiniHealthAPI.Builder {
          pinningConfig: [String: Any],
          logLevel: LogLevel = .none) {
         self.init(client: client, api: api, userApi: userApi, logLevel: logLevel, sessionDelegate: SessionDelegate())
-        TrustKit.initSharedInstance(withConfiguration: pinningConfig)
+        
+        if let pinnedDomains = pinningConfig[kTSKPinnedDomains] as? [String: Any] {
+            var keys: [String] = []
+            for domainConfig in pinnedDomains.values {
+                if let domainConfig = domainConfig as? [String: Any],
+                   let keyHashes = domainConfig[kTSKPublicKeyHashes] as? [String] {
+                    keys.append(contentsOf: keyHashes)
+                }
+            }
+            SSLPinningManager.shared.pinnedKeyHashes = keys
+        }
     }
     
     /**
@@ -42,7 +52,19 @@ public extension GiniHealthAPI.Builder {
          pinningConfig: [String: Any],
          logLevel: LogLevel = .none) {
         self.init(customApiDomain: customApiDomain, alternativeTokenSource: alternativeTokenSource, logLevel: logLevel, sessionDelegate: SessionDelegate())
-        TrustKit.initSharedInstance(withConfiguration: pinningConfig)
+//        TrustKit.initSharedInstance(withConfiguration: pinningConfig)
+        
+        // Extract and set pinnedKeyHashes from the configuration
+        if let pinnedDomains = pinningConfig[kTSKPinnedDomains] as? [String: Any] {
+            var keys: [String] = []
+            for domainConfig in pinnedDomains.values {
+                if let domainConfig = domainConfig as? [String: Any],
+                   let keyHashes = domainConfig[kTSKPublicKeyHashes] as? [String] {
+                    keys.append(contentsOf: keyHashes)
+                }
+            }
+            SSLPinningManager.shared.pinnedKeyHashes = keys
+        }
     }
 }
 
@@ -50,8 +72,9 @@ class SessionDelegate: NSObject, URLSessionDelegate {
     public func urlSession(_ session: URLSession,
                     didReceive challenge: URLAuthenticationChallenge,
                     completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        if TrustKit.sharedInstance().pinningValidator.handle(challenge, completionHandler: completionHandler) == false {
-            completionHandler(.performDefaultHandling, nil)
-        }
+        SSLPinningManager.shared.validate(challenge: challenge, completionHandler: completionHandler)
+//        if TrustKit.sharedInstance().pinningValidator.handle(challenge, completionHandler: completionHandler) == false {
+//            completionHandler(.performDefaultHandling, nil)
+//        }
     }
 }
---
+++ HealthAPILibrary/GiniHealthAPILibraryPinning/Sources/GiniHealthAPILibraryPinning/SSLPinningManager.swift
@@ -0,0 +1,123 @@
+//
+//  SSLPinningManager.swift
+//
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+import CryptoKit
+import Foundation
+import CommonCrypto
+
+struct SSLPinningManager {
+    private enum PinningError: Error {
+        case noCertificatesFromServer
+        case failedToGetPublicKey
+        case failedToGetDataFromPublicKey
+        case receivedWrongCertificate
+    }
+    
+    private static let rsa2048ASN1Header: [UInt8] = [
+        0x30, 0x82, 0x01, 0x22, 0x30, 0x0D, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86,
+        0xF7, 0x0D, 0x01, 0x01, 0x01, 0x05, 0x00, 0x03, 0x82, 0x01, 0x0F, 0x00
+    ]
+
+    //TODO: Provide proper initialization
+    var pinnedKeyHashes: [String]
+
+    public static var shared = SSLPinningManager(pinnedKeyHashes: [])
+    
+    init(pinnedKeyHashes: [String]) {
+        self.pinnedKeyHashes = pinnedKeyHashes
+    }
+
+    func validate(challenge: URLAuthenticationChallenge,
+                  completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        do {
+            let trust = try validateAndGetTrust(with: challenge)
+            completionHandler(.useCredential, URLCredential(trust: trust))
+        } catch {
+            completionHandler(.cancelAuthenticationChallenge, nil)
+        }
+    }
+}
+
+//MARK: - private methods
+
+private extension SSLPinningManager {
+    func validateAndGetTrust(with challenge: URLAuthenticationChallenge) throws -> SecTrust {
+        guard let trust = challenge.protectionSpace.serverTrust,
+              let trustCertificateChain = trustCopyCertificateChain(trust),
+              !trustCertificateChain.isEmpty else {
+            throw PinningError.noCertificatesFromServer
+        }
+
+        for serverCertificate in trustCertificateChain {
+            let publicKey = try getPublicKey(for: serverCertificate)
+            let publicKeyHash = try getKeyHash(of: publicKey)
+
+            if pinnedKeyHashes.contains(publicKeyHash) {
+                return trust
+            }
+        }
+        throw PinningError.receivedWrongCertificate
+    }
+    
+    func getPublicKey(for certificate: SecCertificate) throws -> SecKey {
+        let policy = SecPolicyCreateBasicX509()
+        var trust: SecTrust?
+        let trustCreationStatus = SecTrustCreateWithCertificates(certificate, policy, &trust)
+
+        guard let trust, trustCreationStatus == errSecSuccess, let publicKey = trustCopyPublicKey(trust) else {
+            throw PinningError.failedToGetPublicKey
+        }
+        
+        return publicKey
+    }
+    
+    func getKeyHash(of publicKey: SecKey) throws -> String {
+        guard let publicKeyCFData = SecKeyCopyExternalRepresentation(publicKey, nil) else {
+            throw PinningError.failedToGetDataFromPublicKey
+        }
+
+        let publicKeyData = (publicKeyCFData as NSData) as Data
+        var publicKeyWithHeaderData = Data(Self.rsa2048ASN1Header)
+        publicKeyWithHeaderData.append(publicKeyData)
+        let publicKeyHashData = sha256(data: publicKeyWithHeaderData)
+        return publicKeyHashData.base64EncodedString()
+    }
+}
+
+//MARK: - private methods for capability with old ios versions
+
+private extension SSLPinningManager {
+    func trustCopyPublicKey(_ trust: SecTrust) -> SecKey? {
+        if #available(iOS 14, macOS 11, tvOS 14, watchOS 7, visionOS 1, *) {
+            return SecTrustCopyKey(trust)
+        } else {
+            return SecTrustCopyPublicKey(trust)
+        }
+    }
+    
+    func trustCopyCertificateChain(_ trust: SecTrust) -> [SecCertificate]? {
+        if #available(iOS 15, macOS 11, tvOS 14, watchOS 7, visionOS 1, *) {
+            return (SecTrustCopyCertificateChain(trust) as? [SecCertificate])
+        } else {
+            return (0..<SecTrustGetCertificateCount(trust)).compactMap { index in
+                SecTrustGetCertificateAtIndex(trust, index)
+            }
+        }
+    }
+
+    func sha256(data: Data) -> Data {
+        if #available(iOS 13.0, macOS 10.15, watchOS 6.0, tvOS 13.0, *) {
+            return Data(SHA256.hash(data: data))
+        } else {
+            var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+            data.withUnsafeBytes { buffer in
+                _ = CC_SHA256(buffer.baseAddress!, CC_LONG(buffer.count), &hash)
+            }
+            return Data(hash)
+        }
+    }
+}
