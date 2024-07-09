5ce1a29e74337017c183bd41cc8f03dd1486c136
Max Backevich <Backevich@gmail.com>
2024-07-10 00:49:38 +0200

    fix(GiniMerchantSDK): Remove package modifyer

--- MerchantSDK/GiniMerchantSDK/Sources/GiniMerchantSDK/Core/SSLPinning/GiniSessionDelegate.swift
+++ MerchantSDK/GiniMerchantSDK/Sources/GiniMerchantSDK/Core/SSLPinning/GiniSessionDelegate.swift
@@ -10,11 +10,11 @@ import Foundation
 class GiniSessionDelegate: NSObject, URLSessionDelegate {
     private let pinningManager: SSLPinningManager
     
-    package init(pinnedKeyHashes: [String]) {
+    init(pinnedKeyHashes: [String]) {
         self.pinningManager = SSLPinningManager(pinnedKeyHashes: pinnedKeyHashes)
     }
     
-    package func urlSession(_ session: URLSession,
+    func urlSession(_ session: URLSession,
                     didReceive challenge: URLAuthenticationChallenge,
                     completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
         pinningManager.validate(challenge: challenge, completionHandler: completionHandler)
