47bcd6c37c04bffdce8b992cf27e99efa87f5231
Valentina Iancu <valentina.iancu@gini.net>
2024-07-16 14:10:21 +0200

    fix(GiniBankSDK): Fix crash in Skonto info badge due to incorrect type for `remainingDays`
    - Ensured remainingDays is properly formatted and passed as a string.
    
    PP-572

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoAppliedInfoView.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoAppliedInfoView.swift
@@ -18,7 +18,7 @@ class SkontoAppliedInfoView: UIView {
     private lazy var label: UILabel = {
         let label = UILabel()
         let text = String.localizedStringWithFormat(skontoTitle,
-                                                    viewModel.remainingDays,
+                                                    "\(viewModel.remainingDays)",
                                                     viewModel.formattedPercentageDiscounted)
         label.text = text
         label.accessibilityValue = text
@@ -86,7 +86,7 @@ class SkontoAppliedInfoView: UIView {
 
     private func configure() {
         let text = String.localizedStringWithFormat(skontoTitle,
-                                                    viewModel.remainingDays,
+                                                    "\(viewModel.remainingDays)",
                                                     viewModel.formattedPercentageDiscounted)
         label.text = text
     }
