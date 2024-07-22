50015bc82fea8a3752a8440eef9c5b0981bcea17
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-07-22 15:16:29 +0200

    feat(GiniBankSDK): add multiline label support for info view
    PP-555

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoAppliedInfoView.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Views/SkontoAppliedInfoView.swift
@@ -18,6 +18,7 @@ class SkontoAppliedInfoView: UIView {
     private lazy var label: UILabel = {
         let label = UILabel()
         label.font = configuration.textStyleFonts[.caption1]
+        label.numberOfLines = 0
         label.adjustsFontForContentSizeCategory = true
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -50,15 +51,19 @@ class SkontoAppliedInfoView: UIView {
 
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: topAnchor,
+            imageView.topAnchor.constraint(greaterThanOrEqualTo: topAnchor,
                                            constant: Constants.imageVerticalPadding),
-            imageView.bottomAnchor.constraint(equalTo: bottomAnchor,
+            imageView.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor,
                                               constant: -Constants.imageVerticalPadding),
             imageView.leadingAnchor.constraint(equalTo: leadingAnchor,
                                                constant: Constants.imageHorizontalPadding),
             imageView.widthAnchor.constraint(equalToConstant: Constants.imageSize),
             imageView.heightAnchor.constraint(equalToConstant: Constants.imageSize),
 
+            label.topAnchor.constraint(greaterThanOrEqualTo: topAnchor,
+                                       constant: Constants.labelVerticalPadding),
+            label.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor,
+                                          constant: -Constants.labelVerticalPadding),
             label.leadingAnchor.constraint(equalTo: imageView.trailingAnchor,
                                            constant: Constants.labelHorizontalPadding),
             label.trailingAnchor.constraint(equalTo: trailingAnchor,
@@ -142,6 +147,7 @@ private extension SkontoAppliedInfoView {
         static let imageHorizontalPadding: CGFloat = 10
         static let imageSize: CGFloat = 24
         static let labelHorizontalPadding: CGFloat = 8
+        static let labelVerticalPadding: CGFloat = 10
         static let cornerRadius: CGFloat = 8
     }
 }
