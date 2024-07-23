6a1a59d0863ae9b1c209e38641a841cf0e4bad35
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-07-23 17:00:03 +0200

    feat(GiniBankSDK): Berlin TimeZone for current date
    PP-555

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Extensions/Foundation/Date+Formatting.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Extensions/Foundation/Date+Formatting.swift
@@ -14,4 +14,16 @@ extension Date {
     var yearMonthDayString: String {
         return DateFormatter.yearMonthDay.string(from: self)
     }
+
+    var inBerlinTimeZone: Date {
+        guard let berlinTimeZone = TimeZone(identifier: "Europe/Berlin") else {
+            return Date()
+        }
+        
+        var calendar = Calendar.current
+        calendar.timeZone = berlinTimeZone
+        
+        let berlinDate = calendar.date(byAdding: .second, value: calendar.timeZone.secondsFromGMT(for: self), to: self)
+        return berlinDate ?? Date()
+    }
 }
