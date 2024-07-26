23d4f1e9b85bee21671606225dcda29937a68578
motokotomax <backevich@gmail.com>
2024-07-26 11:30:06 +0200

    feat (GiniHealthAPILibrary): Add API version parametr (#599)
    * feat (GiniHealthAPILibrary): Add API version parametr
    
    EC-69 Add API version parametr, update tests, update GiniHealthSDK, GiniMerchantSDK
    
    * fix (GiniHealthAPILibrary): Move default version API to constants

--- MerchantSDK/GiniMerchantSDK/Tests/GiniMerchantSDKTests/GiniMerchantSDKTests.swift
+++ MerchantSDK/GiniMerchantSDK/Tests/GiniMerchantSDKTests/GiniMerchantSDKTests.swift
@@ -13,11 +13,12 @@ final class GiniMerchantTests: XCTestCase {
     
     var giniHealthAPI: GiniHealthAPI!
     var giniMerchant: GiniMerchant!
-    
+    private let versionAPI = 4
+
     override func setUp() {
         let sessionManagerMock = MockSessionManager()
-        let documentService = DefaultDocumentService(sessionManager: sessionManagerMock)
-        let paymentService = PaymentService(sessionManager: sessionManagerMock)
+        let documentService = DefaultDocumentService(sessionManager: sessionManagerMock, apiVersion: versionAPI)
+        let paymentService = PaymentService(sessionManager: sessionManagerMock, apiVersion: versionAPI)
         giniHealthAPI = GiniHealthAPI(documentService: documentService, paymentService: paymentService)
         giniMerchant = GiniMerchant(giniApiLib: giniHealthAPI)
     }
