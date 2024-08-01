8a9fa663f2bf14772a4d6a81223a4fdc50a63edb
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-08-01 19:00:45 +0200

    fix(GiniBankSDK): rename TextFieldWithoutActions -> TextFieldActionsDisabled
    PP-658

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Components/TextFieldActionsDisabled.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Components/TextFieldActionsDisabled.swift
@@ -1,12 +1,12 @@
 //
-//  TextFieldWithoutActions.swift
+//  TextFieldActionsDisabled.swift
 //
 //  Copyright © 2024 Gini GmbH. All rights reserved.
 //
 
 import UIKit
 
-class TextFieldWithoutActions: UITextField {
+class TextFieldActionsDisabled: UITextField {
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
         return false
     }
