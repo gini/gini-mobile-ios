9bd6fae862704a9db6bd0131a556ad137e628c46
Valentina Iancu <valentina.iancu@gini.net>
2024-08-02 13:26:08 +0200

    ci(ui-automation): Cleanup `GiniBankSDK` to contain only things related to it and just accessibility identifiers

---
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExampleUITests/AccessibilityIdentifiers/MainScreenAccessibilityIdentifiers.swift
@@ -0,0 +1,16 @@
+//
+//  MainScreenAccessibilityIdentifiers.swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+
+import Foundation
+public enum MainScreenAccessibilityIdentifiers: String {
+    case photoPaymentButton = "photoPaymentButtonIdentifier"
+    case cameraIconButton = "cameraIconButtonIdentifier"
+    case ibanTextField = "ibanTextFieldIdentifier"
+    case welcomeTextTitle = "welcomeTextTitleIdentifier"
+    case descriptionTextTitle = "descriptionTextTitleIdentifier"
+    case metaInformationLabel = "metaInformationLabelIdentifier"
+}
