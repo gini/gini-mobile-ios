da42f98693ea386f3476cdc8905cabf2ad7fa33b
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-08-07 17:20:30 +0200

    feat(GiniBankSDK): SkontoHelpFooterView
    PP-684

---
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Help/Views/SkontoHelpFooterView.swift
@@ -0,0 +1,42 @@
+//
+//  SkontoHelpFooterView.swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+import UIKit
+
+class SkontoHelpFooterView: UIView {
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = NSLocalizedStringPreferredGiniBankFormat("ginibank.skonto.help.footer.title",
+                                                                   comment: "Snap a photo of your invoice...")
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = .giniColorScheme().text.secondary.uiColor()
+        titleLabel.font = configuration.textStyleFonts[.body]
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.numberOfLines = 0
+        return titleLabel
+    }()
+
+    private let configuration = GiniBankConfiguration.shared
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupView() {
+        addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
