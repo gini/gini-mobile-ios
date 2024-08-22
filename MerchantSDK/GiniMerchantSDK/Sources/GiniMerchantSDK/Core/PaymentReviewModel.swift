5e871c6225bc2139e4ba80b16daf04f0f845a4fa
Anton Selivonchyk <anton.selyvonchyk.ext@gini.net>
2024-08-22 17:00:35 +0200

    fix(GiniMerchantSDKExample): Restore document collection EC-112

--- MerchantSDK/GiniMerchantSDK/Sources/GiniMerchantSDK/Core/PaymentReviewModel.swift
+++ MerchantSDK/GiniMerchantSDK/Sources/GiniMerchantSDK/Core/PaymentReviewModel.swift
@@ -20,7 +20,10 @@ protocol PaymentReviewViewModelDelegate: AnyObject {
   */
 public class PaymentReviewModel: NSObject {
 
+    var onPreviewImagesFetched: (() -> Void)?
+    var reloadCollectionViewClosure: (() -> Void)?
     var updateLoadingStatus: (() -> Void)?
+    var updateImagesLoadingStatus: (() -> Void)?
 
     var onErrorHandling: ((_ error: GiniMerchantError) -> Void)?
 
@@ -37,9 +40,25 @@ public class PaymentReviewModel: NSObject {
     private var merchantSDK: GiniMerchant
     private var selectedPaymentProvider: PaymentProvider?
 
+    private var cellViewModels: [PageCollectionCellViewModel] = [PageCollectionCellViewModel]() {
+        didSet {
+            self.reloadCollectionViewClosure?()
+        }
+    }
+
+    var numberOfCells: Int {
+        return cellViewModels.count
+    }
+
     var isLoading: Bool = false {
         didSet {
             self.updateLoadingStatus?()
+        }
+    }
+
+    var isImagesLoading: Bool = false {
+        didSet {
+            self.updateImagesLoadingStatus?()
         }
     }
 
@@ -53,6 +72,14 @@ public class PaymentReviewModel: NSObject {
         self.paymentInfo = paymentInfo
         self.selectedPaymentProvider = selectedPaymentProvider
         self.paymentComponentsController = paymentComponentsController
+    }
+
+    func getCellViewModel(at indexPath: IndexPath) -> PageCollectionCellViewModel {
+        return cellViewModels[indexPath.section]
+    }
+
+    private func createCellViewModel(previewImage: UIImage) -> PageCollectionCellViewModel {
+        return PageCollectionCellViewModel(preview: previewImage)
     }
 
     func sendFeedback(updatedExtractions: [Extraction]) {
@@ -91,6 +118,51 @@ public class PaymentReviewModel: NSObject {
     func openPaymentProviderApp(requestId: String, universalLink: String) {
         merchantSDK.openPaymentProviderApp(requestID: requestId, universalLink: universalLink)
     }
+
+    func fetchImages() {
+        guard let document else { return }
+        self.isImagesLoading = true
+        let dispatchGroup = DispatchGroup()
+        let dispatchQueue = DispatchQueue(label: "imagesQueue")
+        let dispatchSemaphore = DispatchSemaphore(value: 0)
+        var vms = [PageCollectionCellViewModel]()
+        dispatchQueue.async {
+            for page in 1 ... document.pageCount {
+                dispatchGroup.enter()
+
+                self.merchantSDK.documentService.preview(for: document.id, pageNumber: page) { [weak self] result in
+                    if let cellModel = self?.proccessPreview(result) {
+                        vms.append(cellModel)
+                    }
+                    dispatchSemaphore.signal()
+                    dispatchGroup.leave()
+                }
+                dispatchSemaphore.wait()
+            }
+
+            dispatchGroup.notify(queue: dispatchQueue) {
+                DispatchQueue.main.async {
+                    self.isImagesLoading = false
+                    self.cellViewModels.append(contentsOf: vms)
+                    self.onPreviewImagesFetched?()
+                }
+            }
+        }
+    }
+
+    private func proccessPreview(_ result: Result<Data, GiniError>) -> PageCollectionCellViewModel? {
+        switch result {
+        case let .success(dataImage):
+            if let image = UIImage(data: dataImage) {
+               return createCellViewModel(previewImage: image)
+            }
+        case let .failure(error):
+            if let delegate = merchantSDK.delegate, delegate.shouldHandleErrorInternally(error: GiniMerchantError.apiError(error)) {
+                onErrorHandling?(.apiError(error))
+            }
+        }
+        return nil
+    }
 }
 
 extension PaymentReviewModel: InstallAppBottomViewProtocol {
@@ -103,4 +175,12 @@ extension PaymentReviewModel: ShareInvoiceBottomViewProtocol {
     func didTapOnContinueToShareInvoice() {
         viewModelDelegate?.obtainPDFFromPaymentRequest()
     }
+}
+
+/**
+ View model class for collection view cell
+
+  */
+public struct PageCollectionCellViewModel {
+    let preview: UIImage
 }
