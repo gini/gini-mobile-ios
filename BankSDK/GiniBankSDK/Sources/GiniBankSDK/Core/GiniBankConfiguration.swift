224bc6ded1e7b05a1820a69c67cc75a5d9e87a8e
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-08-27 10:22:26 +0200

    feat(GiniBankSDK): remove extra spacing in configuration
    PP-409

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankConfiguration.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankConfiguration.swift
@@ -296,7 +296,7 @@ public final class GiniBankConfiguration: NSObject {
      Set an adapter implementation to show a custom bottom navigation bar on the digital invoice overview screen.
      */
     public var digitalInvoiceNavigationBarBottomAdapter: DigitalInvoiceNavigationBarBottomAdapter?
-    
+
     /**
      Set an adapter implementation to show a custom bottom navigation bar on the digital invoice Skonto screen.
      */
