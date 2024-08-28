7660762297ff15fd5c8e69cf42dda06a8e0fc1c6
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-08-28 16:31:52 +0200

    feat(GiniBankSDK): decompose setupConstraints() into small methods for DigitalInvoiceBottomNavigationBar
    PP-409

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/ReturnAssistant/DigitalInvoice/Bottom navigation/DigitalInvoiceBottomNavigationBar.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/ReturnAssistant/DigitalInvoice/Bottom navigation/DigitalInvoiceBottomNavigationBar.swift
@@ -169,38 +169,67 @@ final class DigitalInvoiceBottomNavigationBar: UIView {
     }
 
     private func setupConstraints() {
+        setupContentViewConstraints()
+        setupDividerViewConstraints()
+        setupTotalLabelConstraints()
+        setupTotalValueLabelConstraints()
+        setupSavingsAmountLabelConstraints()
+        setupSkontoBadgeViewConstraints()
+        setupHelpButtonConstraints()
+        setupProceedButtonConstraints()
+    }
+
+    private func setupContentViewConstraints() {
         let multiplier: CGFloat = UIDevice.current.isIpad ? Constants.tabletWidthMultiplier : 1.0
 
         NSLayoutConstraint.activate([
             contentView.centerXAnchor.constraint(equalTo: centerXAnchor),
             contentView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: multiplier),
-            contentView.topAnchor.constraint(equalTo: topAnchor),
+            contentView.topAnchor.constraint(equalTo: topAnchor)
+        ])
+    }
 
+    private func setupDividerViewConstraints() {
+        NSLayoutConstraint.activate([
             dividerView.topAnchor.constraint(equalTo: contentView.topAnchor),
             dividerView.leadingAnchor.constraint(equalTo: leadingAnchor),
             dividerView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            dividerView.heightAnchor.constraint(equalToConstant: Constants.dividerViewHeight),
+            dividerView.heightAnchor.constraint(equalToConstant: Constants.dividerViewHeight)
+        ])
+    }
 
+    private func setupTotalLabelConstraints() {
+        NSLayoutConstraint.activate([
             totalLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.padding),
-            totalLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
-                                                constant: Constants.padding),
+            totalLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.padding),
             totalLabel.trailingAnchor.constraint(lessThanOrEqualTo: skontoBadgeView.leadingAnchor,
-                                                 constant: -Constants.badgeHorizontalPadding),
+                                                 constant: -Constants.badgeHorizontalPadding)
+        ])
+    }
 
+    private func setupTotalValueLabelConstraints() {
+        NSLayoutConstraint.activate([
             totalValueLabel.topAnchor.constraint(equalTo: totalLabel.bottomAnchor,
                                                  constant: Constants.totalValueLabelTopPadding),
-            totalValueLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
-                                                     constant: Constants.padding),
+            totalValueLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.padding),
             totalValueLabel.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor,
-                                                      constant: -Constants.padding),
+                                                      constant: -Constants.padding)
+        ])
+    }
 
+    private func setupSavingsAmountLabelConstraints() {
+        NSLayoutConstraint.activate([
             savingsAmountLabel.topAnchor.constraint(equalTo: totalValueLabel.bottomAnchor,
-                                                  constant: Constants.savingsAmountLabelTopPadding),
+                                                    constant: Constants.savingsAmountLabelTopPadding),
             savingsAmountLabel.leadingAnchor.constraint(equalTo: totalValueLabel.leadingAnchor),
             savingsAmountLabel.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor,
                                                          constant: -Constants.padding),
-            savingsAmountLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            savingsAmountLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
 
+    private func setupSkontoBadgeViewConstraints() {
+        NSLayoutConstraint.activate([
             skontoBadgeView.centerYAnchor.constraint(equalTo: totalLabel.centerYAnchor),
             skontoBadgeView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
                                                       constant: -Constants.padding),
@@ -212,30 +241,32 @@ final class DigitalInvoiceBottomNavigationBar: UIView {
             skontoBadgeLabel.leadingAnchor.constraint(equalTo: skontoBadgeView.leadingAnchor,
                                                       constant: Constants.badgeHorizontalPadding),
             skontoBadgeLabel.trailingAnchor.constraint(equalTo: skontoBadgeView.trailingAnchor,
-                                                       constant: -Constants.badgeHorizontalPadding),
+                                                       constant: -Constants.badgeHorizontalPadding)
+        ])
+    }
 
-            helpButton.buttonView.trailingAnchor.constraint(equalTo: trailingAnchor,
-                                                           constant: -Constants.padding),
-            helpButton.buttonView.centerYAnchor.constraint(equalTo: proceedButton.centerYAnchor),
+    private func setupHelpButtonConstraints() {
+        NSLayoutConstraint.activate([
+            helpButton.buttonView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.padding),
+            helpButton.buttonView.centerYAnchor.constraint(equalTo: proceedButton.centerYAnchor)
+        ])
+    }
 
-            proceedButton.topAnchor.constraint(equalTo: contentView.bottomAnchor,
-                                               constant: Constants.proceedButtonTopPadding),
+    private func setupProceedButtonConstraints() {
+        NSLayoutConstraint.activate([
+            proceedButton.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: Constants.proceedButtonTopPadding),
             proceedButton.centerXAnchor.constraint(equalTo: centerXAnchor),
-            proceedButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor,
-                                                  constant: -Constants.verticalPadding),
+            proceedButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -Constants.verticalPadding),
             proceedButton.heightAnchor.constraint(equalToConstant: Constants.proceedButtonHeight)
         ])
         if UIDevice.current.isIpad {
             NSLayoutConstraint.activate([
-                proceedButton.widthAnchor.constraint(equalTo: contentView.widthAnchor,
-                                                     constant: -Constants.padding * 2)
+                proceedButton.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant: -Constants.padding * 2)
             ])
         } else {
             NSLayoutConstraint.activate([
-                proceedButton.widthAnchor.constraint(lessThanOrEqualTo: contentView.widthAnchor,
-                                                     constant: -Constants.padding * 2),
-                proceedButton.trailingAnchor.constraint(equalTo: helpButton.buttonView.leadingAnchor,
-                                                        constant: -Constants.helpButtonHorizontalPadding)
+                proceedButton.widthAnchor.constraint(lessThanOrEqualTo: contentView.widthAnchor, constant: -Constants.padding * 2),
+                proceedButton.trailingAnchor.constraint(equalTo: helpButton.buttonView.leadingAnchor, constant: -Constants.helpButtonHorizontalPadding)
             ])
         }
     }
