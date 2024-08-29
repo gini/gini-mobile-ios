edf67b2a4f189a5760e15145a1fb16792a6a3508
vamelkaqa <vladislav.amelka@distcotech.com>
2024-08-29 18:28:23 +0200

    ci (ui-automation): Fix file upload for iPads and iPhones

--- BankSDK/GiniBankSDKExample/GiniBankSDKExampleUITests/Screens/MainScreen.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExampleUITests/Screens/MainScreen.swift
@@ -18,6 +18,7 @@ public class MainScreen {
     let deleteButton: XCUIElement
     let sendFeedbackButton: XCUIElement
     let recentsButton: XCUIElement
+    let recentsText: XCUIElement
     
     public init(app: XCUIApplication, locale: String) {
         self.app = app
@@ -26,11 +27,13 @@ public class MainScreen {
         case "en":
             deleteButton = app.buttons["Delete"]
             sendFeedbackButton = app.navigationBars.buttons["Send feedback and close"]
-            recentsButton = app.staticTexts["Recents"]
+            recentsButton = app.buttons["Recents"]
+            recentsText = app.staticTexts["Recents"]
         case "de":
             deleteButton = app.buttons["Löschen"]
             sendFeedbackButton = app.navigationBars.buttons["Feedback senden und schließen"]
-            recentsButton = app.staticTexts["Verlauf"]
+            recentsButton = app.buttons["Verlauf"]
+            recentsText = app.staticTexts["Verlauf"]
         default:
             fatalError("Locale \(locale) is not supported")
         }
@@ -187,7 +190,13 @@ public class MainScreen {
     }
     
     func tapFileWithName(fileName: String) {
-        recentsButton.tap()
+
+        if  recentsButton.exists {
+            recentsButton.tap()
+        } else if recentsText.exists {
+            recentsText.tap()
+        }
+          
         let fileElement = app.staticTexts[fileName].firstMatch
         XCTAssertTrue(fileElement.waitForExistence(timeout: 5),"Please add file with file name '\(fileName)' to the device before launching the test.")
         fileElement.tap()
