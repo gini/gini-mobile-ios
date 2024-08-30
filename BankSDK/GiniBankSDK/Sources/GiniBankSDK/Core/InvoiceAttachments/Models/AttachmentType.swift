226cdc4ab1bcc385fed434cd751e805a282cab7d
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-08-30 17:31:59 +0200

    feat(GiniBankSDK): add resources & models for attachments view
    PP-769

---
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/InvoiceAttachments/Models/AttachmentType.swift
@@ -0,0 +1,21 @@
+//
+//  AttachmentType.swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+import UIKit
+
+enum AttachmentType {
+    case image
+    case document
+
+    var icon: UIImage? {
+        switch self {
+        case .image:
+            return GiniImages.attachmentImageIcon.image
+        case .document:
+            return GiniImages.attachmentDocumentIcon.image
+        }
+    }
+}
