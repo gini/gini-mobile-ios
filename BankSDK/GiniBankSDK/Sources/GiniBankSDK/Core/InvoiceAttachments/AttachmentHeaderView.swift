9357c5b9be7f1fe68ebfe10afda2c536a4c6ce10
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-09-02 17:33:35 +0200

    feat(GiniBankSDK): AttachmentsView, AttachmentView, AttachmentHeaderView, AttachmentFooterView
    PP-769

---
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/InvoiceAttachments/AttachmentHeaderView.swift
@@ -0,0 +1,56 @@
+//
+//  AttachmentHeaderView.swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+import UIKit
+
+class AttachmentHeaderView: UIView {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = NSLocalizedStringPreferredGiniBankFormat("ginibank.invoice.attachments.title",
+                                                              comment: "Attachments")
+        label.font = configuration.textStyleFonts[.footnoteBold]
+        label.textColor = .giniColorScheme().text.primary.uiColor()
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+
+    private let configuration = GiniBankConfiguration.shared
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        addSubview(titleLabel)
+    }
+
+    private func setupConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.headerLabelLeadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                 constant: Constants.headerLabelTrailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: Constants.headerLabelTopAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: Constants.headerLabelBottomAnchor)
+        ])
+    }
+}
+
+private extension AttachmentHeaderView {
+    enum Constants {
+        static let headerLabelLeadingAnchor: CGFloat = 12
+        static let headerLabelTrailingAnchor: CGFloat = -12
+        static let headerLabelTopAnchor: CGFloat = 12
+        static let headerLabelBottomAnchor: CGFloat = -8
+    }
+}
