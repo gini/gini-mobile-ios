8a2f19fdca1aaa25e7c16f122c19facda6f3d7b6
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-09-03 15:56:45 +0200

    feat(GiniBankSDKExample): integrate attachments view to results transaction summary
    PP-769

---
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/ResultAttachmentsTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  ResultAttachmentsTableViewCell.swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+import UIKit
+import GiniBankSDK
+
+class ResultAttachmentsTableViewCell: UITableViewCell {
+    static let reuseIdentifier = "ResultAttachmentsTableViewCell"
+    
+    private lazy var attachmentsView: AttachmentsView = {
+        let view = AttachmentsView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    private func setupView() {
+        contentView.addSubview(attachmentsView)
+
+        NSLayoutConstraint.activate([
+            attachmentsView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.padding),
+            attachmentsView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Constants.padding),
+            attachmentsView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.padding),
+            attachmentsView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Constants.padding)
+        ])
+    }
+
+    func configure(delegate: AttachmentsViewDelegate?, attachments: [Attachment]) {
+        attachmentsView.delegate = delegate
+        attachmentsView.updateAttachments(attachments)
+    }
+}
+
+private extension ResultAttachmentsTableViewCell {
+    enum Constants {
+        static let padding: CGFloat = 16
+    }
+}
