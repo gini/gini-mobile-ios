27fd9fb31aac4bbd27f38490d38314fb444d45bc
Valentina Iancu <valentina.iancu@gini.net>
2024-09-04 09:20:21 +0200

    feat(GiniBankAPILibrary): Bumps version 3.3.0
    PP-792

--- BankAPILibrary/GiniBankAPILibraryPinning/Package-release.swift
+++ BankAPILibrary/GiniBankAPILibraryPinning/Package-release.swift
@@ -17,7 +17,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         
         .package(name: "TrustKit", url: "https://github.com/datatheorem/TrustKit.git", from: "2.0.0"),
-        .package(name: "GiniBankAPILibrary", url: "https://github.com/gini/bank-api-library-ios.git", .exact("3.2.0")),
+        .package(name: "GiniBankAPILibrary", url: "https://github.com/gini/bank-api-library-ios.git", .exact("3.3.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
