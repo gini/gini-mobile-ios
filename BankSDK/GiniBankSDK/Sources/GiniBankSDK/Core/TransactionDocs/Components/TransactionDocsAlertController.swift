a69dd9e942e1d6eae15f1287495ad94511b0c0c7
Valentina Iancu <valentina.iancu@gini.net>
2024-09-12 15:51:01 +0200

    refactor(GiniBankSDK): Rename `TransactionDocsAlert` to `TransactionDocsAlertController`
    PP-809

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/TransactionDocs/Components/TransactionDocsAlertController.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/TransactionDocs/Components/TransactionDocsAlertController.swift
@@ -1,20 +1,20 @@
 //
-//  TransactionDocsAlert.swift
+//  TransactionDocsAlertController.swift
 //
 //  Copyright © 2024 Gini GmbH. All rights reserved.
 //
 
 import UIKit
 
-class TransactionDocsAlert {
+class TransactionDocsAlertController {
     static func show(on viewController: UIViewController,
                      alwaysAttachHandler: @escaping () -> Void,
                      attachOnceHandler: @escaping () -> Void,
                      doNotAttachHandler: @escaping () -> Void) {
-        let actionSheet = UIAlertController(title: Constants.title,
-                                            message: Constants.message,
-                                            preferredStyle: .alert)
-        actionSheet.view.tintColor = .GiniBank.accent1
+        let alterController = UIAlertController(title: Constants.title,
+                                                message: Constants.message,
+                                                preferredStyle: .alert)
+        alterController.view.tintColor = .GiniBank.accent1
 
         let alwaysAttachAction = UIAlertAction(title: AttachmentOption.alwaysAttach.title, style: .default) { _ in
             alwaysAttachHandler()
@@ -25,16 +25,16 @@ class TransactionDocsAlert {
         let doNotAttachAction = UIAlertAction(title: AttachmentOption.doNotAttach.title, style: .cancel) { _ in
             doNotAttachHandler()
         }
-        actionSheet.addAction(alwaysAttachAction)
-        actionSheet.addAction(attachOnce)
-        actionSheet.addAction(doNotAttachAction)
-        actionSheet.preferredAction = alwaysAttachAction
+        alterController.addAction(alwaysAttachAction)
+        alterController.addAction(attachOnce)
+        alterController.addAction(doNotAttachAction)
+        alterController.preferredAction = alwaysAttachAction
 
-        viewController.present(actionSheet, animated: true, completion: nil)
+        viewController.present(alterController, animated: true, completion: nil)
     }
 }
 
-private extension TransactionDocsAlert {
+private extension TransactionDocsAlertController {
     enum Constants {
         static let title = NSLocalizedStringPreferredGiniBankFormat("ginibank.transactionDocs.alert.title",
                                                                     comment: "Add an attachment to this transaction")
