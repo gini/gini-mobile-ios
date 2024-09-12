4f6a3afc18818ec2056df55c63de278ea0be4d2c
Valentina Iancu <valentina.iancu@gini.net>
2024-09-12 15:51:01 +0200

    refactor(GiniBankSDK): Rename `TransactionDocsAlert` to `TransactionDocsAlertController`
    PP-809

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankNetworkingScreenApiCoordinator.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankNetworkingScreenApiCoordinator.swift
@@ -667,16 +667,17 @@ extension GiniBankNetworkingScreenApiCoordinator: SkontoCoordinatorDelegate {
 }
 
 extension GiniBankNetworkingScreenApiCoordinator {
-    private func handleTransactionDocsAlertIfNeeded(on controller: UIViewController, action: @escaping () -> Void) {
+    private func handleTransactionDocsAlertIfNeeded(on controller: UIViewController, 
+                                                    action: @escaping () -> Void) {
         let savedConfiguration = GiniBankUserDefaultsStorage.clientConfiguration
         let transactionDocsEnabled = savedConfiguration?.transactionDocsEnabled ?? false
 
         if transactionDocsEnabled && self.giniBankConfiguration.transactionDocsEnabled {
             // MARK: same action for all handlers, before backend will be implemented
-            TransactionDocsAlert.show(on: controller,
-                                      alwaysAttachHandler: action,
-                                      attachOnceHandler: action,
-                                      doNotAttachHandler: action)
+            TransactionDocsAlertController.show(on: controller,
+                                                alwaysAttachHandler: action,
+                                                attachOnceHandler: action,
+                                                doNotAttachHandler: action)
         } else {
             action()
         }
