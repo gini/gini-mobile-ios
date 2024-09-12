5460ea4f591451fd226907c7543d5f3b56227cfe
Igor <igor.zharii.ext@gini.net>
2024-09-12 09:43:30 +0200

    fix(GiniBankSDK): PP-401 add alert delay to processExternalDocument (previously was only in processExternalDocumentFromPhotos)

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/AppCoordinator.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/AppCoordinator.swift
@@ -118,8 +118,11 @@ final class AppCoordinator: Coordinator {
                 do {
                     try GiniCapture.validate(document,
                                              withConfig: captureConfiguration)
-                    self.showOpenWithSwitchDialog(
-                        for: [GiniCapturePage(document: document, error: nil)])
+                    // waiting one second for controllers to dismiss, and we can't use swift concurrency in ios 12 for `popToRootViewControllerIfNeeded`
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        self.showOpenWithSwitchDialog(
+                            for: [GiniCapturePage(document: document, error: nil)])
+                    }
                 } catch  {
                     self.rootViewController.showErrorDialog(for: error, positiveAction: nil)
                 }            // When a document is imported with "Open with", a dialog allowing to choose between both APIs
@@ -304,7 +307,7 @@ final class AppCoordinator: Coordinator {
     fileprivate func popToRootViewControllerIfNeeded() {
         self.childCoordinators.forEach { coordinator in
             // there can be another controller presented, like action sheet or document picker
-            coordinator.rootViewController.presentedViewController?.dismiss(animated: true)
+            coordinator.rootViewController.presentedViewController?.dismiss(animated: false)
             coordinator.rootViewController.dismiss(animated: true)
             self.remove(childCoordinator: coordinator)
         }
