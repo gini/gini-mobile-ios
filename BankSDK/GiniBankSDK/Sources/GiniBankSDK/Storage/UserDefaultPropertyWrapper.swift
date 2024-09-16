4a1c319f535d07c665fec65830c8a046cd6aa176
Valentina Iancu <valentina.iancu@gini.net>
2024-09-16 15:20:33 +0200

    feat(GiniBankSDK):  Handle primitive types directly in `UserDefaultPropertyWrapper`
    - we need to handle primitives directly since our customers doesn't know Gini objects structure and this will easy the access of the key we saved in UserDefaults for `alwaysAttachDocs
    
    PP-809

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Storage/UserDefaultPropertyWrapper.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Storage/UserDefaultPropertyWrapper.swift
@@ -19,9 +19,18 @@ struct UserDefault<T: Codable> {
 
     var wrappedValue: T {
         get {
-            if let value = UserDefaults.standard.object(forKey: key) as? T {
-                return value
+            // Handle primitive types directly
+            if T.self == Bool.self {
+                return (UserDefaults.standard.bool(forKey: key) as? T) ?? defaultValue
+            } else if T.self == Int.self {
+                return (UserDefaults.standard.integer(forKey: key) as? T) ?? defaultValue
+            } else if T.self == Double.self {
+                return (UserDefaults.standard.double(forKey: key) as? T) ?? defaultValue
+            } else if T.self == String.self {
+                return (UserDefaults.standard.string(forKey: key) as? T) ?? defaultValue
             }
+
+            // Handle Codable types with JSON encoding/decoding
             if let data = UserDefaults.standard.data(forKey: key),
                let decodedValue = try? JSONDecoder().decode(T.self, from: data) {
                 return decodedValue
@@ -29,10 +38,22 @@ struct UserDefault<T: Codable> {
             return defaultValue
         }
         set {
-            if let encodedValue = try? JSONEncoder().encode(newValue) {
-                UserDefaults.standard.set(encodedValue, forKey: key)
+            // Handle primitive types directly
+            if let boolValue = newValue as? Bool {
+                UserDefaults.standard.set(boolValue, forKey: key)
+            } else if let intValue = newValue as? Int {
+                UserDefaults.standard.set(intValue, forKey: key)
+            } else if let doubleValue = newValue as? Double {
+                UserDefaults.standard.set(doubleValue, forKey: key)
+            } else if let stringValue = newValue as? String {
+                UserDefaults.standard.set(stringValue, forKey: key)
             } else {
-                UserDefaults.standard.set(newValue, forKey: key)
+                // Handle Codable types with JSON encoding
+                if let encodedValue = try? JSONEncoder().encode(newValue) {
+                    UserDefaults.standard.set(encodedValue, forKey: key)
+                } else {
+                    UserDefaults.standard.set(newValue, forKey: key)
+                }
             }
         }
     }
