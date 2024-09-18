0b3cd44f6a6ba3041629e0b4ed56fe39b53b8652
Valentina Iancu <valentina.iancu@gini.net>
2024-09-18 17:21:06 +0200

    feat(GiniBankSDK): Update and rename user defaults
    PP-809

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Storage/GiniBankUserDefaultsStorage.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Storage/GiniBankUserDefaultsStorage.swift
@@ -9,10 +9,14 @@ import Foundation
 import GiniBankAPILibrary
 
 struct GiniBankUserDefaultsStorage {
-    @UserDefault("ginibank.defaults.client.configurations", defaultValue: nil)
+    @GiniUserDefault("ginibank.defaults.client.configurations", defaultValue: nil)
     static var clientConfiguration: ClientConfiguration?
 
     // Bool should be saved as a primitive type directly
-    @UserDefault("ginibank.defaults.user.alwaysAttachDocs", defaultValue: nil)
+    @GiniUserDefault("ginibank.defaults.user.alwaysAttachDocs", defaultValue: nil)
     static var alwaysAttachDocs: Bool?
+    // Method to remove the value from UserDefaults
+    static func removeAlwaysAttachDocs() {
+        GiniBankUserDefaultsStorage.alwaysAttachDocs = nil
+    }
 }
