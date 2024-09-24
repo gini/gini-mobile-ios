09d79787c3f7acadbb3e9a0798a5c97b95a094d3
Valentina Iancu <valentina.iancu@gini.net>
2024-09-24 21:20:10 +0200

    fix(GiniBankSDKExample): Add boarder and paddings to attachment view to look like in Figma
    PP-809

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/TransactionSummary/AttachmentsTableViewCell.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/TransactionSummary/AttachmentsTableViewCell.swift
@@ -6,10 +6,22 @@
 
 import UIKit
 import GiniBankSDK
+import GiniCaptureSDK
 
 class AttachmentsTableViewCell: UITableViewCell {
     static let reuseIdentifier = "AttachmentsTableViewCell"
     
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.layer.borderColor = Constants.containerViewBorderColor.cgColor
+        view.layer.borderWidth = Constants.containerViewBorderWidth
+        view.layer.cornerRadius = Constants.containerViewBorderCornerRadius
+        view.backgroundColor = GiniColor(light: .GiniBank.light1,
+                                         dark: .GiniBank.dark3).uiColor()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     private lazy var attachmentsView: TransactionDocsView = {
         let view = TransactionDocsView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -28,13 +40,27 @@ class AttachmentsTableViewCell: UITableViewCell {
 
     private func setupView() {
         selectionStyle = .none
-        contentView.addSubview(attachmentsView)
+        addSubview(containerView)
+        containerView.addSubview(attachmentsView)
 
         NSLayoutConstraint.activate([
-            attachmentsView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.padding),
-            attachmentsView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Constants.padding),
-            attachmentsView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.padding),
-            attachmentsView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Constants.padding)
+            containerView.leadingAnchor.constraint(equalTo: leadingAnchor,
+                                                   constant: Constants.padding),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                    constant: -Constants.padding),
+            containerView.topAnchor.constraint(equalTo: topAnchor,
+                                               constant: Constants.padding),
+            containerView.bottomAnchor.constraint(equalTo: bottomAnchor,
+                                                  constant: -Constants.padding),
+
+            attachmentsView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor,
+                                                     constant: Constants.containerPadding),
+            attachmentsView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor,
+                                                      constant: -Constants.containerPadding),
+            attachmentsView.topAnchor.constraint(equalTo: containerView.topAnchor,
+                                                 constant: Constants.containerPadding),
+            attachmentsView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor,
+                                                    constant: -Constants.containerPadding)
         ])
     }
 
@@ -46,5 +72,10 @@ class AttachmentsTableViewCell: UITableViewCell {
 private extension AttachmentsTableViewCell {
     enum Constants {
         static let padding: CGFloat = 16
+        static let containerViewBorderColor: UIColor = GiniColor(light: .GiniBank.light3,
+                                                                 dark: .GiniBank.dark4).uiColor()
+        static let containerViewBorderWidth: CGFloat = 1.0
+        static let containerViewBorderCornerRadius: CGFloat = 8.0
+        static let containerPadding: CGFloat = 12
     }
 }
