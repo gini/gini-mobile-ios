d233150744c3fb1ae7a7fc05fa058734f78098a6
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-10-07 16:58:42 +0200

    feat(GiniBankSDK): finalise GiniColorScheme: SkontoHelpItemView
    PP-633

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Help/Views/SkontoHelpItemView.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/Help/Views/SkontoHelpItemView.swift
@@ -77,7 +77,7 @@ final class SkontoHelpItemView: UIView {
     private func addDividerView() {
         let dividerView = UIView()
         dividerView.translatesAutoresizingMaskIntoConstraints = false
-        dividerView.backgroundColor = .giniColorScheme().bottomBar.border.uiColor()
+        dividerView.backgroundColor = .giniColorScheme().textField.border.uiColor()
         addSubview(dividerView)
         NSLayoutConstraint.activate([
             dividerView.bottomAnchor.constraint(equalTo: bottomAnchor),
