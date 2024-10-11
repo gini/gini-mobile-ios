a7b94de5aa6ad76cabf70ae40fbd04b9a3e14a25
Valentina Iancu <valentina.iancu@gini.net>
2024-10-11 10:50:37 +0200

    feat(GiniBankSDK): Add integration tests for `Skonto` feature

---
+++ BankSDK/GiniBankSDKExample/Tests/SkontoIntegrationTests.swift
@@ -0,0 +1,113 @@
+//
+//  SkontoIntegrationTests.swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+import XCTest
+
+@testable import GiniBankAPILibrary
+@testable import GiniCaptureSDK
+@testable import GiniBankSDK
+
+class SkontoIntegrationTests: BaseIntegrationTest {
+
+    func testExtractionsForSkonto() {
+        let expect = expectation(description: "The skonto details extracted from the invoice were accurately compared against the static JSON.")
+        let mockedInvoiceName = "Gini_invoice_example_skonto"
+        let captureDelegate = CaptureResultsDelegate(testCase: self, expect: expect)
+        uploadAndAnalyzeDocument(fileName: mockedInvoiceName,
+                                 delegate: captureDelegate)
+        wait(for: [expect], timeout: 60)
+    }
+
+    class CaptureResultsDelegate: GiniCaptureResultsDelegate {
+
+        let testCase: SkontoIntegrationTests
+        let expect: XCTestExpectation
+
+        init(testCase: SkontoIntegrationTests, expect: XCTestExpectation) {
+            self.testCase = testCase
+            self.expect = expect
+        }
+
+        func giniCaptureAnalysisDidFinishWith(result: AnalysisResult) {
+            let mockedInvoiceName = "result_Gini_invoice_example_skonto"
+            guard let analysisExtractionResult = testCase.analysisExtractionResult,
+                  let fixtureContainer = testCase.verifyExtractions(result: result,
+                                                              fileName: mockedInvoiceName) else {
+                return
+            }
+            testCase.verifySkontoDiscounts(result: analysisExtractionResult, fixtureContainer: fixtureContainer)
+            expect.fulfill()
+        }
+
+        func giniCaptureDidCancelAnalysis() {
+            // nothing to test here
+        }
+
+        func giniCaptureDidEnterManually() {
+            // nothing to test here
+        }
+    }
+}
+extension BaseIntegrationTest {
+
+    /**
+     Verifies the `skontoDiscounts` compound extractions against the the mocked JSON.
+
+     - Parameters:
+     - result: The `ExtractionResult` containing the extracted data.
+     - fixtureContainer: The `ExtractionsContainer` representing the expected extractions from the mocked JSON.
+     */
+    func verifySkontoDiscounts(result: ExtractionResult, fixtureContainer: ExtractionsContainer) {
+        do {
+            // Initialize SkontoDiscounts from the extraction result
+            let mappedExtractedSkontoDiscounts = try SkontoDiscounts(extractions: result)
+            // Map fixtureSkontoDiscounts from `fixtureContainer` - mock data
+            let fixtureSkontoDiscounts = fixtureContainer.compoundExtractions?.skontoDiscounts ?? []
+            // Make sure the number of discounts matches
+            XCTAssertEqual(mappedExtractedSkontoDiscounts.discounts.count,
+                           fixtureSkontoDiscounts.count,
+                           "Mismatch in the number of skonto discounts")
+
+            let extractedSkontoDiscount = mappedExtractedSkontoDiscounts.discounts.first
+            let fixtureSkontoDiscount = try fixtureSkontoDiscounts.map { try SkontoDiscountDetails(extractions: $0) }.first
+
+            // Existence check for required fields in extracted discounts
+            XCTAssertNotNil(extractedSkontoDiscount?.dueDate, "The dueDate should exist in the extracted skonto discount")
+            XCTAssertNotNil(extractedSkontoDiscount?.percentageDiscounted, "The percentageDiscounted should exist in the extracted skonto discount")
+            XCTAssertNotNil(extractedSkontoDiscount?.amountToPay, "The amountToPay should exist in the extracted skonto discount")
+            XCTAssertNotNil(extractedSkontoDiscount?.remainingDays, "The remainingDays should exist in the extracted skonto discount")
+            XCTAssertNotNil(extractedSkontoDiscount?.amountDiscounted, "The amountDiscounted should exist in the extracted skonto discount")
+            XCTAssertNotNil(extractedSkontoDiscount?.paymentMethod, "The paymentMethod should exist in the extracted skonto discount")
+            XCTAssertFalse(extractedSkontoDiscount?.boundingBoxes.isEmpty ?? true, "Bounding boxes should exist in the extracted skonto discount")
+
+            // Compare the values - keep in mind that this might fail since the extraction result might be different
+            XCTAssertEqual(extractedSkontoDiscount?.amountToPay.value,
+                           fixtureSkontoDiscount?.amountToPay.value,
+                           "Skonto amount to pay values are not equal.")
+            XCTAssertEqual(extractedSkontoDiscount?.amountToPay.currencyCode,
+                           fixtureSkontoDiscount?.amountToPay.currencyCode,
+                           "Skonto amount to pay currency is not equal.")
+            XCTAssertEqual(extractedSkontoDiscount?.percentageDiscounted,
+                           fixtureSkontoDiscount?.percentageDiscounted,
+                           "Skonto percentage discounted values are not equal.")
+            XCTAssertEqual(extractedSkontoDiscount?.dueDate,
+                           fixtureSkontoDiscount?.dueDate,
+                           "Skonto due date values are not equal.")
+            XCTAssertEqual(extractedSkontoDiscount?.amountDiscounted.value,
+                           fixtureSkontoDiscount?.amountDiscounted.value,
+                           "Skonto amount discounted values are not equal.")
+            XCTAssertEqual(extractedSkontoDiscount?.amountDiscounted.currencyCode,
+                           fixtureSkontoDiscount?.amountDiscounted.currencyCode,
+                           "Skonto amount discounted currency is not equal.")
+            XCTAssertEqual(extractedSkontoDiscount?.paymentMethod,
+                           fixtureSkontoDiscount?.paymentMethod,
+                           "Skonto payment method values are not equal.")
+
+        } catch {
+            XCTFail("Failed to map skonto discounts: \(error)")
+        }
+    }
+}
