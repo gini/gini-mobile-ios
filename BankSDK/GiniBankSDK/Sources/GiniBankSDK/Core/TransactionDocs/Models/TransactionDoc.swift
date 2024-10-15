01c9ed9305abe8396ec0ff6ae07072abc999609e
Gleb Kulik <gleb.kulik.ext@gini.net>
2024-10-15 17:51:08 +0300

    feat(GiniBankSDK): remove TD extension with createMockDocuments
    PP-839

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/TransactionDocs/Models/TransactionDoc.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/TransactionDocs/Models/TransactionDoc.swift
@@ -11,11 +11,3 @@ struct TransactionDoc {
     let fileName: String
     let type: TransactionDocType
 }
-
-extension TransactionDoc {
-    static func createMockDocuments() -> [TransactionDoc] {
-        let doc1 = TransactionDoc(documentId: "doc1", fileName: "filename1", type: .document)
-        let doc2 = TransactionDoc(documentId: "doc2", fileName: "filename2", type: .document)
-        return [doc1, doc2]
-    }
-}
