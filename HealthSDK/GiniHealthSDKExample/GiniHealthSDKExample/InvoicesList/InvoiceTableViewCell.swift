ed2ae6162396f1f08edc8e40d67b27ae26e6a12a
Razvan Capra <razvan.capra@lateralgroup.com>
2024-11-07 11:56:32 +0200

    fix(GiniHealthSDKExample): Fix constants in InvoiceTableViewCell

--- HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/InvoicesList/InvoiceTableViewCell.swift
+++ HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/InvoicesList/InvoiceTableViewCell.swift
@@ -30,7 +30,7 @@ final class InvoiceTableViewCell: UITableViewCell {
     @IBOutlet private weak var amountLabel: UILabel!
     @IBOutlet private weak var ctaButton: UIButton! {
         didSet {
-            ctaButton.roundCorners(corners: .allCorners, radius: 10)
+            ctaButton.roundCorners(corners: .allCorners, radius: Constants.ctaButtonCornerRadius)
         }
     }
     @IBAction func ctaAction(_ sender: Any) {
@@ -40,7 +40,7 @@ final class InvoiceTableViewCell: UITableViewCell {
     let rightStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        stackView.spacing = 3
+        stackView.spacing = Constants.rightStackViewSpacing
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.isUserInteractionEnabled = false
         return stackView
@@ -69,11 +69,11 @@ final class InvoiceTableViewCell: UITableViewCell {
             contentView.addSubview(rightStackView)
             NSLayoutConstraint.activate([
                 rightStackView.centerYAnchor.constraint(equalTo: ctaButton.centerYAnchor),
-                rightStackView.trailingAnchor.constraint(equalTo: ctaButton.trailingAnchor, constant: -60),
-                rightStackView.heightAnchor.constraint(equalToConstant: 20)
+                rightStackView.trailingAnchor.constraint(equalTo: ctaButton.trailingAnchor, constant: Constants.rightStackViewTrailingConstant),
+                rightStackView.heightAnchor.constraint(equalToConstant: Constants.rightStackViewHeight)
             ])
 
-            ctaButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 50)
+            ctaButton.titleEdgeInsets = Constants.ctaButtonTitleEdgeInsets
         }
     }
 
@@ -81,13 +81,13 @@ final class InvoiceTableViewCell: UITableViewCell {
         let image = UIImage(data: data)
         let imageView = UIImageView(image: image)
         imageView.contentMode = .scaleAspectFill
-        imageView.layer.cornerRadius = 4
+        imageView.layer.cornerRadius = Constants.logoImageViewCornerRadius
         imageView.clipsToBounds = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            imageView.widthAnchor.constraint(equalToConstant: 20),
-            imageView.heightAnchor.constraint(equalToConstant: 20)
+            imageView.widthAnchor.constraint(equalToConstant: Constants.logoImageViewSize),
+            imageView.heightAnchor.constraint(equalToConstant: Constants.logoImageViewSize)
         ])
 
         return imageView
@@ -95,14 +95,14 @@ final class InvoiceTableViewCell: UITableViewCell {
 
     private func createBadgeView(withNumber number: Int) -> UIView {
         let badgeView = UIView()
-        badgeView.backgroundColor = .lightGray
-        badgeView.layer.cornerRadius = 10
+        badgeView.backgroundColor = Constants.badgeViewBackgroundColor
+        badgeView.layer.cornerRadius = Constants.badgeViewCornerRadius
         badgeView.translatesAutoresizingMaskIntoConstraints = false
 
         let badgeLabel = UILabel()
         badgeLabel.text = "+\(number)"
-        badgeLabel.textColor = .black
-        badgeLabel.font = UIFont.systemFont(ofSize: 10)
+        badgeLabel.textColor = Constants.badgeLabelTextColor
+        badgeLabel.font = Constants.badgeLabelFont
         badgeLabel.translatesAutoresizingMaskIntoConstraints = false
 
         badgeView.addSubview(badgeLabel)
@@ -113,10 +113,29 @@ final class InvoiceTableViewCell: UITableViewCell {
         ])
 
         NSLayoutConstraint.activate([
-            badgeView.widthAnchor.constraint(equalToConstant: 20),
-            badgeView.heightAnchor.constraint(equalToConstant: 20)
+            badgeView.widthAnchor.constraint(equalToConstant: Constants.badgeViewSize),
+            badgeView.heightAnchor.constraint(equalToConstant: Constants.badgeViewSize)
         ])
 
         return badgeView
+    }
+}
+
+// MARK: - Constants
+
+private extension InvoiceTableViewCell {
+    enum Constants {
+        static let ctaButtonCornerRadius: CGFloat = 10
+        static let rightStackViewSpacing: CGFloat = 3
+        static let rightStackViewTrailingConstant: CGFloat = -60
+        static let rightStackViewHeight: CGFloat = 20
+        static let ctaButtonTitleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 50)
+        static let logoImageViewCornerRadius: CGFloat = 4
+        static let logoImageViewSize: CGFloat = 20
+        static let badgeViewBackgroundColor = UIColor.lightGray
+        static let badgeViewCornerRadius: CGFloat = 10
+        static let badgeLabelTextColor = UIColor.black
+        static let badgeLabelFont = UIFont.systemFont(ofSize: 10)
+        static let badgeViewSize: CGFloat = 20
     }
 }
