64f2d3cf65f6c4af4b14aee017f5063539f06f19
Igor <igor.zharii.ext@gini.net>
2024-11-22 16:23:33 +0100

    fix(GiniBankSDK): Fix naming inside tests and add fail reasons
    PP-138

--- BankSDK/GiniBankSDK/Tests/GiniBankSDKTests/NetworkingScreenApiCoordinatorTests.swift
+++ BankSDK/GiniBankSDK/Tests/GiniBankSDKTests/NetworkingScreenApiCoordinatorTests.swift
@@ -10,10 +10,13 @@
 import XCTest
 
 private class MockTokenSource: AlternativeTokenSource {
-    var tokenToReturn: Token?
+    var token: Token?
+    init(token: Token? = nil) {
+        self.token = token
+    }
     func fetchToken(completion: @escaping (Result<Token, GiniError>) -> Void) {
-        if let tokenToReturn {
-            completion(.success(tokenToReturn))
+        if let token {
+            completion(.success(token))
         } else {
             completion(.failure(.requestCancelled))
         }
@@ -46,105 +49,138 @@ private class MockTrackingDelegate: GiniCaptureTrackingDelegate {
 }
 
 final class NetworkingScreenApiCoordinatorTests: XCTestCase {
+    private var tokenSource: MockTokenSource!
+    private var resultsDelegate: MockCaptureResultsDelegate!
+    private var configuration: GiniBankConfiguration!
+    private var metadata: Document.Metadata!
+    private var trackingDelegate: MockTrackingDelegate!
+
+    override func setUp() {
+        tokenSource = makeTokenSource()
+        resultsDelegate = MockCaptureResultsDelegate()
+        configuration = GiniBankConfiguration()
+        metadata = Document.Metadata(branchId: "branch")
+        trackingDelegate = MockTrackingDelegate()
+    }
 
     func testInitWithAlternativeTokenSource() throws {
-        /// ready
-        let source = MockTokenSource()
-        let testToken = Token(expiration: .init(), scope: "scope", type: "type", accessToken: "some_totally_random_gibberish")
-        source.tokenToReturn = testToken
-        let resultsDelegate = MockCaptureResultsDelegate()
-        let cfg = GiniBankConfiguration()
-        let metadata = Document.Metadata(branchId: "branch")
-        let trackingDelegate = MockTrackingDelegate()
+        let (coordinator, service) = try makeCoordinatorAndService()
 
-        /// set
-        let coord = GiniBankNetworkingScreenApiCoordinator(alternativeTokenSource: source, resultsDelegate: resultsDelegate, configuration: cfg, documentMetadata: metadata, trackingDelegate: trackingDelegate)
-        let service = try XCTUnwrap(
-            (
-                (coord.documentService as? GiniCaptureSDK.DocumentService)?
-                    .captureNetworkService as? DefaultCaptureNetworkService
-            )?.documentService
+        // check domain
+        XCTAssertEqual(service.apiDomain.domainString, "pay-api.gini.net", "Service api domain should match our default")
+
+        // check token
+        let receivedToken = try XCTUnwrap(
+            login(service: service),
+            "Should log in successfully"
         )
-
-        /// test
-
-        // check default api domain
-        XCTAssertEqual(service.apiDomain.domainString, "pay-api.gini.net")
-
-        // check for correct token
-        let logInExpectation = self.expectation(description: "login")
-        var receivedTokenOptional: Token?
-        service.sessionManager.logIn { result in
-            switch result {
-                case .success(let token):
-                receivedTokenOptional = token
-                logInExpectation.fulfill()
-            case .failure(let error):
-                XCTFail("Failure: \(error.localizedDescription)")
-            }
-        }
-        wait(for: [logInExpectation], timeout: 1)
-
-        let receivedToken = try XCTUnwrap(receivedTokenOptional)
-        XCTAssertEqual(receivedToken, testToken)
+        XCTAssertEqual(receivedToken, tokenSource.token, "Received token should match the expected token")
 
         // check for delegates/configs
-        XCTAssertNotNil(coord.resultsDelegate as? MockCaptureResultsDelegate)
-        XCTAssertEqual(coord.giniBankConfiguration, cfg)
-        XCTAssertNotNil(coord.trackingDelegate as? MockTrackingDelegate)
-        XCTAssertEqual(coord.documentService.metadata?.headers[Document.Metadata.headerKeyPrefix + Document.Metadata.branchIdHeaderKey], "branch")
+        XCTAssertNotNil(
+            coordinator.resultsDelegate as? MockCaptureResultsDelegate,
+            "Coordinator should have correct results delegate instance"
+        )
+        XCTAssertEqual(coordinator.giniBankConfiguration, configuration, "Coordinator should have correct configuration instance")
+        XCTAssertNotNil(
+            coordinator.trackingDelegate as? MockTrackingDelegate,
+            "Coordinator should have correct tracking delegate instance"
+        )
+        XCTAssertEqual(coordinator.documentService.metadata?.headers, metadata.headers, "Metadata headers should match")
     }
 
     func testViewControllerWithAlternativeTokenSource() throws {
-        /// ready
-        let source = MockTokenSource()
-        let testToken = Token(expiration: .init(), scope: "scope", type: "type", accessToken: "some_totally_random_gibberish")
-        source.tokenToReturn = testToken
-        let resultsDelegate = MockCaptureResultsDelegate()
-        let cfg = GiniBankConfiguration()
-        let metadata = Document.Metadata(branchId: "branch")
-        let trackingDelegate = MockTrackingDelegate()
+        let (coordinator, service) = try makeCoordinatorAndService(fromViewController: true)
 
-        /// set
-        let vc = try XCTUnwrap(
-            GiniBank.viewController(withAlternativeTokenSource: source, configuration: cfg, resultsDelegate: resultsDelegate, documentMetadata: metadata, trackingDelegate: trackingDelegate) as? ContainerNavigationController
+        // check domain
+        XCTAssertEqual(service.apiDomain.domainString, "pay-api.gini.net", "Service api domain should match our default")
+
+        // check token
+        let receivedToken = try XCTUnwrap(
+            login(service: service),
+            "Should log in successfully"
         )
-        let coord = try XCTUnwrap(
-            vc.coordinator as? GiniBankNetworkingScreenApiCoordinator
+        XCTAssertEqual(receivedToken, tokenSource.token, "Received token should match the expected token")
+
+        // check for delegates/configs
+        XCTAssertNotNil(
+            coordinator.resultsDelegate as? MockCaptureResultsDelegate,
+            "Coordinator should have correct results delegate instance"
         )
-        let service = try XCTUnwrap(
-            (
-                (coord.documentService as? GiniCaptureSDK.DocumentService)?
-                    .captureNetworkService as? DefaultCaptureNetworkService
-            )?.documentService
+        XCTAssertEqual(coordinator.giniBankConfiguration, configuration, "Coordinator should have correct configuration instance")
+        XCTAssertNotNil(
+            coordinator.trackingDelegate as? MockTrackingDelegate,
+            "Coordinator should have correct tracking delegate instance"
+        )
+        XCTAssertEqual(coordinator.documentService.metadata?.headers, metadata.headers, "Metadata headers should match")
+    }
+}
+
+private extension NetworkingScreenApiCoordinatorTests {
+    func makeTokenSource() -> MockTokenSource {
+        MockTokenSource(
+            token:
+                Token(
+                    expiration: .init(),
+                    scope: "the_scope",
+                    type: "the_type",
+                    accessToken: "some_totally_random_gibberish"
+                )
+        )
+    }
+
+    func makeCoordinatorAndService(fromViewController: Bool = false) throws -> (GiniBankNetworkingScreenApiCoordinator, DefaultDocumentService) {
+        let coordinator: GiniBankNetworkingScreenApiCoordinator
+        if fromViewController {
+            let viewController = try XCTUnwrap(
+                GiniBank.viewController(
+                    withAlternativeTokenSource: tokenSource,
+                    configuration: configuration,
+                    resultsDelegate: resultsDelegate,
+                    documentMetadata: metadata,
+                    trackingDelegate: trackingDelegate
+                ) as? ContainerNavigationController,
+                "There should be an instance of `ContainerNavigationController`"
+            )
+            coordinator = try XCTUnwrap(
+                viewController.coordinator as? GiniBankNetworkingScreenApiCoordinator,
+                "The instance of `ContainerNavigationController` should have a coordinator of type `GiniBankNetworkingScreenApiCoordinator"
+            )
+        } else {
+            coordinator = GiniBankNetworkingScreenApiCoordinator(
+                alternativeTokenSource: tokenSource,
+                resultsDelegate: resultsDelegate,
+                configuration: configuration,
+                documentMetadata: metadata,
+                trackingDelegate: trackingDelegate
+            )
+        }
+        let documentService = try XCTUnwrap(
+            coordinator.documentService as? GiniCaptureSDK.DocumentService,
+            "The coordinator should have a document service of type `GiniCaptureSDK.DocumentService"
+        )
+        let captureNetworkService = try XCTUnwrap(
+            documentService.captureNetworkService as? DefaultCaptureNetworkService,
+            "The document service should have a capture network service of type `DefaultCaptureNetworkService"
         )
 
-        /// test
 
-        // check default api domain
-        XCTAssertEqual(service.apiDomain.domainString, "pay-api.gini.net")
+        return (coordinator, captureNetworkService.documentService)
+    }
 
-        // check for correct token
+    func login(service: DefaultDocumentService) throws -> Token? {
         let logInExpectation = self.expectation(description: "login")
-        var receivedTokenOptional: Token?
+        var receivedToken: Token?
         service.sessionManager.logIn { result in
             switch result {
                 case .success(let token):
-                receivedTokenOptional = token
+                receivedToken = token
                 logInExpectation.fulfill()
             case .failure(let error):
                 XCTFail("Failure: \(error.localizedDescription)")
             }
         }
         wait(for: [logInExpectation], timeout: 1)
-
-        let receivedToken = try XCTUnwrap(receivedTokenOptional)
-        XCTAssertEqual(receivedToken, testToken)
-
-        // check for delegates/configs
-        XCTAssertNotNil(coord.resultsDelegate as? MockCaptureResultsDelegate)
-        XCTAssertEqual(coord.giniBankConfiguration, cfg)
-        XCTAssertNotNil(coord.trackingDelegate as? MockTrackingDelegate)
-        XCTAssertEqual(coord.documentService.metadata?.headers[Document.Metadata.headerKeyPrefix + Document.Metadata.branchIdHeaderKey], "branch")
+        return receivedToken
     }
 }
