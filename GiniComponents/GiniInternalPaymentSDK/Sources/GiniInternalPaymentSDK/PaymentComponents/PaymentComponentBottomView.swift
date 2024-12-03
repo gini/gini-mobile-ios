ea19fc7721a680ae802cac5bc70dc5e3857d7623
Razvan Capra <razvan.capra@lateralgroup.com>
2024-12-03 16:50:43 +0100

    feat(GiniHealthSDK): Landscape mode in PaymenComponentBottomView

--- GiniComponents/GiniInternalPaymentSDK/Sources/GiniInternalPaymentSDK/PaymentComponents/PaymentComponentBottomView.swift
+++ GiniComponents/GiniInternalPaymentSDK/Sources/GiniInternalPaymentSDK/PaymentComponents/PaymentComponentBottomView.swift
@@ -14,9 +14,14 @@ public final class PaymentComponentBottomView: BottomSheetViewController {
 
     private let contentView = EmptyView()
 
+    var portraitConstraints: [NSLayoutConstraint] = []
+    var landscapeConstraints: [NSLayoutConstraint] = []
+
     public override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        // Detect the initial orientation and set up the appropriate constraints
+        setupInitialLayout()
     }
 
     public init(paymentView: UIView, bottomSheetConfiguration: BottomSheetConfiguration) {
@@ -33,16 +38,65 @@ public final class PaymentComponentBottomView: BottomSheetViewController {
         self.setContent(content: contentView)
 
         NSLayoutConstraint.activate([
-            paymentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.padding),
-            paymentView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Constants.padding),
             paymentView.topAnchor.constraint(equalTo: contentView.topAnchor),
             paymentView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
+    }
+
+    // Detect and setup initial layout based on the current orientation
+    func setupInitialLayout() {
+        let deviceOrientation = UIDevice.current.orientation
+
+        if deviceOrientation == .portrait || deviceOrientation == .portraitUpsideDown {
+            setupPortraitConstraints()
+        } else if deviceOrientation == .landscapeLeft || deviceOrientation == .landscapeRight {
+            setupLandscapeConstraints()
+        }
+    }
+
+    // Portrait Layout Constraints
+    func setupPortraitConstraints() {
+        NSLayoutConstraint.deactivate(landscapeConstraints)
+        portraitConstraints = [
+            paymentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.portraitPadding),
+            paymentView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Constants.portraitPadding)
+        ]
+        NSLayoutConstraint.activate(portraitConstraints)
+    }
+
+    // Landscape Layout Constraints
+    func setupLandscapeConstraints() {
+        NSLayoutConstraint.deactivate(portraitConstraints)
+        landscapeConstraints = [
+            paymentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.landscapePadding),
+            paymentView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Constants.landscapePadding)
+        ]
+        NSLayoutConstraint.activate(landscapeConstraints)
+    }
+
+    // Handle orientation change
+    public override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+
+        // Check the device orientation
+        let deviceOrientation = UIDevice.current.orientation
+
+        if deviceOrientation == .portrait {
+            setupPortraitConstraints() // or upsideDown based on orientation
+        } else if deviceOrientation == .landscapeLeft || deviceOrientation == .landscapeRight {
+            setupLandscapeConstraints()
+        }
+
+        // Perform layout updates with animation
+        coordinator.animate(alongsideTransition: { context in
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
 }
 
 extension PaymentComponentBottomView {
     private enum Constants {
-        static let padding = 16.0
+        static let portraitPadding = 16.0
+        static let landscapePadding = 126.0
     }
 }
