90434fa1fac676c404be71f309cc715813f2fdae
Razvan Capra <razvan.capra@lateralgroup.com>
2025-02-19 11:47:09 +0200

    fix(GiniHealthSDK): Remove clientId from client configuration object
    We are not using it
    IPC-507

--- HealthAPILibrary/GiniHealthAPILibrary/Sources/GiniHealthAPILibrary/Documents/Core/ClientConfiguration.swift
+++ HealthAPILibrary/GiniHealthAPILibrary/Sources/GiniHealthAPILibrary/Documents/Core/ClientConfiguration.swift
@@ -15,19 +15,15 @@ public struct ClientConfiguration: Codable {
     /**
      Creates a new `ClientConfiguration` instance.
      
-     - parameter clientID: A unique identifier for the client.
      - parameter communicationTone: A configuration that indicates the comunication tone of the texts. Defaults to `nil
      - parameter ingredientBrandType: A configuration that indicates the presence of the ingredient brand. Defaults to `nil`.
      */
-    public init(clientID: String,
-                communicationTone: CommunicationToneEnum? = .formal,
+    public init(communicationTone: CommunicationToneEnum? = .formal,
                 ingredientBrandType: IngredientBrandTypeEnum? = .invisible) {
-        self.clientID = clientID
         self.communicationTone = communicationTone
         self.ingredientBrandType = ingredientBrandType
     }
-    
-    public let clientID: String
+
     public var communicationTone: CommunicationToneEnum?
     public var ingredientBrandType: IngredientBrandTypeEnum?
 }
