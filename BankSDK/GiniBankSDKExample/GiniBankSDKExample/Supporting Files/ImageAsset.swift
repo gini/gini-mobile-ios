0347499f979cc6b2041383022a46d64f8af1e780
Valentina Iancu <valentina.iancu@gini.net>
2025-02-19 09:57:14 +0100

    feat(GiniBankSDKExample): Add image resource for transactionDocs attached image document
    PP-749

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/Supporting Files/ImageAsset.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/Supporting Files/ImageAsset.swift
@@ -8,6 +8,7 @@ import UIKit
 
 enum ImageAsset: String {
     case transactionDocsFileIcon = "transactionDocs_attached_document_file_icon"
+    case transactionDocsImageIcon = "transactionDocs_attached_image_icon"
 
     var image: UIImage {
         return UIImage(named: rawValue).require(hint: "Image with name \(rawValue) missing")
