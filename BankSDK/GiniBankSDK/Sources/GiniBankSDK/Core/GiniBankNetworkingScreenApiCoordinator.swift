cbe448bf4f28de52837ce54890bf3023d6442b34
Valentina Iancu <valentina.iancu@gini.net>
2025-02-21 11:41:20 +0100

    fix(GiniBankSDK): Add weak self inside a closure
    PP-926

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankNetworkingScreenApiCoordinator.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankNetworkingScreenApiCoordinator.swift
@@ -342,8 +342,8 @@ private extension GiniBankNetworkingScreenApiCoordinator {
     // MARK: - Start Analysis with Return Assistant or Skonto
 
     private func startAnalysisWithReturnAssistant(networkDelegate: GiniCaptureNetworkDelegate) {
-        documentService.startAnalysis { result in
-
+        documentService.startAnalysis {[weak self] result in
+            guard let self = self else { return }
             switch result {
             case let .success(extractionResult):
                     self.setDcoumentIdAsUserProperty()
