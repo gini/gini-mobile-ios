9a32e5027e2f9e04454aa98730cbe7e97a751a36
Valentina Iancu <valentina.iancu@gini.net>
2025-02-26 09:37:55 +0100

    refactor(GiniBankSDK): Extracts error handling into handleFailure, improving readability
    PP-194

--- BankSDK/GiniBankSDKPinningExample/Tests/BankAPILibraryPinning/GiniBankAPILibraryPinningIntegrationWrongCertificatesTests.swift
+++ BankSDK/GiniBankSDKPinningExample/Tests/BankAPILibraryPinning/GiniBankAPILibraryPinningIntegrationWrongCertificatesTests.swift
@@ -11,11 +11,11 @@ import XCTest
 class PinningWrongCertificatesIntegrationTests: XCTestCase {
     // When running from Xcode: update these environment variables in the scheme.
     // Make sure not to commit the credentials if the scheme is shared!
-    let clientId = ProcessInfo.processInfo.environment["CLIENT_ID"]!
-    let clientSecret = ProcessInfo.processInfo.environment["CLIENT_SECRET"]!
-    let paymentRequestID = "a6466506-acf1-4896-94c8-9b398d4e0ee1"
-    var giniBankAPILib: GiniBankAPI!
-    var documentService: DefaultDocumentService!
+    private let clientId = ProcessInfo.processInfo.environment["CLIENT_ID"]!
+    private let clientSecret = ProcessInfo.processInfo.environment["CLIENT_SECRET"]!
+    private let paymentRequestID = "a6466506-acf1-4896-94c8-9b398d4e0ee1"
+    private var giniBankAPILib: GiniBankAPI!
+    private var documentService: DefaultDocumentService!
 
     override func setUp() {
         let yourPublicPinningConfig = [
@@ -41,6 +41,18 @@ class PinningWrongCertificatesIntegrationTests: XCTestCase {
     func testResolvePaymentRequestFails() {
         let expect = expectation(description: "it fails to resolve the payment request due to wrong pinning certificates")
 
+        func handleFailure(error: GiniBankAPILibrary.GiniError, expectation: XCTestExpectation) {
+            switch error {
+            case .noResponse:
+                XCTAssertTrue(true, "Expected SSL pinning failure")
+            case .badRequest:
+                XCTAssertTrue(true, "Received expected bad request error due to incorrect setup")
+            default:
+                XCTFail("Expected SSL pinning failure but got \(error)")
+            }
+            expectation.fulfill()
+        }
+
         let paymentService = giniBankAPILib.paymentService()
         paymentService.resolvePaymentRequest(id: paymentRequestID,
                                              recipient: "Dr. med. Hackler",
@@ -49,17 +61,10 @@ class PinningWrongCertificatesIntegrationTests: XCTestCase {
                                              amount: "335.50:EUR",
                                              purpose: "ReNr AZ356789Z") { result in
             switch result {
-                case .success(_):
-                    XCTFail("resolving the payment request should have failed due to wrong pinning certificates")
-                case .failure(let error):
-                    if case GiniBankAPILibrary.GiniError.noResponse = error {
-                        XCTAssertTrue(true, "Expected SSL pinning failure")
-                    } else if case .badRequest = error {
-                        XCTAssertTrue(true, "Received expected bad request error due to incorrect setup")
-                    } else {
-                        XCTFail("Expected SSL pinning failure but got \(error)")
-                    }
-                    expect.fulfill()
+            case .success:
+                XCTFail("Resolving the payment request should have failed due to wrong pinning certificates")
+            case .failure(let error):
+                handleFailure(error: error, expectation: expect)
             }
         }
         wait(for: [expect], timeout: 10)
