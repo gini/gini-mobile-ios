a356d3f24b4b8910d07641cbecdbe67c02c53252
Valentina Iancu <valentina.iancu@gini.net>
2025-04-29 16:02:06 +0200

    fix(GiniCaptureSDK): Fix missing parameter in tests for `setupCamera` method from CamerPreviewViewController.
    PP-1158

--- CaptureSDK/GiniCaptureSDK/Tests/GiniCaptureSDKTests/CameraPreviewViewControllerTests.swift
+++ CaptureSDK/GiniCaptureSDK/Tests/GiniCaptureSDKTests/CameraPreviewViewControllerTests.swift
@@ -32,8 +32,9 @@ final class CameraPreviewViewControllerTests: XCTestCase {
         let camera = CameraMock(state: .unauthorized)
         cameraPreviewViewController = CameraPreviewViewController(giniConfiguration: GiniConfiguration(),
                                                                   camera: camera)
-        cameraPreviewViewController.setupCamera()
-        
+        let bottomAnchor = cameraPreviewViewController.view.bottomAnchor
+        cameraPreviewViewController.setupCamera(bottomAnchor: bottomAnchor)
+
         let notAuthorizedView = cameraPreviewViewController
             .view
             .subviews
@@ -48,8 +49,9 @@ final class CameraPreviewViewControllerTests: XCTestCase {
         giniConfiguration.qrCodeScanningEnabled = true
         cameraPreviewViewController = CameraPreviewViewController(giniConfiguration: giniConfiguration)
         _ = cameraPreviewViewController.view
-        cameraPreviewViewController.setupCamera()
-        
+        let bottomAnchor = cameraPreviewViewController.view.bottomAnchor
+        cameraPreviewViewController.setupCamera(bottomAnchor: bottomAnchor)
+
         DispatchQueue.main.async {
             let metadataOutput = self.cameraPreviewViewController.previewView.session
                 .outputs
@@ -66,7 +68,8 @@ final class CameraPreviewViewControllerTests: XCTestCase {
         giniConfiguration.debugModeOn = true
         cameraPreviewViewController = CameraPreviewViewController(giniConfiguration: giniConfiguration)
         _ = cameraPreviewViewController.view
-        cameraPreviewViewController.setupCamera()
+        let bottomAnchor = cameraPreviewViewController.view.bottomAnchor
+        cameraPreviewViewController.setupCamera(bottomAnchor: bottomAnchor)
 
         cameraPreviewViewController.captureImage { imageData, _ in
             XCTAssertNotNil(imageData, "image captured data should not be nil")
