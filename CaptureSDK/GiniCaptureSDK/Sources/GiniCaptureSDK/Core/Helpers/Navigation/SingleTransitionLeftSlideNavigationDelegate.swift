d52fe2681448e0ac911ff24a07b55eca5944438a
Gleb Kulik <gleb.kulik.ext@gini.net>
2025-04-29 13:35:14 +0200

    feat(GiniCaptureSDK): add documentation for SingleTransitionLeftSlideNavigationDelegate
    PP-1132

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Helpers/Navigation/SingleTransitionLeftSlideNavigationDelegate.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Helpers/Navigation/SingleTransitionLeftSlideNavigationDelegate.swift
@@ -6,7 +6,12 @@
 
 import UIKit
 
-class SingleTransitionLeftSlideNavigationDelegate: NSObject, UINavigationControllerDelegate {
+/*
+ SingleTransitionLeftSlideNavigationDelegate sets LeftSlideTransitionAnimator
+ as the animation controller for a single transition and then resets the delegate.
+ Can be used for applying a custom transition once without affecting future navigation actions.
+*/
+final class SingleTransitionLeftSlideNavigationDelegate: NSObject, UINavigationControllerDelegate {
     func navigationController(_ navigationController: UINavigationController,
                               animationControllerFor operation: UINavigationController.Operation,
                               from fromVC: UIViewController,
