8fa8e5717bbf1d53e8c53af98121ceb7f0ec4c34
Valentina Iancu <valentina.iancu@gini.net>
2025-04-30 12:59:33 +0200

    feat(GiniBankAPILibrary): Add `qrCodeEducationEnabled` flag in the client configuration object
    PP-1151

--- BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Configuration/ClientConfiguration.swift
+++ BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Configuration/ClientConfiguration.swift
@@ -11,36 +11,44 @@ import Foundation
  A struct representing configuration settings.
  
  This struct holds various configuration options that can be used to customize the behavior and features.
+
+ Note: This configuration is intended for internal use with Gini SDKs only.
  */
 public struct ClientConfiguration: Codable {
-    /**
-     Creates a new `ClientConfiguration` instance.
 
-     - parameter clientID: A unique identifier for the client.
-     - parameter userJourneyAnalyticsEnabled: A flag indicating whether user journey analytics is enabled.
-     - parameter skontoEnabled: A flag indicating whether Skonto is enabled.
-     - parameter returnAssistantEnabled: A flag indicating whether the return assistant feature is enabled.
-     - parameter transactionDocsEnabled: A flag indicating whether TransactionDocs feature is enabled.
-     - parameter instantPaymentEnabled: A flag indicating whether Instant Payment feature is enabled.
-     */
-    public init(clientID: String,
-                userJourneyAnalyticsEnabled: Bool,
-                skontoEnabled: Bool,
-                returnAssistantEnabled: Bool,
-                transactionDocsEnabled: Bool,
-                instantPaymentEnabled: Bool) {
-        self.clientID = clientID
-        self.userJourneyAnalyticsEnabled = userJourneyAnalyticsEnabled
-        self.skontoEnabled = skontoEnabled
-        self.returnAssistantEnabled = returnAssistantEnabled
-        self.transactionDocsEnabled = transactionDocsEnabled
-        self.instantPaymentEnabled = instantPaymentEnabled
-    }
-    
     public let clientID: String
     public let userJourneyAnalyticsEnabled: Bool
     public let skontoEnabled: Bool
     public let returnAssistantEnabled: Bool
     public let transactionDocsEnabled: Bool
     public let instantPaymentEnabled: Bool
+    public let qrCodeEducationEnabled: Bool
+
+    /**
+     Creates a new `ClientConfiguration` instance.
+
+     - Parameters:
+        - clientID: A unique identifier for the client.
+        - userJourneyAnalyticsEnabled: A flag indicating whether user journey analytics is enabled.
+        - skontoEnabled: A flag indicating whether Skonto is enabled.
+        - returnAssistantEnabled: A flag indicating whether the return assistant feature is enabled.
+        - transactionDocsEnabled: A flag indicating whether TransactionDocs feature is enabled.
+        - instantPaymentEnabled: A flag indicating whether Instant Payment feature is enabled.
+        - qrCodeEducationEnabled: A flag indicating whether QR code education is enabled.
+     */
+    public init(clientID: String,
+                userJourneyAnalyticsEnabled: Bool,
+                skontoEnabled: Bool,
+                returnAssistantEnabled: Bool,
+                transactionDocsEnabled: Bool,
+                instantPaymentEnabled: Bool,
+                qrCodeEducationEnabled: Bool) {
+        self.clientID = clientID
+        self.userJourneyAnalyticsEnabled = userJourneyAnalyticsEnabled
+        self.skontoEnabled = skontoEnabled
+        self.returnAssistantEnabled = returnAssistantEnabled
+        self.transactionDocsEnabled = transactionDocsEnabled
+        self.instantPaymentEnabled = instantPaymentEnabled
+        self.qrCodeEducationEnabled = qrCodeEducationEnabled
+    }
 }
