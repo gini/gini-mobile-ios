9fe5a0fa29014f6d926bec5a5d284d66b97fc092
Qazi Naveed <qazi.naveed@gini.net>
2025-05-02 11:08:33 +0200

    fix(GiniBankSDK): onboarding custom bottom nav bar support PP-1157

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingViewController.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Onboarding/OnboardingViewController.swift
@@ -14,6 +14,7 @@ class OnboardingViewController: UIViewController {
     @IBOutlet weak var collectionViewToPageControlConstraint: NSLayoutConstraint!
     @IBOutlet weak var collectionViewToViewBottomConstraint: NSLayoutConstraint!
     var bottomPaddingPageIndicatorConstraint: NSLayoutConstraint!
+    var navigationBarHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var skipBottomBarButton: MultilineTitleButton!
     private(set) var dataSource: OnboardingDataSource
     private let configuration = GiniConfiguration.shared
@@ -85,9 +86,19 @@ class OnboardingViewController: UIViewController {
         }
         if UIDevice.current.isIphone {
             if view.currentInterfaceOrientation.isLandscape {
-                nextButton?.isHidden = false
-                skipBottomBarButton?.isHidden = !configuration.bottomNavigationBarEnabled || pageControl.currentPage == dataSource.pageModels.count - 1
-                bottomNavigationBar?.isHidden = false
+                navigationBarHeightConstraint.constant = getBottomBarHeight()
+                
+                if configuration.onboardingNavigationBarBottomAdapter != nil {
+                    bottomNavigationBar?.isHidden = false
+                    nextButton?.isHidden = true
+                    skipBottomBarButton?.isHidden = true
+                } else {
+                    bottomNavigationBar?.isHidden = true
+                    nextButton?.isHidden = false
+                    skipBottomBarButton?.isHidden = !configuration.bottomNavigationBarEnabled
+                        || pageControl.currentPage == dataSource.pageModels.count - 1
+                }
+                
                 let safeareaLeftPadding = view.safeAreaInsets.left
                 let safeareaRightPadding = view.safeAreaInsets.right
 
@@ -113,6 +124,7 @@ class OnboardingViewController: UIViewController {
                 collectionViewToPageControlConstraint.isActive = false
                 collectionViewToViewBottomConstraint.isActive = true
             } else {
+                navigationBarHeightConstraint.constant = getBottomBarHeight()
                 buttonCenterXConstraint.constant = 0
                 collectionViewToViewBottomConstraint.isActive = false
                 collectionViewToPageControlConstraint.isActive = true
@@ -131,13 +143,12 @@ class OnboardingViewController: UIViewController {
             equalTo: pageControl.bottomAnchor,
             constant: getBottomPaddingForPageController()
         )
-        
+        navigationBarHeightConstraint = navigationBar.heightAnchor.constraint(equalToConstant: getBottomBarHeight())
         NSLayoutConstraint.activate([
-            bottomPaddingPageIndicatorConstraint    ,
             navigationBar.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            navigationBar.heightAnchor.constraint(equalToConstant: navigationBar.frame.height),
             navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navigationBarHeightConstraint
         ])
     }
 
@@ -366,12 +377,26 @@ class CollectionFlowLayout: UICollectionViewFlowLayout {
 private extension OnboardingViewController {
     enum Constants {
         static let pageControlBottomBarPadding: CGFloat = 46
+        static let pageControlBottomBarPaddingLandscape: CGFloat = 0
+        static let bottomBarHeightPortrait: CGFloat = 110
+        static let bottomBarHeightLandscape: CGFloat = 64
     }
     
     func getBottomPaddingForPageController() -> CGFloat {
-        if UIDevice.current.isIphone && view.currentInterfaceOrientation.isLandscape {
-            return 0
+        if isiPhoneAndLandscape() {
+            return Constants.pageControlBottomBarPaddingLandscape
         }
         return Constants.pageControlBottomBarPadding
+    }
+    
+    func getBottomBarHeight() -> CGFloat {
+        if isiPhoneAndLandscape() {
+            return Constants.bottomBarHeightLandscape
+        }
+        return Constants.bottomBarHeightPortrait
+    }
+    
+    func isiPhoneAndLandscape() -> Bool {
+        return UIDevice.current.isIphone && view.currentInterfaceOrientation.isLandscape
     }
 }
