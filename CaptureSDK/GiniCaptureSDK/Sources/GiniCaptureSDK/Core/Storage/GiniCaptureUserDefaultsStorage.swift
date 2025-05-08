44c8a198e831231a900a874d8ab2240f4352f99f
Valentina Iancu <valentina.iancu@gini.net>
2025-05-08 12:35:06 +0200

    feat(GiniCaptureSDK): Add logic to manage UserDefaults storage inside CaptureSDK
    PP-1221

---
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Storage/GiniCaptureUserDefaultsStorage.swift
@@ -0,0 +1,24 @@
+//
+//  GiniCaptureUserDefaultsStorage.swift
+//
+//  Copyright © 2025 Gini GmbH. All rights reserved.
+//
+
+import Foundation
+import GiniBankAPILibrary
+
+/**
+    This is for internal use only.
+    A struct that manages user defaults for Gini Capture configurations.
+ */
+public struct GiniCaptureUserDefaultsStorage {
+    // Configuration flag
+    @GiniCaptureUserDefault("ginicapture.defaults.client.configurations.qrCodeEducationEnabled",
+                            defaultValue: nil)
+    public static var qrCodeEducationEnabled: Bool?
+
+    // Counts how many times the education message was shown in the invoice photo flow
+    @GiniCaptureUserDefault("ginicapture.defaults.invoicephoto.messageDisplayCount",
+                            defaultValue: 0)
+    static var messageDisplayCount: Int
+}
