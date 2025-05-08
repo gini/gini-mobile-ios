876700d8a5313a0ec9cbabae9ef9f72f5a52b0da
Valentina Iancu <valentina.iancu@gini.net>
2025-05-08 15:09:46 +0200

    feat(GiniCaptureSDK): Add code documentation
    PP-1221

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/EducationFlow/EducationFlowConfiguration.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/EducationFlow/EducationFlowConfiguration.swift
@@ -4,6 +4,16 @@
 //  Copyright © 2025 Gini GmbH. All rights reserved.
 //
 
+/**
+ A configuration object that defines parameters and dependencies for an education flow.
+
+ - Parameters:
+    - maxTotalDisplays: The maximum total number of times any education message should be shown.
+    - numberOfMessages: The number of different messages to alternate between.
+    - isEducationEnabled: A closure returning whether the education flow is enabled.
+    - getDisplayCount: A closure returning the current display count.
+    - setDisplayCount: A closure to update the display count.
+ */
 struct EducationFlowConfiguration {
     let maxTotalDisplays: Int
     let numberOfMessages: Int
