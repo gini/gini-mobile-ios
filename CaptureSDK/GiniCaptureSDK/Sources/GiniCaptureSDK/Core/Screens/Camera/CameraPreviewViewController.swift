6aae4b9b388580b65fe1f9e10159a28dc27f5ed5
Valentina Iancu <valentina.iancu@gini.net>
2025-05-26 15:22:12 +0200

    fix(GiniCaptureSDK): Revert initial changes for `cameraFocusSmall` usage
    PP-1347

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/CameraPreviewViewController.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/Camera/CameraPreviewViewController.swift
@@ -488,10 +488,8 @@ extension CameraPreviewViewController {
                      exposeWithMode: .autoExpose,
                      atDevicePoint: devicePoint,
                      monitorSubjectAreaChange: true)
-        let tintColor = GiniColor(light: .GiniCapture.warning3, dark: .GiniCapture.warning3).uiColor()
-        let imageWithTindColor = cameraFocusSmall?.tintedImageWithColor(tintColor)
         let imageView =
-        createFocusIndicator(withImage: imageWithTindColor,
+        createFocusIndicator(withImage: cameraFocusSmall,
                              atPoint: previewView.videoPreviewLayer
             .layerPointConverted(fromCaptureDevicePoint: devicePoint))
         showFocusIndicator(imageView)
