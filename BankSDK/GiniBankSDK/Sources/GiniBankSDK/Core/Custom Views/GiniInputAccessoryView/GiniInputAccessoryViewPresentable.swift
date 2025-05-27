4f2d52d6523d89478361591f1c416216ccd89b93
Lester Batres <lester.batres@gini.net>
2025-05-27 17:01:44 +0200

    feat(GiniBankSDK): Apply sonarcloud suggestions.
    PP-1380

---
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Custom Views/GiniInputAccessoryView/GiniInputAccessoryViewPresentable.swift
@@ -0,0 +1,11 @@
+//
+//  GiniInputAccessoryViewPresentable.swift
+//
+//  Copyright © 2025 Gini GmbH. All rights reserved.
+//
+
+import UIKit
+
+protocol GiniInputAccessoryViewPresentable {
+    var inputAccessoryView: UIView? { get set }
+}
