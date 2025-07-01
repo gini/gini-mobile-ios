607981617ca6612696cc21648f9d6203f7863574
Valentina Iancu <valentina.iancu@gini.net>
2025-07-01 17:24:28 +0200

    refactor(GiniCaptureSDK): Refactor storing the value for `onboardingShowAtLaunch` using `GiniCaptureUserDefaultsStorage`
    PP-1222

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Storage/GiniCaptureUserDefaultsStorage.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Storage/GiniCaptureUserDefaultsStorage.swift
@@ -11,24 +11,27 @@ import GiniUtilites
  A struct that manages user defaults for Gini Capture configurations.
  */
 public struct GiniCaptureUserDefaultsStorage {
+    @GiniUserDefault("ginicapture.defaults.onboardingShowAtLaunch", defaultValue: false)
+    public static var onboardingShowAtLaunch: Bool
+
     @GiniUserDefault("ginicapture.defaults.onboardingShowed", defaultValue: false)
     public static var onboardingShowed: Bool
-    
+
     // Configuration flag for the QR code education message
     @GiniUserDefault("ginicapture.defaults.clientConfigurations.qrCodeEducationEnabled",
                      defaultValue: nil)
     public static var qrCodeEducationEnabled: Bool?
-    
+
     // Configuration flag for the E-Invoice flow
     @GiniUserDefault("ginicapture.defaults.clientConfigurations.eInvoiceEnabled",
                      defaultValue: nil)
     public static var eInvoiceEnabled: Bool?
-    
+
     // Counts how many times the education message was shown in the invoice photo flow
     @GiniUserDefault("ginicapture.defaults.captureInvoice.educationMessageDisplayCount",
                      defaultValue: 0)
     static var captureInvoiceEducationMessageDisplayCount: Int
-    
+
     // Counts how many times the education message was shown in the QR code flow
     @GiniUserDefault("ginicapture.defaults.qrCode.educationMessageDisplayCount",
                      defaultValue: 0)
