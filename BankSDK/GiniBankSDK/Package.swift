e53fb75e4f23123bb02af0498c258ba5f162e622
Valentina Iancu <valentina.iancu@gini.net>
2025-07-03 18:43:22 +0200

    feat(GiniBankSDK): Remove `GiniUtilites` from the `Package` file

--- BankSDK/GiniBankSDK/Package.swift
+++ BankSDK/GiniBankSDK/Package.swift
@@ -16,8 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "GiniCaptureSDK", path: "../../CaptureSDK/GiniCaptureSDK"),
-        .package(name: "GiniUtilites", path: "../../GiniComponents/GiniUtilites")
+        .package(name: "GiniCaptureSDK", path: "../../CaptureSDK/GiniCaptureSDK")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
