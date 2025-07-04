07ce63fca1bac1aa3e40654c9376a0cfe12fb0db
Lester Batres <lester.batres@gini.net>
2025-07-04 18:08:49 +0200

    feat(GiniInternalPaymentSDK):  Add `BottomSheetPresentable` protocol to add the ability to a `ViewController` to be presented as a `sheetController`
    IPC-665

---
+++ GiniComponents/GiniUtilites/Sources/GiniUtilites/Protocols/BottomSheetPresentable.swift
@@ -0,0 +1,44 @@
+//
+//  BottomSheetPresentable.swift
+//
+//  Copyright © 2025 Gini GmbH. All rights reserved.
+//
+
+import UIKit
+
+public typealias GiniBottomSheetViewController = UIViewController & BottomSheetPresentable
+
+public protocol BottomSheetPresentable {
+    
+    var shouldShowDragIndicator: Bool { get }
+    
+    func configureBottomSheet()
+    func updateBottomSheetHeight(_ height: CGFloat)
+}
+
+public extension BottomSheetPresentable where Self: UIViewController {
+    
+    func configureBottomSheet() {
+        if #available(iOS 15, *),
+           let presentationController = sheetPresentationController {
+            presentationController.detents = [.medium()]
+            presentationController.prefersGrabberVisible = true
+            presentationController.prefersScrollingExpandsWhenScrolledToEdge = false
+            presentationController.prefersEdgeAttachedInCompactHeight = false
+        }
+    }
+    
+    func updateBottomSheetHeight(_ height: CGFloat) {
+        if #available(iOS 16, *),
+            let presentationController = sheetPresentationController {
+            let identifier = UISheetPresentationController.Detent.Identifier("customHeight")
+            
+            let customDetent = UISheetPresentationController.Detent.custom(identifier: identifier) { context in
+                return height
+            }
+            
+            presentationController.detents = [customDetent]
+            presentationController.selectedDetentIdentifier = identifier
+        }
+    }
+}
