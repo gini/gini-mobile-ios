e98d0f34a917855f1ac355bcb4bd8048915f9dd9
Qazi Naveed <qazi.naveed@gini.net>
2025-07-25 09:12:38 +0200

    fix(GiniBankSDK): adding comments to improve clearity PP-1361

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/SkontoViewController.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/Skonto/SkontoViewController.swift
@@ -645,6 +645,8 @@ private extension SkontoViewController {
         static let scrollViewSideInset: CGFloat = 0
         static let groupCornerRadius: CGFloat = 8
         static let scrollIndicatorInset: CGFloat = 0
+        // This multiplier was chosen to accommodate 200% text scaling on iPads,
+        // ensuring proper layout and readability for Dynamic Type support.
         static let tabletWidthMultiplier: CGFloat = 0.71
         static let navigationBarViewDefaultHeight: CGFloat = 62
         static let landscapeHorizontalPadding: CGFloat = 16
