09463c99428040c6ae9ef12218bc2bc5b1e0c73c
Valentina Iancu <valentina.iancu@gini.net>
2025-08-18 19:27:14 +0200

    fix(GiniBankSDKExample):Revert force a test crash to test Crashlytics

--- BankSDK/GiniBankSDKExample/GiniBankSDKExample/DemoViewController.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExample/DemoViewController.swift
@@ -74,18 +74,6 @@ final class DemoViewController: UIViewController {
         configureSettingsButton()
 
         dismissKeyboardOnTap()
-        // Do any additional setup after loading the view, typically from a nib.
-
-        let button = UIButton(type: .roundedRect)
-        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
-        button.setTitle("Test Crash", for: [])
-        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
-        view.addSubview(button)
-    }
-
-    @IBAction func crashButtonTapped(_ sender: AnyObject) {
-        let numbers = [0]
-        let _ = numbers[1]
     }
 
     override func viewDidAppear(_ animated: Bool) {
