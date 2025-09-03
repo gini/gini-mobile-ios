ea8a2558d5abb0b48a524f9d4de6931ae1a3528f
Valentina Iancu <valentina.iancu@gini.net>
2025-09-03 18:52:17 +0200

    feat(GiniBankExampleSwiftUI): Introducing `CredentialsManger` swift file

---
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExampleSwiftUI/CredentialsManager.swift
@@ -0,0 +1,13 @@
+//
+//  CredentialsManager.swift
+//  Example Swift
+//
+//  Copyright © 2024 Gini GmbH. All rights reserved.
+//
+
+import Foundation
+
+let clientID = "client_id"
+let clientPassword = "client_password"
+let clientDomain = "client_domain"
+
