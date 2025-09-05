c3f18614089a690640cc82019ae44d72c6efaf64
Valentina Iancu <valentina.iancu@gini.net>
2025-09-05 11:11:19 +0200

    feat(GiniBankExampleSwiftUI): Migrate to swift catalogs

--- BankSDK/GiniBankSDKExample/GiniBankSDKExampleSwiftUI/Utils/Localization/Localized.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExampleSwiftUI/Utils/Localization/Localized.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 protocol Localized {
-    var localized: String { get }
     var localizationKey: String { get }
-    
     static var tableName: String? { get }
 }
 
 extension Localized {
     var localized: String {
-        return NSLocalizedString(localizationKey, tableName: Self.tableName, comment: "")
+        NSLocalizedString(localizationKey,
+                          tableName: Self.tableName,
+                          comment: "")
     }
 }
