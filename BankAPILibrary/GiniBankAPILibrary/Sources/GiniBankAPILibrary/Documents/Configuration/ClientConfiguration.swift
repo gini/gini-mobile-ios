dfc514d5a6f35b281e62565a79308bc76efcacd8
Valentina Iancu <valentina.iancu@gini.net>
2025-10-01 17:02:13 +0200

    feat(GiniBankAPILibrary): Add new bool property for Save Photos Locally feature
    PP-1901

--- BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Configuration/ClientConfiguration.swift
+++ BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Configuration/ClientConfiguration.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /**
  A struct representing configuration settings.
- 
+
  This struct holds various configuration options that can be used to customize the behavior and features.
 
  Note: This configuration is intended for internal use in Gini SDKs only.
@@ -25,20 +25,22 @@ public struct ClientConfiguration: Codable {
     public let qrCodeEducationEnabled: Bool
     public let eInvoiceEnabled: Bool
     public let paymentHintsEnabled: Bool
+    public let savePhotosLocallyEnabled: Bool
 
     /**
      Creates a new `ClientConfiguration` instance.
 
      - Parameters:
-        - clientID: A unique identifier for the client.
-        - userJourneyAnalyticsEnabled: A flag indicating whether user journey analytics is enabled.
-        - skontoEnabled: A flag indicating whether Skonto is enabled.
-        - returnAssistantEnabled: A flag indicating whether the return assistant feature is enabled.
-        - transactionDocsEnabled: A flag indicating whether TransactionDocs feature is enabled.
-        - instantPaymentEnabled: A flag indicating whether Instant Payment feature is enabled.
-        - qrCodeEducationEnabled: A flag indicating whether QR code education is enabled.
-        - eInvoiceEnabled: A flag indicating whether the E-Invoice feature is enabled.
-        - paymentHintsEnabled: A flag indicating whether the user hints are enabled.
+     - clientID: A unique identifier for the client.
+     - userJourneyAnalyticsEnabled: A flag indicating whether user journey analytics is enabled.
+     - skontoEnabled: A flag indicating whether Skonto is enabled.
+     - returnAssistantEnabled: A flag indicating whether the return assistant feature is enabled.
+     - transactionDocsEnabled: A flag indicating whether TransactionDocs feature is enabled.
+     - instantPaymentEnabled: A flag indicating whether Instant Payment feature is enabled.
+     - qrCodeEducationEnabled: A flag indicating whether QR code education is enabled.
+     - eInvoiceEnabled: A flag indicating whether the E-Invoice feature is enabled.
+     - paymentHintsEnabled: A flag indicating whether the user hints are enabled.
+     - savePhotosLocallyEnabled: A flag indicating whether saving photos locally is enabled.
      */
     public init(clientID: String,
                 userJourneyAnalyticsEnabled: Bool,
@@ -48,7 +50,8 @@ public struct ClientConfiguration: Codable {
                 instantPaymentEnabled: Bool,
                 qrCodeEducationEnabled: Bool,
                 eInvoiceEnabled: Bool,
-                paymentHintsEnabled: Bool) {
+                paymentHintsEnabled: Bool,
+                savePhotosLocallyEnabled: Bool) {
         self.clientID = clientID
         self.userJourneyAnalyticsEnabled = userJourneyAnalyticsEnabled
         self.skontoEnabled = skontoEnabled
@@ -58,5 +61,6 @@ public struct ClientConfiguration: Codable {
         self.qrCodeEducationEnabled = qrCodeEducationEnabled
         self.eInvoiceEnabled = eInvoiceEnabled
         self.paymentHintsEnabled = paymentHintsEnabled
+        self.savePhotosLocallyEnabled = savePhotosLocallyEnabled
     }
 }
