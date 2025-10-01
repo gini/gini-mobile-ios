b7e86fbbdf4d9e8ef3971b5b8b727de37dbfc6bf
Qazi Naveed <qazi.naveed@gini.net>
2025-10-01 17:47:13 +0200

    refactor(GiniHealthApiLibrary): removed force unwraping
    PP-1689

--- HealthAPILibrary/GiniHealthAPILibrary/Tests/GiniHealthAPILibraryTests/Utils.swift
+++ HealthAPILibrary/GiniHealthAPILibrary/Tests/GiniHealthAPILibraryTests/Utils.swift
@@ -40,43 +40,71 @@ func loadPaymentRequests() -> PaymentRequests {
 }
 
 func loadProvider() -> PaymentProvider {
-    let fileURLPath: String? = Bundle.module
-        .path(forResource: "provider", ofType: "json")
-    let jsonData = try? Data.init(contentsOf: URL(fileURLWithPath: fileURLPath!))
-    return try! JSONDecoder().decode(PaymentProvider.self, from: jsonData!)
+    guard let fileURLPath = Bundle.module.path(forResource: "provider", ofType: "json"),
+          let jsonData = try? Data(contentsOf: URL(fileURLWithPath: fileURLPath)),
+          let provider = try? JSONDecoder().decode(PaymentProvider.self, from: jsonData) else {
+        fatalError("Could not load provider.json in tests")
+    }
+    return provider
 }
 
 func loadProviderResponse() -> PaymentProviderResponse {
-    let fileURLPath: String? = Bundle.module
-        .path(forResource: "provider", ofType: "json")
-    let jsonData = try? Data.init(contentsOf: URL(fileURLWithPath: fileURLPath!))
-    return try! JSONDecoder().decode(PaymentProviderResponse.self, from: jsonData!)
+    guard let fileURLPath = Bundle.module.path(forResource: "provider", ofType: "json"),
+          let jsonData = try? Data(contentsOf: URL(fileURLWithPath: fileURLPath)),
+          let response = try? JSONDecoder().decode(PaymentProviderResponse.self, from: jsonData) else {
+        fatalError("Could not load PaymentProviderResponse.json in tests")
+    }
+    return response
 }
 
 func loadProviders() -> PaymentProviders {
     var providers: PaymentProviders = []
-    let fileURLPath: String? = Bundle.module
-        .path(forResource: "providers", ofType: "json")
-    let jsonData = try? Data.init(contentsOf: URL(fileURLWithPath: fileURLPath!))
-    let providersResponse = try! JSONDecoder().decode([PaymentProviderResponse].self, from: jsonData!)
+    
+    guard let fileURLPath = Bundle.module.path(forResource: "providers", ofType: "json"),
+          let jsonData = try? Data(contentsOf: URL(fileURLWithPath: fileURLPath)),
+          let providersResponse = try? JSONDecoder().decode([PaymentProviderResponse].self, from: jsonData) else {
+        fatalError("Could not load providers.json in tests")
+    }
+    
     for providerResponse in providersResponse {
-       let imageData = UIImage(named: "Gini-Test-Payment-Provider", in: Bundle.module, compatibleWith: nil)?.pngData()
-        let provider = PaymentProvider(id: providerResponse.id, name: providerResponse.name, appSchemeIOS: providerResponse.appSchemeIOS, minAppVersion: providerResponse.minAppVersion, colors: providerResponse.colors, iconData: imageData ?? Data(), appStoreUrlIOS: providerResponse.appStoreUrlIOS, universalLinkIOS: providerResponse.universalLinkIOS, index: providerResponse.index, gpcSupportedPlatforms: providerResponse.gpcSupportedPlatforms, openWithSupportedPlatforms: providerResponse.openWithSupportedPlatforms)
-            providers.append(provider)
-        }
+        let imageData = UIImage(named: "Gini-Test-Payment-Provider",
+                                in: Bundle.module,
+                                compatibleWith: nil)?.pngData() ?? Data()
+        
+        let provider = PaymentProvider(
+            id: providerResponse.id,
+            name: providerResponse.name,
+            appSchemeIOS: providerResponse.appSchemeIOS,
+            minAppVersion: providerResponse.minAppVersion,
+            colors: providerResponse.colors,
+            iconData: imageData,
+            appStoreUrlIOS: providerResponse.appStoreUrlIOS,
+            universalLinkIOS: providerResponse.universalLinkIOS,
+            index: providerResponse.index,
+            gpcSupportedPlatforms: providerResponse.gpcSupportedPlatforms,
+            openWithSupportedPlatforms: providerResponse.openWithSupportedPlatforms
+        )
+        
+        providers.append(provider)
+    }
+    
     return providers
 }
 
 func loadPaymentRequest() -> PaymentRequest {
-    let fileURLPath: String? = Bundle.module
-        .path(forResource: "paymentRequest", ofType: "json")
-    let jsonData = try? Data.init(contentsOf: URL(fileURLWithPath: fileURLPath!))
-        return try! JSONDecoder().decode(PaymentRequest.self, from: jsonData!)
+    guard let fileURLPath = Bundle.module.path(forResource: "paymentRequest", ofType: "json"),
+          let jsonData = try? Data(contentsOf: URL(fileURLWithPath: fileURLPath)),
+          let request = try? JSONDecoder().decode(PaymentRequest.self, from: jsonData) else {
+        fatalError("Could not load paymentRequest.json in tests")
+    }
+    return request
 }
 
 func loadPayment() -> Payment {
-    let fileURLPath: String? = Bundle.module
-        .path(forResource: "payment", ofType: "json")
-    let jsonData = try? Data.init(contentsOf: URL(fileURLWithPath: fileURLPath!))
-        return try! JSONDecoder().decode(Payment.self, from: jsonData!)
+    guard let fileURLPath = Bundle.module.path(forResource: "payment", ofType: "json"),
+          let jsonData = try? Data(contentsOf: URL(fileURLWithPath: fileURLPath)),
+          let payment = try? JSONDecoder().decode(Payment.self, from: jsonData) else {
+        fatalError("Could not load payment.json in tests")
+    }
+    return payment
 }
