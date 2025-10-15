07ae990f57170a563337396aca02b45d4b2dae9e
Qazi Naveed <qazi.naveed@gini.net>
2025-10-15 13:57:26 +0200

    refactor(GiniHealthApiLibrary) remove nested switch
    PP-1685

--- HealthAPILibrary/GiniHealthAPILibrary/Sources/GiniHealthAPILibrary/Documents/Payments/PaymentService.swift
+++ HealthAPILibrary/GiniHealthAPILibrary/Sources/GiniHealthAPILibrary/Documents/Payments/PaymentService.swift
@@ -323,23 +323,30 @@ extension PaymentService {
     func paymentProvider(id: String, resourceHandler: ResourceDataHandler<APIResource<PaymentProviderResponse>>,
                          completion: @escaping CompletionResult<PaymentProvider>) {
         let resource = APIResource<PaymentProviderResponse>(method: .paymentProvider(id: id), apiDomain: apiDomain, apiVersion: apiVersion, httpMethod: .get)
-
+        
         resourceHandler(resource, { result in
             switch result {
             case let .success(providerResponse):
                 self.file(urlString: providerResponse.iconLocation) { result in
-                    switch result {
-                    case let .success(imageData):
-                        let provider = PaymentProvider(id: providerResponse.id, name: providerResponse.name, appSchemeIOS: providerResponse.appSchemeIOS, minAppVersion: providerResponse.minAppVersion, colors: providerResponse.colors, iconData: imageData, appStoreUrlIOS: providerResponse.appStoreUrlIOS, universalLinkIOS: providerResponse.universalLinkIOS, index: providerResponse.index, gpcSupportedPlatforms: providerResponse.gpcSupportedPlatforms, openWithSupportedPlatforms: providerResponse.openWithSupportedPlatforms)
-                        completion(.success(provider))
-                    case let .failure(error):
-                        completion(.failure(error))
-                    }
+                    self.handleFileResult(result, providerResponse: providerResponse, completion: completion)
                 }
             case let .failure(error):
                 completion(.failure(error))
             }
         })
+    }
+
+    private func handleFileResult(_ result: Result<Data, GiniError>,
+                                  providerResponse: PaymentProviderResponse,
+                                  completion: @escaping CompletionResult<PaymentProvider>) {
+
+        switch result {
+        case let .success(imageData):
+            let provider = PaymentProvider(id: providerResponse.id, name: providerResponse.name, appSchemeIOS: providerResponse.appSchemeIOS, minAppVersion: providerResponse.minAppVersion, colors: providerResponse.colors, iconData: imageData, appStoreUrlIOS: providerResponse.appStoreUrlIOS, universalLinkIOS: providerResponse.universalLinkIOS, index: providerResponse.index, gpcSupportedPlatforms: providerResponse.gpcSupportedPlatforms, openWithSupportedPlatforms: providerResponse.openWithSupportedPlatforms)
+            completion(.success(provider))
+        case let .failure(error):
+            completion(.failure(error))
+        }
     }
 
     func createPaymentRequest(paymentRequestBody: PaymentRequestBody,
