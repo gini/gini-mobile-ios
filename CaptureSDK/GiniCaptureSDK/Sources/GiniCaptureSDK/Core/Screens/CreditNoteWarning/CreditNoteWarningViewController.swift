37dab18554d3a6fab3607761e509c0294b284dba
Qazi Naveed <qazi.naveed@gini.net>
2025-12-16 13:59:04 +0100

    feat(GiniBankSDK): adding localize content and colors
    PP-2241

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/CreditNoteWarning/CreditNoteWarningViewController.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Screens/CreditNoteWarning/CreditNoteWarningViewController.swift
@@ -7,14 +7,10 @@
 import UIKit
 
 private struct CreditNoteContentViewModel: InfoBottomSheetViewModel {
-    var image: UIImage? = UIImageNamedPreferred(named: "infoMessageIcon")
-
-    var imageTintColor: UIColor? = .yellow
-
-    var title: String = "creditNoteWarningTitle"
-
-    var description: String = "creditNoteWarningDescription"
-
+    var image: UIImage? = CreditNoteWarningViewController.Images.errorIcon
+    var imageTintColor: UIColor? = CreditNoteWarningViewController.Colors.errorTintColor
+    var title: String = CreditNoteWarningViewController.Strings.title
+    var description: String = CreditNoteWarningViewController.Strings.description
 }
 
 /**
@@ -26,10 +22,10 @@ public final class CreditNoteWarningViewController: InfoBottomSheetViewControlle
     public init(onCancel: @escaping () -> Void,
                 onProceed: @escaping () -> Void) {
         let contentViewModel = CreditNoteContentViewModel()
-        let primaryButton = InfoBottomSheetButtonsViewModel.Button(title: "cancelButton",
+        let primaryButton = InfoBottomSheetButtonsViewModel.Button(title: Strings.cancelButton,
                                                                    action: onCancel)
 
-        let secondaryButton = InfoBottomSheetButtonsViewModel.Button(title: "proceedButton",
+        let secondaryButton = InfoBottomSheetButtonsViewModel.Button(title: Strings.proceedButton,
                                                                      action: onProceed)
 
         let buttonsViewModel = InfoBottomSheetButtonsViewModel(primaryButton, secondaryButton)
@@ -43,7 +39,39 @@ public final class CreditNoteWarningViewController: InfoBottomSheetViewControlle
 }
 
 extension CreditNoteWarningViewController {
+    struct Strings {
+        static let titleKey = "ginicapture.creditNote.warning.title"
+        static let titleComment = "Credit Note Detected"
+        static let title = NSLocalizedStringPreferredFormat(titleKey,
+                                                            comment: titleComment)
 
+        static let descriptionKey = "ginicapture.creditNote.warning.description"
+        static let descriptionComment = "This document is marked as a credit note"
+        static let description = NSLocalizedStringPreferredFormat(descriptionKey,
+                                                                  comment: descriptionComment)
+
+        static let cancelButtonKey = "ginicapture.creditNote.warning.cancelButtonTitle"
+        static let cancelButtonComment = "Cancel"
+        static let cancelButton = NSLocalizedStringPreferredFormat(cancelButtonKey,
+                                                                   comment: cancelButtonComment)
+
+        static let proceedButtonKey = "ginicapture.creditNote.warning.proceedButtonTitle"
+        static let proceedButtonComment = "Proceed"
+        static let proceedButton = NSLocalizedStringPreferredFormat(proceedButtonKey,
+                                                                    comment: proceedButtonComment)
+    }
+
+    // MARK: - Images
+    struct Images {
+        static var errorIcon: UIImage? { UIImageNamedPreferred(named: "errorOctagon") }
+    }
+
+    // MARK: - Colors
+    struct Colors {
+        static var errorTintColor: UIColor {
+            GiniColor(light: .GiniCapture.warning2, dark: .GiniCapture.warning2).uiColor()
+        }
+    }
 }
 
 #if DEBUG
