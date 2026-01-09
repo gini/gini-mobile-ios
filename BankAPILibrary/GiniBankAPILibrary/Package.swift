cff7596390e0d41c155f21b645312c48f2877c67
Valentina Iancu <valentina.iancu@gini.net>
2026-01-09 09:57:38 +0100

    feat(GiniBankAPILibrary): Make packages only be of type dynamic when building via script
    PP-829

--- BankAPILibrary/GiniBankAPILibrary/Package.swift
+++ BankAPILibrary/GiniBankAPILibrary/Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import Foundation
 import PackageDescription
 
 let package = Package(
@@ -10,6 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GiniBankAPILibrary",
+            type: ProcessInfo.processInfo.environment["GINI_FORCE_DYNAMIC_LIBRARY"] == "1" ? .dynamic : nil,
             targets: ["GiniBankAPILibrary"]),
     ],
     dependencies: [
