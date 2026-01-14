6c3132b4ea0b95e0eab683f717b33a1ce23940c0
Valentina Iancu <valentina.iancu@gini.net>
2026-01-14 17:18:25 +0100

    fix(GiniCaptureSDK): Remove unused import
    PP-2232

--- CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Custom views/InfoBottomSheet/InfoBottomSheetViewController.swift
+++ CaptureSDK/GiniCaptureSDK/Sources/GiniCaptureSDK/Core/Custom views/InfoBottomSheet/InfoBottomSheetViewController.swift
@@ -4,7 +4,6 @@
 //  Copyright © 2025 Gini GmbH. All rights reserved.
 //
 import UIKit
-import GiniUtilites
 
 /**
  A bottom sheet view controller that displays informational content with an icon, title, description, and action buttons.
