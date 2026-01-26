f08da7b4cd7b6a5aae625ae6d1445d84e5b90d79
Lester Batres <lester.batres@gini.net>
2026-01-26 09:18:10 +0100

    feat(GiniInternalPaymentSDK):  Listen for screen size changes to dynamically build the image carousel.
    HEAL-107

--- GiniComponents/GiniInternalPaymentSDK/Sources/GiniInternalPaymentSDK/PaymentReviewV2/Views/PaymentReviewContentView.swift
+++ GiniComponents/GiniInternalPaymentSDK/Sources/GiniInternalPaymentSDK/PaymentReviewV2/Views/PaymentReviewContentView.swift
@@ -12,6 +12,7 @@ public struct PaymentReviewContentView: View {
     @State private var hasAppeared = false
     @State private var showBottomSheet = true
     @State private var bottomSheetHeight = Constants.bottomSheetDefaultHeight
+    @State private var carouselHeight = Constants.carouselDefaultHeight
     
     /// The init method is internal to prevent users from creating instances of this view directly
     /// outside of GiniInternalPaymentSDK.
@@ -20,33 +21,41 @@ public struct PaymentReviewContentView: View {
     }
     
     public var body: some View {
-        ZStack {
-            ScrollView {
-                documentPreviewContent
-                    .padding()
-                    .padding(.bottom, bottomSheetHeight - Constants.bottomSheetOverlap)
-            }
-            .onAppear {
-                guard !hasAppeared else { return }
-                hasAppeared = true
-                
-                Task {
-                    await viewModel.fetchImages()
+        GeometryReader { geometry in
+            ZStack {
+                ScrollView {
+                    documentPreviewContent()
+                        .padding()
+                        .padding(.bottom, max(bottomSheetHeight - Constants.bottomSheetOverlap, 0))
+                }
+                .onAppear {
+                    guard !hasAppeared else { return }
+                    hasAppeared = true
+                    
+                    Task {
+                        await viewModel.fetchImages()
+                    }
+                }
+                .onChange(of: bottomSheetHeight) { newValue in
+                    updateCarouselHeight(screenHeight: geometry.size.height)
+                }
+                .onChange(of: geometry.size.height) { newHeight in
+                    updateCarouselHeight(screenHeight: newHeight)
                 }
             }
-        }
-        .sheet(isPresented: $showBottomSheet) {
-            
-        } content: {
-            viewModel.paymentReviewPaymentInformationView(contentHeight: $bottomSheetHeight)
-                .modifier(GiniBottomSheetModifier(contentHeight: bottomSheetHeight))
+            .sheet(isPresented: $showBottomSheet) {
+                
+            } content: {
+                viewModel.paymentReviewPaymentInformationView(contentHeight: $bottomSheetHeight)
+                    .modifier(GiniBottomSheetModifier(contentHeight: bottomSheetHeight))
+            }
         }
     }
     
     // MARK: - Private Views
     
     @ViewBuilder
-    private var documentPreviewContent: some View {
+    private func documentPreviewContent() -> some View {
         VStack(spacing: 16) {
             if viewModel.isImagesLoading {
                 showLoader()
@@ -59,19 +68,29 @@ public struct PaymentReviewContentView: View {
     @ViewBuilder
     private func showLoader() -> some View {
         ProgressView()
-            .frame(height: 400)
+            .frame(height: carouselHeight)
     }
     
     @ViewBuilder
     private func showPreviewImageCarousel() -> some View {
         let images = viewModel.cellViewModels.compactMap { $0.preview }
+        
         GiniCarouselView(images: images)
-            .frame(height: 500)
+            .frame(height: carouselHeight)
+    }
+    
+    private func updateCarouselHeight(screenHeight: CGFloat) {
+        let totalPaddings = 32.0
+        let effectiveBottomSheetHeight = bottomSheetHeight > 0 ? bottomSheetHeight : Constants.bottomSheetDefaultHeight
+        let calculatedHeight = screenHeight - effectiveBottomSheetHeight + Constants.bottomSheetOverlap - totalPaddings
+        
+        carouselHeight = max(calculatedHeight, Constants.carouselDefaultHeight)
     }
     
     private struct Constants {
         
-        static let bottomSheetDefaultHeight: CGFloat = 400
+        static let bottomSheetDefaultHeight: CGFloat = 300
         static let bottomSheetOverlap: CGFloat = 20
+        static let carouselDefaultHeight = 300.0
     }
 }
