8c4c3c56b1b6bf021a8f7ae00cabb8a17be5b8a2
Lester Batres <lester.batres@gini.net>
2026-02-05 20:23:00 +0100

    feat(GiniInternalPaymentSDK):  Increase min version to `iOS16`
    HEAL-107

--- HealthSDK/GiniHealthSDK/Package.swift
+++ HealthSDK/GiniHealthSDK/Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 let package = Package(
     name: "GiniHealthSDK",
     defaultLocalization: "en",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS("16")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
