8321e4111652189f429677c759002c3e0a4338e8
Valentina Iancu <valentina.iancu@gini.net>
2026-02-26 13:56:55 +0100

    fix(GiniBankApiLibrary): Move `CancellationToken` in a separate file
    PP-2334

--- BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Core/SessionManager.swift
+++ BankAPILibrary/GiniBankAPILibrary/Sources/GiniBankAPILibrary/Documents/Core/SessionManager.swift
@@ -114,34 +114,6 @@ extension SessionManager: SessionProtocol {
     }
 }
 
-/**
- * Cancellation token needed during the analysis process.
- *
- * Calling `cancel()` sets the `isCancelled` flag so the SDK will discard any
- * subsequent response, and also cancels the underlying network request via
- * the `CancellableTask` returned by the `GiniHTTPClient`.
- */
-public final class CancellationToken {
-    internal var task: CancellableTask?
-
-    /** Indicates if the analysis has been cancelled */
-    public var isCancelled = false
-
-    public init() {
-        // This initializer is intentionally left empty because no custom setup is required at initialization.
-    }
-
-    /**
-     * Cancels the current task.
-     *
-     * This cancels the underlying network request (both for the default and
-     * custom HTTP clients) and sets `isCancelled` to `true`.
-     */
-    public func cancel() {
-        isCancelled = true
-        task?.cancel()
-    }
-}
 
 // MARK: - Private
 
