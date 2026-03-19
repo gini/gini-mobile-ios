ebd69280ae7f9428d12532b8bbb4b9e8dd0ece97
Nadya Karaban <nadya@gini.net>
2026-03-19 15:16:13 +0100

    fix(GiniUtilites): Replace centerXAnchor with frameLayoutGuide.widthAnchor in EmptyScrollView
    HEAL-107

--- GiniComponents/GiniUtilites/Sources/GiniUtilites/Views/EmptyScrollView.swift
+++ GiniComponents/GiniUtilites/Sources/GiniUtilites/Views/EmptyScrollView.swift
@@ -41,7 +41,7 @@ public final class EmptyScrollView: UIScrollView {
             contentView.bottomAnchor.constraint(equalTo: contentLayoutGuide.bottomAnchor),
             contentView.leadingAnchor.constraint(equalTo: contentLayoutGuide.leadingAnchor),
             contentView.trailingAnchor.constraint(equalTo: contentLayoutGuide.trailingAnchor),
-            contentView.widthAnchor.constraint(equalTo: widthAnchor)
+            contentView.widthAnchor.constraint(equalTo: frameLayoutGuide.widthAnchor)
         ])
         
         bindToSizeUpdates()
