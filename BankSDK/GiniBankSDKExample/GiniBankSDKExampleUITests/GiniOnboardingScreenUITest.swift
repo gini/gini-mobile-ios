f38c8be79c25a4f660c81fae6b3f84060958b167
Valentina Iancu <valentina.iancu@gini.net>
2026-04-06 18:35:53 +0200

    refactor(GiniBankSDKExample): Remove bottom nav bar onboarding UI tests

--- BankSDK/GiniBankSDKExample/GiniBankSDKExampleUITests/GiniOnboardingScreenUITest.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExampleUITests/GiniOnboardingScreenUITest.swift
@@ -46,8 +46,6 @@ class GiniOnboardingScreenUITest: GiniBankSDKExampleUITests {
     //Preconditions
         //Open settings screen
         mainScreen.configurationButton.tap()
-        //Enable Bottom navigation bar
-        mainScreen.tapSwitchNextToTextElement(text: settingScreen.bottomNavBar, enabled: true)
         //Enable Onboarding at every launch switch
         mainScreen.tapSwitchNextToTextElement(text: settingScreen.onboardingEveryLaunchSwitch, enabled: true)
         //Disable Onboarding at first launch switch
@@ -90,66 +88,6 @@ class GiniOnboardingScreenUITest: GiniBankSDKExampleUITests {
         app.swipeLeft()
         //Tap Get Started butto
         onboadingScreen.getStartedButton.tap()
-        //Assert Take pickrute button is displayed
-        XCTAssertTrue(captureScreen.captureButton.isHittable)
-    }
-    
-    func testOnboardingSwipeActionBottomNavBar() throws {
-        
-    //Preconditions
-        //Open settings screen
-        mainScreen.configurationButton.tap()
-        //Enable Bottom navigation bar
-        mainScreen.tapSwitchNextToTextElement(text: settingScreen.bottomNavBar, enabled: true)
-        //Enable Onboarding at every launch switch
-        mainScreen.tapSwitchNextToTextElement(text: settingScreen.onboardingEveryLaunchSwitch, enabled: true)
-        //Disable Onboarding at first launch switch
-        mainScreen.tapSwitchNextToTextElement(text: settingScreen.onboardingAtFirstLaunchSwitch, enabled: false)
-        settingScreen.closeButton.tap()
-    //Test Case
-        //Tap Photopaymen button
-        mainScreen.photoPaymentButton.tap()
-        //Handle Camera access pop up
-        mainScreen.handleCameraPermission(answer: true)
-        //Tap Next button
-        onboadingScreen.nextButton.tap()
-        //Tap Next button
-        onboadingScreen.nextButton.tap()
-        //Swipe Left
-        app.swipeLeft()
-        //Tap Get Started butto
-        onboadingScreen.getStartedButton.tap()
-        //Assert Take pickrute button is displayed
-        XCTAssertTrue(captureScreen.captureButton.isHittable)
-    }
-
-    func testOnboardingSwipeActionCustomBottomNavBar() throws {
-        
-    //Preconditions
-        //Open settings screen
-        mainScreen.configurationButton.tap()
-        //Enable Bottom navigation bar
-        mainScreen.tapSwitchNextToTextElement(text: settingScreen.bottomNavBar, enabled: true)
-        //Enable Onboarding at every launch switch
-        mainScreen.tapSwitchNextToTextElement(text: settingScreen.onboardingEveryLaunchSwitch, enabled: true)
-        //Disable Onboarding at first launch switch
-        mainScreen.tapSwitchNextToTextElement(text: settingScreen.onboardingAtFirstLaunchSwitch, enabled: false)
-        //Disable Onboarding at first launch switch
-        mainScreen.tapSwitchNextToTextElement(text: settingScreen.onboardingCustomBottomNavBar, enabled: true)
-        settingScreen.closeButton.tap()
-    //Test Case
-        //Tap Photopaymen button
-        mainScreen.photoPaymentButton.tap()
-        //Handle Camera access pop up
-        mainScreen.handleCameraPermission(answer: true)
-        //Tap Next button
-        onboadingScreen.nextButtonCustom.tap()
-        //Tap Next button
-        onboadingScreen.nextButtonCustom.tap()
-        //Swipe Left
-        app.swipeLeft()
-        //Tap Get Started butto
-        onboadingScreen.nextButtonCustom.tap()
         //Assert Take pickrute button is displayed
         XCTAssertTrue(captureScreen.captureButton.isHittable)
     }
