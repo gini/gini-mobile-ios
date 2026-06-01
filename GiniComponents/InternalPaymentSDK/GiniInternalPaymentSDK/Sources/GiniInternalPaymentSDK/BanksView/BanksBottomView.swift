fc3ed7d6f368522e5c98e3930c707bbb3260aace
Qazi Naveed <qazi.naveed@gini.net>
2026-06-01 17:40:47 +0200

    refactor(GiniMerchantSDK): Fix file headers in GiniInternalPaymentSDK and GiniUtilites
    PP-2326

--- GiniComponents/InternalPaymentSDK/GiniInternalPaymentSDK/Sources/GiniInternalPaymentSDK/BanksView/BanksBottomView.swift
+++ GiniComponents/InternalPaymentSDK/GiniInternalPaymentSDK/Sources/GiniInternalPaymentSDK/BanksView/BanksBottomView.swift
@@ -1,6 +1,6 @@
 //
 //  PaymentProvidersBottomView.swift
-//  GiniMerchantSDK
+//  GiniInternalPaymentSDK
 //
 //  Copyright © 2024 Gini GmbH. All rights reserved.
 //
--- GiniComponents/InternalPaymentSDK/GiniInternalPaymentSDK/Sources/GiniInternalPaymentSDK/MoreInformation/MoreInformationViewModel.swift
+++ GiniComponents/InternalPaymentSDK/GiniInternalPaymentSDK/Sources/GiniInternalPaymentSDK/MoreInformation/MoreInformationViewModel.swift
@@ -1,6 +1,6 @@
 //
 //  MoreInformationViewModel.swift
-//  GiniMerchantSDK
+//  GiniInternalPaymentSDK
 //
 //  Copyright © 2024 Gini GmbH. All rights reserved.
 //
