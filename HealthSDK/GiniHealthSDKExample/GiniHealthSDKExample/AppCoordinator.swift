da29b7d59e14cf778cabc1300f7323dfc3635691
Qazi Naveed <qazi.naveed@gini.net>
2026-06-10 18:18:50 +0200

    fix(GiniHealthSDKExampleApp): Used Ja via localize string instead of hardcoded string
    HEAL-418

--- HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/AppCoordinator.swift
+++ HealthSDK/GiniHealthSDKExample/GiniHealthSDKExample/AppCoordinator.swift
@@ -289,7 +289,7 @@ final class AppCoordinator: Coordinator {
                                                     "Gini Health SDK verwenden?",
                                                     preferredStyle: .alert)
         
-        let jaAction = UIAlertAction(title: "Ja", style: .default) { [weak self] _ in
+        let jaAction = UIAlertAction(title: NSLocalizedString("gini.health.example.order.detail.alert.ok", comment: ""), style: .default) { [weak self] _ in
             self?.showScreenAPI(with: pages)
         }
         alertViewController.addAction(jaAction)
