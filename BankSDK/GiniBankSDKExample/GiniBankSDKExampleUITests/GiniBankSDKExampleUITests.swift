495ff441fbe128b573712f7610ad29701732797a
Valentina Iancu <valentina.iancu@gini.net>
2026-06-12 19:25:08 +0200

    refactor(GiniBankSDK): Remove unused isSimulator property from UI tests
    PP-2483

--- BankSDK/GiniBankSDKExample/GiniBankSDKExampleUITests/GiniBankSDKExampleUITests.swift
+++ BankSDK/GiniBankSDKExample/GiniBankSDKExampleUITests/GiniBankSDKExampleUITests.swift
@@ -27,8 +27,6 @@ class GiniBankSDKExampleUITests: XCTestCase {
     var transactionSummaryScreen: TransactionSummaryScreen!
     var noResultsScreen: NoResultsScreen!
     var cxExtractionScreen: CXExtractionScreen!
-    var isSimulator = true
-
     /**
      Override in a subclass to inject extra launch arguments before the app launches.
      The base argument `-StartFromCleanState YES` is always included.
