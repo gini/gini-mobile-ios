e49b11880cf1be79b76c895817a126fc6b14108b
Nadya Karaban <nadya@gini.net>
2026-08-21 16:24:23 +0200

    fix(GiniBankSDK): Mirror Android flow
    PP-3261, PP-3263

--- BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankNetworkingScreenApiCoordinator.swift
+++ BankSDK/GiniBankSDK/Sources/GiniBankSDK/Core/GiniBankNetworkingScreenApiCoordinator.swift
@@ -825,7 +825,7 @@ internal extension GiniBankNetworkingScreenApiCoordinator {
         let analysisResult = AnalysisResult(extractions: isCrossBorderPayment() ? [:] : extractions,
                                             lineItems: isCrossBorderPayment() ? nil : extractionResult.lineItems,
                                             skontoDiscounts: isCrossBorderPayment() ? nil : extractionResult.skontoDiscounts,
-                                            crossBorderPayment: isCrossBorderPayment() ? extractionResult.crossBorderPayment : nil,
+                                            crossBorderPayment: nil,
                                             images: images,
                                             document: documentService.document,
                                             candidates: extractionResult.candidates)
